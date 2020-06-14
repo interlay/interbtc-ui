962439f525a1e394e809bfaabb4135e3b7610f7c
Dominik Harz <dominik.harz@gmail.com>
2020-06-14 19:42:48 +0900

    WIP: start react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import LandingPage from "./views/LandingPage";
+import Help from "./views/Help";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Topbar from "./components/Topbar";
+import Footer from "./components/Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <div class="main d-flex flex-column min-vh-100">
+        <div class="mb-5">
+          <LandingPage />
+        </div>
+        <Footer />
+      </div>
+    )
+  }
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
