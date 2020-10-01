53ffc6d59e3c1d3507092ed583ab0e70a9fb5076
Daniel Perez <daniel@perez.sh>
2020-10-02 00:10:45 +0100

    Add logic to get signer and address from extension

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,8 @@
+export const APP_NAME = "PolkaBTC";
 export const ALICE = "//Alice";
 export const BOB = "5DTestUPts3kjeXSTMyerHihn1uwMfLj8vU8sqF7qYrFabHE";
 export const ALICE_BTC = "tb1qmwv7aqktv5l44x65qmsk6u4z9wh66nazv9rgv3"; //Alexei's testnet BTC address
 export const BOB_BTC = "tb1q4kspwcf42cqp66hrhw407djna4dgpw9lsnfx5e"; // Dom's testnet BTC address
+
+export const PARACHAIN_URL = process.env.REACT_APP_PARACHAIN_URL || "ws://127.0.0.1:9944";
+export const STAKED_RELAYER_URL = process.env.REACT_APP_STAKED_RELAYER_URL || "http://localhost:3030";
