a042bb4d9021992b2e2e4eab105f7ef163dde4ce
Dominik Harz <dominik@interlay.io>
2020-10-01 14:31:23 +0200

    fix build error

--- src/common/components/issue/request-confirmation.tsx
+++ src/common/components/issue/request-confirmation.tsx
@@ -3,14 +3,6 @@ import { FormGroup, ListGroup, ListGroupItem } from "react-bootstrap";
 import { IssueWizardProps } from "./issue-wizard";
 
 export default function RequestConfirmation(props: IssueWizardProps){
-    const polkaBTC = useSelector((state: StoreType)=>state.api);
-
-    useEffect(()=>{
-        const fetchData = async () => {
-        }
-        fetchData();
-    },[polkaBTC])
-
     if (props.step !== 2) {
         return null;
     }
