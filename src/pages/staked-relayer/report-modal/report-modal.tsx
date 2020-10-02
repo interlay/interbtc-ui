4238aeb0a10641118ebf0d3edbf3e3a552891343
Gregory Hill <gregorydhill@outlook.com>
2020-10-02 09:57:10 +0100

    deregister call
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- src/pages/staked-relayer/report-modal/report-modal.tsx
+++ src/pages/staked-relayer/report-modal/report-modal.tsx
@@ -3,91 +3,90 @@ import { Modal, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { StoreType } from "../../../common/types/util.types";
+import { toast } from "react-toastify";
 
 const STATUS_UPDATE_DEPOSIT = 100;
 
 type ReportModalType = {
-  onClose: () => void;
-  show: boolean;
+    onClose: () => void;
+    show: boolean;
 };
 
 type ReportForm = {
-  btcBlock: string;
-  message: string;
+    btcBlock: string;
+    message: string;
 };
 
 export default function ReportModal(props: ReportModalType): ReactElement {
-  const { register, handleSubmit, errors } = useForm<ReportForm>();
-  const stakedRelayer = useSelector((state: StoreType) => state.relayer);
+    const { register, handleSubmit, errors } = useForm<ReportForm>();
+    const stakedRelayer = useSelector((state: StoreType) => state.relayer);
 
-  const onSubmit = handleSubmit(({ btcBlock, message }) => {
-    const reportInvalidBlock = async () => {
-      let result = await stakedRelayer.suggestInvalidBlock(STATUS_UPDATE_DEPOSIT, btcBlock);
-      console.log(result,message);
-    }
-    reportInvalidBlock();
-    props.onClose();
-  });
+    const onSubmit = handleSubmit(({ btcBlock, message }) => {
+        const reportInvalidBlock = async () => {
+            try {
+                await stakedRelayer.suggestInvalidBlock(STATUS_UPDATE_DEPOSIT, btcBlock);
+                toast.success("Status Update Suggested");
+                props.onClose();
+            } catch (error) {
+                toast.error(error.toString());
+            }
+        };
+        reportInvalidBlock();
+    });
 
-  return (
-    <Modal show={props.show} onHide={props.onClose}>
-      <form onSubmit={onSubmit}>
-        <Modal.Header closeButton>
-          <Modal.Title>Report Invalid BTC Block in BTC-Relay</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="row">
-            <div className="col-12">BTC-Relay block header</div>
-            <div className="col-12">
-              <input
-                name="btcBlock"
-                type="text"
-                className={
-                  "custom-input" + (errors.btcBlock ? " error-borders" : "")
-                }
-                ref={register({
-                  required: true,
-                  pattern: {
-                    value: /^[1-9a-zA-Z]{1,1}[0-9a-zA-Z]{31,31}$/,
-                    message: "Please enter valid BTC header",
-                  },
-                })}
-              ></input>
-              {errors.btcBlock && (
-                <div className="input-error">
-                  {errors.btcBlock.type === "required"
-                    ? "BTC-Relay block header is required"
-                    : errors.btcBlock.message}
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12">Proof / Message</div>
-            <div className="col-12">
-              <textarea
-                className={
-                  "custom-textarea" + (errors.message ? " error-borders" : "")
-                }
-                name="message"
-                ref={register({ required: true })}
-                rows={6}
-              ></textarea>
-              {errors.message && (
-                <div className="input-error">Message is required</div>
-              )}
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.onClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" type="submit">
-            Report
-          </Button>
-        </Modal.Footer>
-      </form>
-    </Modal>
-  );
+    return (
+        <Modal show={props.show} onHide={props.onClose}>
+            <form onSubmit={onSubmit}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Report Invalid BTC Block in BTC-Relay</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="row">
+                        <div className="col-12">BTC-Relay block header</div>
+                        <div className="col-12">
+                            <input
+                                name="btcBlock"
+                                type="text"
+                                className={"custom-input" + (errors.btcBlock ? " error-borders" : "")}
+                                ref={register({
+                                    required: true,
+                                    pattern: {
+                                        value: /^[1-9a-zA-Z]{1,1}[0-9a-zA-Z]{31,31}$/,
+                                        message: "Please enter valid BTC header",
+                                    },
+                                })}
+                            ></input>
+                            {errors.btcBlock && (
+                                <div className="input-error">
+                                    {errors.btcBlock.type === "required"
+                                        ? "BTC-Relay block header is required"
+                                        : errors.btcBlock.message}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">Proof / Message</div>
+                        <div className="col-12">
+                            <textarea
+                                className={"custom-textarea" + (errors.message ? " error-borders" : "")}
+                                name="message"
+                                ref={register({ required: true })}
+                                rows={6}
+                            ></textarea>
+                            {errors.message && <div className="input-error">Message is required</div>}
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.onClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="primary" type="submit">
+                        Report
+                    </Button>
+                </Modal.Footer>
+            </form>
+        </Modal>
+    );
 }
