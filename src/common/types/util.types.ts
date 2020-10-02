2cf3c9b83f8ae9b387e8ba341562d56a00d6946c
Dominik Harz <dominik@interlay.io>
2020-10-02 16:01:26 +0200

    fixes in tables

--- src/common/types/util.types.ts
+++ src/common/types/util.types.ts
@@ -2,6 +2,7 @@ import { Store, CombinedState } from "redux";
 import { AddInstance, AddStakedRelayerInstance } from "./actions.types";
 import { PolkaBTCAPI, StakedRelayerClient } from "@interlay/polkabtc";
 import { rootReducer } from "../reducers/index";
+import { DOT, PolkaBTC } from "@interlay/polkabtc/build/interfaces/default";
 
 export interface Prices {
     dotBtc: number;
@@ -12,9 +13,9 @@ export type Vault = {
     vaultId: string;
     btcAddress: string;
     lockedDOT: number;
-    lockedBTC: number;
+    lockedBTC: PolkaBTC;
     status: string;
-    collateralization: number;
+    collateralization: number | undefined;
 };
 
 export type AppState = ReturnType<typeof rootReducer>;
