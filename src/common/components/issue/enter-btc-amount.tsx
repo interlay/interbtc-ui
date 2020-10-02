9b2996f74f7006e2343b9883374f8687cc669a9f
is319 <is319@ic.ac.uk>
2020-10-02 11:38:54 +0300

    Send requiestIssue

--- src/common/components/issue/enter-btc-amount.tsx
+++ src/common/components/issue/enter-btc-amount.tsx
@@ -6,27 +6,41 @@ import { StoreType } from "../../types/util.types";
 
 interface EnterBTCAmountProps {
     step: number;
-    amountBTC: string;
-    feeBTC: string;
-    vaultBTCAddress: string;
-    handleChange: () => void;
+    amountBTC: string,
+    feeBTC: string,
+    vaultBTCAddress: string,
+    vaultId: string,
+    handleChange: () => void,
 }
 
 export default function EnterBTCAmount(props: IssueWizardProps | EnterBTCAmountProps) {
     const polkaBTC = useSelector((state: StoreType) => state.api);
     if (props.step !== 1) {
         return null;
-    } else if (props.vaultBTCAddress == "") {
+    }
+    else if (props.vaultBTCAddress === "") {
         const fetchData = async () => {
             const polkaBTCObject = polkaBTC.api.createType("Balance", props.amountBTC);
-            const vaultBTCAddress = await polkaBTC.vaults.selectRandomVaultIssue(polkaBTCObject);
-            const vault = await polkaBTC.vaults.get(vaultBTCAddress);
-            props.handleChange({
-                target: {
-                    name: "vaultBTCAddress",
-                    value: vault.btc_address.toHuman(),
-                } as EventTarget & HTMLInputElement,
-            } as ChangeEvent<HTMLInputElement>);
+            const vaultId = await polkaBTC.vaults.selectRandomVaultIssue(polkaBTCObject);
+            const vault = await polkaBTC.vaults.get(vaultId);
+            props.handleChange(
+                {
+                    target: {
+                        name: "vaultBTCAddress",
+                        value: vault.btc_address.toHuman()
+                    } as EventTarget & HTMLInputElement
+                } as ChangeEvent<HTMLInputElement>
+            );
+
+            props.handleChange(
+                {
+                    target:
+                        {
+                            name: "vaultId",
+                            value: vault.id.toHuman()
+                        } as EventTarget & HTMLInputElement
+                } as ChangeEvent<HTMLInputElement>
+            );
         };
         fetchData();
     }
