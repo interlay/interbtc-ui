aca074af674989e04a9ed2b0267be78e377cb1e0
filip88 <eing.filip@gmail.com>
2020-11-16 14:15:33 +0100

    fix NaN bug on vault dashboard page for collateralization

--- src/pages/vault-dashboard/vault-dashboard.page.tsx
+++ src/pages/vault-dashboard/vault-dashboard.page.tsx
@@ -133,7 +133,9 @@ export default function VaultDashboardPage() {
                             <div className="col-12">
                                 <div className="stats">
                                     Collateralization: &nbsp;
-                                    {collateralization === undefined ? "∞" : `${(collateralization * 100).toFixed(3)}%`}
+                                    {collateralization === undefined || isNaN(collateralization) ? 
+                                        "∞" : `${(collateralization * 100).toFixed(3)}%`
+                                    }
                                 </div>
                             </div>
                         </div>
