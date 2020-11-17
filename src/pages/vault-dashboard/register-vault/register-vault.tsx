81d9948f9ceeea28ebb4dfc564b90cb502134fb3
alexei.zamyatin <alexei.zamyatin@gmail.com>
2020-11-17 16:39:31 +0100

    Updated BTC address regex to exclude bech32 for now. Updated user guide

--- src/pages/vault-dashboard/register-vault/register-vault.tsx
+++ src/pages/vault-dashboard/register-vault/register-vault.tsx
@@ -105,7 +105,7 @@ export default function RegisterVaultModal(props: RegisterVaultProps) {
                                     required: true,
                                     pattern: {
                                         value: BTC_ADDRESS_REGEX,
-                                        message: "Please enter valid BTC address",
+                                        message: "Please enter a valid BTC address (Supported formats: p2pkh, p2sh, p2wpkh, p2wsh. Bech32 currently NOT supported).",
                                     },
                                 })}
                             ></input>
--- src/pages/vault-dashboard/update-btc-address/update-btc-address.tsx
+++ src/pages/vault-dashboard/update-btc-address/update-btc-address.tsx
@@ -61,7 +61,7 @@ export default function UpdateBTCAddressModal(props: UpdateBTCAddressProps) {
                                     required: true,
                                     pattern: {
                                         value: BTC_ADDRESS_REGEX,
-                                        message: "Please enter valid BTC address",
+                                        message: "Please enter a valid BTC address (Supported formats: p2pkh, p2sh, p2wpkh, p2wsh. Bech32 currently NOT supported).",
                                     },
                                 })}
                             ></input>
