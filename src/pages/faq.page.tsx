ff2c98476f6f3f5bac76ac0c7ac6ea5e084302ba
alexei.zamyatin <alexei.zamyatin@gmail.com>
2020-11-18 17:07:52 +0100

    minor update to faq page

--- src/pages/faq.page.tsx
+++ src/pages/faq.page.tsx
@@ -6,10 +6,9 @@ type Question = {
     id: number;
     question: string;
     answer: string;
-}
+};
 
 export default function FaqPage(): JSX.Element {
-
     return (
         <div>
             <section className="jumbotron text-center transparent-background">
@@ -31,14 +30,15 @@ export default function FaqPage(): JSX.Element {
                                             <Card.Title style={{ fontSize: "1.1em" }}>{qes.question}</Card.Title>
                                         </Accordion.Toggle>
                                         <Accordion.Collapse eventKey={qes.id.toString()}>
-                                            <Card.Body>
-                                                {qes.answer}
-                                            </Card.Body>
+                                            <Card.Body>{qes.answer}</Card.Body>
                                         </Accordion.Collapse>
                                     </Card>
                                 </Accordion>
                             ))}
-                            <p className="mt-2">Got more questions? <a href="https://discord.gg/C8tjMbgVXh">Join our Discord</a> and ask away!</p>
+                            <p className="mt-2">
+                                Got more questions? <a href="https://discord.gg/C8tjMbgVXh">Join our Discord</a> and ask
+                                away!
+                            </p>
                         </Col>
                     </Row>
                 </div>
