2fc05d22892ea26bf1f5d99f13193a39d417b4e5
alexei.zamyatin <alexei.zamyatin@gmail.com>
2020-11-26 18:30:41 +0100

    Updated staked relayer and vault page design

--- src/common/components/balances.tsx
+++ src/common/components/balances.tsx
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { Col, Row } from "react-bootstrap";
 
+type BalancesProps = {
+    balancePolkaBTC?: string;
+    balanceDOT?: string;
+};
 
-export default class Balances extends React.Component<{
-    balanceDOT: string;
-    balancePolkaBTC: string;
-}> {
-    render() {
-        return (
-            <div>
-                <Row className="mt-5">
-                    <Col xs="12" sm={{ span: 6, offset: 3 }}>
-                        <span className="heavy">{this.props.balancePolkaBTC}</span> PolkaBTC
+export default function Balances(props: BalancesProps): ReactElement {
+    return (
+        <div>
+            <Row className="mt-5">
+                <Col xs="12" sm={{ span: 6, offset: 3 }}>
+                    <span className="heavy">{props.balancePolkaBTC}</span> PolkaBTC
                     </Col>
-                </Row>
-                <Row className="mt-1">
-                    <Col xs="12" sm={{ span: 6, offset: 3 }}>
-                        <span className="heavy">{this.props.balanceDOT}</span> DOT
+            </Row>
+            <Row className="mt-1">
+                <Col xs="12" sm={{ span: 6, offset: 3 }}>
+                    <span className="heavy">{props.balanceDOT}</span> DOT
                     </Col>
-                </Row>
-            </div>
-        );
-    }
+            </Row>
+        </div>
+    );
 }
