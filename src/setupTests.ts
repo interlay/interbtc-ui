0af0f5fba01d9154563696df74a95ea06e9a5af0
filip88 <eing.filip@gmail.com>
2020-12-03 22:59:43 +0100

    tests: setting up test environment

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,43 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+import { JSDOM } from "jsdom";
+import register from "ignore-styles";
+
+register([".sass", ".scss"]);
+
+interface Global extends NodeJS.Global {
+    window: Window;
+    document: Document;
+    navigator: {
+        userAgent: string;
+    };
+}
+
+const globalNode: Global = {
+    ...global,
+    window: window,
+    document: window.document,
+    navigator: {
+        userAgent: "node.js",
+    },
+};
+
+// Simulate window for Node.js
+if (!globalNode.window && !globalNode.document) {
+    const { window } = new JSDOM("<!DOCTYPE html><html><head></head><body></body></html>", {
+        beforeParse(win) {
+            win.scrollTo = () => {
+                console.log("scroll");
+            };
+        },
+        pretendToBeVisual: false,
+        userAgent: "mocha",
+    });
+
+    // configure global variables wich like to be used in testing
+    globalNode.document = window.document;
+    globalNode.navigator = window.navigator;
+}
+
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
