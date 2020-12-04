276fca6b7740b512424e2a4c63d70648ac41e1aa
filip88 <eing.filip@gmail.com>
2020-12-04 19:17:40 +0100

    tests: fix error with the tests in docker, and polyfill

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,41 +1,8 @@
-import { JSDOM } from "jsdom";
 import register from "ignore-styles";
+import "jsdom-global/register";
+import "raf/polyfill";
 
 register([".sass", ".scss"]);
-
-interface Global extends NodeJS.Global {
-    window: Window;
-    document: Document;
-    navigator: {
-        userAgent: string;
-    };
-}
-
-const globalNode: Global = {
-    ...global,
-    window: window,
-    document: window.document,
-    navigator: {
-        userAgent: "node.js",
-    },
-};
-
-// Simulate window for Node.js
-if (!globalNode.window && !globalNode.document) {
-    const { window } = new JSDOM("<!DOCTYPE html><html><head></head><body></body></html>", {
-        beforeParse(win) {
-            win.scrollTo = () => {
-                console.log("scroll");
-            };
-        },
-        pretendToBeVisual: false,
-        userAgent: "mocha",
-    });
-
-    // configure global variables wich like to be used in testing
-    globalNode.document = window.document;
-    globalNode.navigator = window.navigator;
-}
 
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
