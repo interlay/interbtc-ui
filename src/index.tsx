d99ffe02fcd032c60451509e5fa62f569dc4498e
filip88 <eing.filip@gmail.com>
2020-12-08 23:33:29 +0100

    i18next : remove using backend as translaciton loader

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { configureStore } from "./store";
 import { Provider } from "react-redux";
-import "./i18n";
 // import * as serviceWorker from './serviceWorker';
 const store = configureStore();
 
