dd59c214ed991c2ab2edf93f200e38e9fcc00e29
Theodore Bugnet <theodore@interlay.io>
2021-01-21 13:28:00 +0000

    feat(dashboards): initial stats API integration

--- src/pages/dashboard/issue/issue.dashboard.page.tsx
+++ src/pages/dashboard/issue/issue.dashboard.page.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, ReactElement, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
+import * as polkabtcStats from "@interlay/polkabtc-stats";
+
 import IssueTable from "./issue-table/issue-table";
 import { StoreType } from "../../../common/types/util.types";
 import { IssueRequest } from "../../../common/types/issue.types";
@@ -9,22 +11,27 @@ import { IssueRequest } from "../../../common/types/issue.types";
 export default function IssueDashboard(): ReactElement {
     const { polkaBtcLoaded, totalPolkaBTC } = useSelector((state: StoreType) => state.general);
     const { t } = useTranslation();
+    const statsApi = useMemo(
+        () => new polkabtcStats.StatsApi(new polkabtcStats.Configuration({ basePath: "http://localhost:3001" })),
+        []
+    );
 
     const [totalSuccessfulIssues, setTotalSuccessfulIssues] = useState("0");
     const [issueRequests, setIssueRequests] = useState(new Array<IssueRequest>());
-    const [cumulativeIssuesPerDay, setCumulativeIssuesPerDay] = useState(new Array<{ date: Date; amount: number }>());
+    const [cumulativeIssuesPerDay, setCumulativeIssuesPerDay] = useState(new Array<{ date: number; count: number }>());
     const pointIssuesPerDay = useMemo(
         () =>
             cumulativeIssuesPerDay.map((dataPoint, i) => {
                 if (i === 0) return 0;
-                return dataPoint.amount - cumulativeIssuesPerDay[i - 1].amount;
+                return dataPoint.count - cumulativeIssuesPerDay[i - 1].count;
             }),
         [cumulativeIssuesPerDay]
     );
 
     useEffect(() => {
         const fetchTotalSuccessfulIssues = async () => {
-            setTotalSuccessfulIssues("443");
+            const res = await statsApi.getTotalSuccessfulIssues();
+            setTotalSuccessfulIssues(res.data);
         };
 
         const fetchIssueRequests = async () => {
@@ -47,15 +54,15 @@ export default function IssueDashboard(): ReactElement {
         };
 
         const fetchIssuesLastDays = async () => {
-            setCumulativeIssuesPerDay(
-                [0, 1, 2, 3, 4, 5, 6].map((d) => ({ date: new Date(Date.now() - d * 86400000), amount: 50 - d }))
-            );
+            const res = await statsApi.getRecentDailyIssues(6);
+            setCumulativeIssuesPerDay(res.data);
         };
+
         (async () => {
             if (!polkaBtcLoaded) return;
             await Promise.all([fetchTotalSuccessfulIssues(), fetchIssueRequests(), fetchIssuesLastDays()]);
         })();
-    }, [polkaBtcLoaded]);
+    }, [polkaBtcLoaded, statsApi]);
 
     return (
         <div className="dashboard-page container-fluid white-background">
