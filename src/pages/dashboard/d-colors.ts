f18e185d8ed4170377f8242da7025601c3ff1bf4
Chanakya Kilaru <samuraichanakya1@gmail.com>
2021-01-21 12:26:13 +0530

    added card and setup styles

--- src/pages/dashboard/d-colors.ts
+++ src/pages/dashboard/d-colors.ts
@@ -1,6 +1,6 @@
-export var d_yellow: String = "#f7931a";
-export var d_pink: String = "#e6007a";
-export var d_blue: String = "#1c86ee";
-export var d_grey: String = "#a9a9a9";
-export var d_orange: String = "#f95738";
-export var d_green: String = "#4ac948";
+export var d_yellow: string = "#f7931a";
+export var d_pink: string = "#e6007a";
+export var d_blue: string = "#1c86ee";
+export var d_grey: string = "#a9a9a9";
+export var d_orange: string = "#f95738";
+export var d_green: string = "#4ac948";
