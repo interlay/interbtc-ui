752171666b730f3ade9b930aa84f3c6ec22a5d9b
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2021-02-28 07:21:04 +0300

    chore: add a placeholder into .lintstagedrc.js

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  // ray test touch <
+  "./src/**/*.{ts,tsx}": "eslint", // TODO: placeholder for now
+  // "./src/**/*.{ts,tsx}": "eslint --fix",
+  // "./src/**/*.{ts,tsx,scss}": "prettier --config .prettierrc --write"
+  // ray test touch >
+};
