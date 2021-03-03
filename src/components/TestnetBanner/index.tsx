caff30b336c996f08e0fc7abdfc0c6892c69aabe
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2021-03-03 16:36:34 +0300

    fix: adjust the width of TestnetBanner

--- src/components/TestnetBanner/index.tsx
+++ src/components/TestnetBanner/index.tsx
@@ -1,13 +1,15 @@
 
 import { useTranslation } from 'react-i18next';
+import clsx from 'clsx';
+
+import styles from './testnet-banner.module.css';
 
 function TestnetBanner() {
   const { t } = useTranslation();
 
   return (
     <div
-      style={{ margin: 16 }}
-      className='alert border-danger text-center'>
+      className={clsx(styles['testnet-banner'], 'alert border-danger text-center')}>
       <strong className='font-pink'>
         {t('testnet.warning')}
       </strong>
