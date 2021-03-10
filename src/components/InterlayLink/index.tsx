28d3eb0f4db1f49d59a4101bff83871504ae34b5
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-10 17:38:54 +0300

    feat: update the styling for feedback cards

--- src/components/InterlayLink/index.tsx
+++ src/components/InterlayLink/index.tsx
@@ -1,13 +1,21 @@
 
+import clsx from 'clsx';
+
+import styles from './interlay-link.module.css';
+
 interface Props {
+  className?: string;
   children: React.ReactNode;
 }
 
 const InterlayLink = ({
+  className,
   children,
   ...rest
 }: Props & React.ComponentPropsWithoutRef<'a'>) => (
-  <a {...rest}>
+  <a
+    className={clsx(styles['interlay-link'], className)}
+    {...rest}>
     {children}
   </a>
 );
