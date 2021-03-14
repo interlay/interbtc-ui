245a986f466e82f445968bfb36579af4a2259310
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-14 10:33:03 +0300

    refactor: update MainContainer using tailwindcss

--- src/parts/MainContainer/index.tsx
+++ src/parts/MainContainer/index.tsx
@@ -1,15 +1,18 @@
 
 import clsx from 'clsx';
 
-import styles from './main-container.module.css';
-
 // TODO: should double-check as it's a temporary alternative
 const MainContainer = ({
   className,
   ...rest
 }: React.ComponentPropsWithoutRef<'div'>) => (
   <div
-    className={clsx(styles['main-container'], className)}
+    className={clsx(
+      'min-h-screen',
+      'py-7',
+      'xl:py-10',
+      className
+    )}
     {...rest} />
 );
 
