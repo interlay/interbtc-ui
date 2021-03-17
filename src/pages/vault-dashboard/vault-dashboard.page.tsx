dd0ef6718db2da7cd221068f98cd6e7924142395
Dominik Harz <dominik@interlay.io>
2021-03-17 18:10:54 +0100

    fix: parallel loading of vault dashboard values

--- src/pages/vault-dashboard/vault-dashboard.page.tsx
+++ src/pages/vault-dashboard/vault-dashboard.page.tsx
@@ -9,7 +9,6 @@ import RedeemTable from './redeem-table/redeem-table';
 import ReplaceTable from './replace-table/replace-table';
 import { satToBTC, planckToDOT } from '@interlay/polkabtc';
 import {
-  updateBTCAddressAction,
   updateCollateralizationAction,
   updateCollateralAction,
   updateLockedBTCAction,
@@ -17,7 +16,6 @@ import {
   updateAPYAction
 } from '../../common/actions/vault.actions';
 import './vault-dashboard.page.scss';
-import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
 import { safeRoundTwoDecimals } from '../../common/utils/utils';
 import TimerIncrement from '../../common/components/timer-increment';
@@ -48,39 +46,62 @@ export default function VaultDashboardPage(): ReactElement {
 
       try {
         const vaultId = window.polkaBTC.api.createType(ACCOUNT_ID_TYPE_NAME, address);
-        const vault = await window.polkaBTC.vaults.get(vaultId);
 
-        let vaultBTCAddress = vault.wallet.btcAddress;
-        vaultBTCAddress = vaultBTCAddress ? vaultBTCAddress : '';
-        dispatch(updateBTCAddressAction(vaultBTCAddress));
+        const [
+          vault,
+          feesPolkaSAT,
+          feesPlanck,
+          totalPolkaSAT,
+          collateralization,
+          slaScore,
+          apyScore,
+          issuablePolkaBTC
+        ] = await Promise.allSettled([
+          window.polkaBTC.vaults.get(vaultId),
+          window.polkaBTC.vaults.getFeesPolkaBTC(vaultId),
+          window.polkaBTC.vaults.getFeesDOT(vaultId),
+          window.polkaBTC.vaults.getIssuedPolkaBTCAmount(vaultId),
+          window.polkaBTC.vaults.getVaultCollateralization(vaultId),
+          window.polkaBTC.vaults.getSLA(vaultId),
+          window.polkaBTC.vaults.getAPY(vaultId),
+          window.polkaBTC.vaults.getIssuablePolkaBTC()
+        ]);
 
-        const balanceLockedDOT = await window.polkaBTC.collateral.balanceLockedDOT(vaultId);
-        const collateralDot = planckToDOT(balanceLockedDOT.toString());
-        dispatch(updateCollateralAction(collateralDot));
+        // const balanceLockedDOT = await window.polkaBTC.collateral.balanceLockedDOT(vaultId);
+        if (vault.status === 'fulfilled') {
+          const collateralDot = planckToDOT(vault.value.backing_collateral.toString());
+          dispatch(updateCollateralAction(collateralDot));
+        }
 
-        const feesPolkaSAT = await window.polkaBTC.vaults.getFeesPolkaBTC(vaultId);
-        setFeesEarnedPolkaBTC(satToBTC(feesPolkaSAT));
+        if (feesPolkaSAT.status === 'fulfilled') {
+          setFeesEarnedPolkaBTC(satToBTC(feesPolkaSAT.value));
+        }
 
-        const feesPlanck = await window.polkaBTC.vaults.getFeesDOT(vaultId);
-        setFeesEarnedDOT(planckToDOT(feesPlanck));
+        if (feesPlanck.status === 'fulfilled') {
+          setFeesEarnedDOT(planckToDOT(feesPlanck.value));
+        }
 
-        const totalPolkaSAT = await window.polkaBTC.vaults.getIssuedPolkaBTCAmount(vaultId);
         const lockedAmountBTC = satToBTC(totalPolkaSAT.toString());
         dispatch(updateLockedBTCAction(lockedAmountBTC));
 
-        const collateralization = await window.polkaBTC.vaults.getVaultCollateralization(vaultId);
-        dispatch(updateCollateralizationAction(collateralization?.mul(100).toString()));
+        if (collateralization.status === 'fulfilled') {
+          dispatch(updateCollateralizationAction(collateralization.value?.mul(100).toString()));
+        }
 
-        const slaScore = await window.polkaBTC.vaults.getSLA(vaultId);
-        dispatch(updateSLAAction(slaScore));
+        if (slaScore.status === 'fulfilled') {
+          dispatch(updateSLAAction(slaScore.value));
+        }
 
-        const apyScore = await window.polkaBTC.vaults.getAPY(vaultId);
-        dispatch(updateAPYAction(apyScore));
+        if (apyScore.status === 'fulfilled') {
+          dispatch(updateAPYAction(apyScore.value));
+        }
 
-        const issuablePolkaBTC = await window.polkaBTC.vaults.getIssuablePolkaBTC();
-        setCapacity(issuablePolkaBTC);
-      } catch (err) {
-        toast.error(err);
+        if (issuablePolkaBTC.status === 'fulfilled') {
+          setCapacity(issuablePolkaBTC.value);
+        }
+      } catch (error) {
+        console.log('Error getting vault data');
+        console.log(error.message);
       }
     };
     fetchData();
@@ -102,7 +123,7 @@ export default function VaultDashboardPage(): ReactElement {
                   className='card stats-card mb-3'
                   style={{ minHeight: '100px' }}>
                   <div className=''>{t('vault.locked_collateral')}</div>
-                  <span className='stats'>{collateral}</span> DOT
+                  <span className='stats'>{safeRoundTwoDecimals(collateral)}</span> DOT
                 </div>
               </div>
               <div className='col-lg-3 col-md-6 col-6'>
