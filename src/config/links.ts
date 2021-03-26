44ee185d73ee35b65e37fd18e7d0fab19b3ea213
Chanakya888 <samuraichanakya1@gmail.com>
2021-03-26 11:11:23 +0530

    feat: footer

--- src/config/links.ts
+++ src/config/links.ts
@@ -8,6 +8,8 @@ const INTERLAY_EMAIL = 'mailto:polkabtc@interlay.io';
 const INTERLAY_DISCORD = 'https://discord.gg/KgCYK3MKSf';
 const INTERLAY_GITHUB = 'https://github.com/interlay';
 const INTERLAY_TWITTER = 'https://twitter.com/interlayHQ';
+const INTERLAY_MEDIUM = 'https://medium.com/interlay';
+const INTERLAY_LINKEDIN = 'https://www.linkedin.com/company/interlay';
 
 const USER_FEEDBACK_FORM = 'https://forms.gle/JXBoRdspbG8pMs3k6';
 const VAULT_FEEDBACK_FORM = 'https://forms.gle/zzKhaEzttCKksjej7';
@@ -19,6 +21,14 @@ const POLKA_BTC_DOC_TREASURE_HUNT_VAULT =
 const POLKA_BTC_DOC_TREASURE_HUNT_RELAYER =
   'https://docs.polkabtc.io/#/start/beta?id=staked-relayer-award-4-dot-for-the-first-50-200-dot-in-total';
 
+const GETTING_STARTED = 'https://docs.polkabtc.io/#/start/overview';
+const VAULTS_DOCS = 'https://docs.polkabtc.io/#/vault/overview';
+const RELAYERS_DOCS = 'https://docs.polkabtc.io/#/relayer/overview';
+const DEVELOPERS_DOCS = 'https://docs.polkabtc.io/#/developers/integration';
+const ROADMAP = 'https://docs.polkabtc.io/#/about/roadmap';
+
+const PRIVACY_POLICY = 'https://www.interlay.io/docs/privacy-policy.pdf';
+const NEWSLETTER_ACTION = 'https://interlay.us4.list-manage.com/subscribe/post?u=4c3c3f21d3ec4c3ed94ea7353&amp;id=ad217abce9';
 export {
   POLKA_BTC_UI_GITHUB,
   WEB3_FOUNDATION,
@@ -27,11 +37,20 @@ export {
   INTERLAY_DISCORD,
   INTERLAY_GITHUB,
   INTERLAY_TWITTER,
+  INTERLAY_LINKEDIN,
+  INTERLAY_MEDIUM,
   POLKA_BTC_UI_GITHUB_ISSUES,
   USER_FEEDBACK_FORM,
   VAULT_FEEDBACK_FORM,
   RELAYER_FEEDBACK_FORM,
   POLKA_BTC_DOC_TREASURE_HUNT,
   POLKA_BTC_DOC_TREASURE_HUNT_VAULT,
-  POLKA_BTC_DOC_TREASURE_HUNT_RELAYER
+  POLKA_BTC_DOC_TREASURE_HUNT_RELAYER,
+  GETTING_STARTED,
+  VAULTS_DOCS,
+  RELAYERS_DOCS,
+  DEVELOPERS_DOCS,
+  ROADMAP,
+  PRIVACY_POLICY,
+  NEWSLETTER_ACTION
 };
--- src/parts/Footer/index.tsx
+++ src/parts/Footer/index.tsx
@@ -1,10 +1,5 @@
 
-import {
-  FaGithub,
-  FaTwitter,
-  FaDiscord
-} from 'react-icons/fa';
-import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
 
 import InterlayImage from 'components/UI/InterlayImage';
 import InterlayLink from 'components/UI/InterlayLink';
@@ -15,9 +10,26 @@ import {
   INTERLAY_COMPANY,
   INTERLAY_EMAIL,
   INTERLAY_DISCORD,
+  INTERLAY_LINKEDIN,
+  INTERLAY_MEDIUM,
   INTERLAY_GITHUB,
-  INTERLAY_TWITTER
+  POLKA_BTC_UI_GITHUB_ISSUES,
+  INTERLAY_TWITTER,
+  USER_FEEDBACK_FORM,
+  VAULT_FEEDBACK_FORM,
+  RELAYER_FEEDBACK_FORM,
+  POLKA_BTC_DOC_TREASURE_HUNT,
+  POLKA_BTC_DOC_TREASURE_HUNT_VAULT,
+  POLKA_BTC_DOC_TREASURE_HUNT_RELAYER,
+  GETTING_STARTED,
+  VAULTS_DOCS,
+  RELAYERS_DOCS,
+  DEVELOPERS_DOCS,
+  ROADMAP,
+  PRIVACY_POLICY,
+  NEWSLETTER_ACTION
 } from 'config/links';
+import styles from './footer.module.css';
 // TODO: should use an SVG
 import interlayImage from 'assets/img/interlay.png';
 // TODO: should use next-gen format
@@ -25,94 +37,282 @@ import web3FoundationImage from 'assets/img/polkabtc/web3-foundation-grants-badg
 
 const packageJson = require('../../../package.json');
 
-const Footer = () => (
-  <footer
-    className={clsx(
-      'flex',
-      'flex-col',
-      'items-center',
-      'px-3',
-      'py-8',
-      'text-sm',
-      'space-y-3'
-    )}>
-    <div className='space-x-6'>
-      <InterlayLink
-        href={INTERLAY_COMPANY}
-        target='_blank'
-        rel='noopener noreferrer'
-        style={{ display: 'inline-block' }}>
-        <InterlayImage
-          src={interlayImage}
-          width={128}
-          height={30}
-          alt='Interlay' />
-      </InterlayLink>
-      <InterlayLink
-        href={WEB3_FOUNDATION}
-        target='_blank'
-        rel='noopener noreferrer'
-        style={{ display: 'inline-block' }}>
-        <InterlayImage
-          src={web3FoundationImage}
-          width={116}
-          height={40}
-          alt='Web3 Foundation' />
-      </InterlayLink>
-    </div>
-    <div className='space-x-2'>
-      <InterlayLink
-        href={POLKA_BTC_UI_GITHUB}
-        target='_blank'
-        rel='noopener noreferrer'>
-        v&nbsp;{packageJson.version}
-      </InterlayLink>
-      <span>&copy;{getCurrentYear()} Interlay.</span>
-      <span>All Rights Reserved</span>
-      <span>|</span>
-      <InterlayLink
-        href='https://www.interlay.io/docs/privacy-policy.pdf'
-        target='_blank'
-        rel='noopener noreferrer'>
-        Privacy Policy
-      </InterlayLink>
-    </div>
-    <div className='space-x-8'>
-      <InterlayLink
-        href={INTERLAY_EMAIL}
-        target='_blank'
-        rel='noopener noreferrer'
-        data-placement='bottom'
-        data-original-title='Drop us an email'>
-        polkabtc@interlay.io
-      </InterlayLink>
-      <InterlayLink
-        href={INTERLAY_DISCORD}
-        target='_blank'
-        rel='noopener noreferrer'
-        data-placement='bottom'
-        data-original-title='Join our Discord channel'>
-        {/* TODO: could larger icons */}
-        <FaDiscord />
-      </InterlayLink>
-      <InterlayLink
-        href={INTERLAY_GITHUB}
-        target='_blank'
-        rel='noopener noreferrer'
-        data-placement='bottom'
-        data-original-title='Follow us on Github'>
-        <FaGithub />
-      </InterlayLink>
-      <InterlayLink
-        href={INTERLAY_TWITTER}
-        target='_blank'
-        rel='noopener noreferrer'
-        data-placement='bottom'
-        data-original-title='Follow us on Twitter'>
-        <FaTwitter />
-      </InterlayLink>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  const { t } = useTranslation();
+
+  return (
+    <footer className={styles['footer']}>
+      <div className={styles[`logos-container`]}>
+        <InterlayLink
+          href={INTERLAY_COMPANY}
+          target='_blank'
+          rel='noopener noreferrer'
+          style={{ display: 'inline-block' }}>
+          <InterlayImage
+            src={interlayImage}
+            width={150}
+            height={40}
+            alt='Interlay' />
+        </InterlayLink>
+        <div className={styles['padding-for-logo-2']}>
+          <InterlayLink
+            href={WEB3_FOUNDATION}
+            target='_blank'
+            rel='noopener noreferrer'
+            style={{ display: 'inline-block' }}>
+            <InterlayImage
+              src={web3FoundationImage}
+              width={150}
+              height={50}
+              alt='Web3 Foundation' />
+          </InterlayLink>
+        </div>
+      </div>
+      <div className={styles['grid-container']}>
+        <div
+          id='contact-container'
+          className={styles['center-container']}>
+          <div>
+            <div className={styles['title']}>{t('footer.contact')}</div>
+            <div className={styles['footer-items-container']}>
+              <ul>
+                <InterlayLink
+                  href={INTERLAY_DISCORD}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  data-placement='bottom'
+                  data-original-title='Join our Discord channel'>
+                  <li>Discord</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={INTERLAY_EMAIL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  data-placement='bottom'
+                  data-original-title='Drop us an email'>
+                  <li>Email</li>
+                </InterlayLink>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div
+          id='challenges-container'
+          className={styles['center-container']}>
+          <div>
+            <div className={styles['title']}>{t('footer.challenges')}</div>
+            <div className={styles['footer-items-container']}>
+              <ul>
+                <InterlayLink
+                  href={POLKA_BTC_DOC_TREASURE_HUNT}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.treasure_hunt')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={POLKA_BTC_DOC_TREASURE_HUNT_VAULT}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.vault_treasure')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={POLKA_BTC_DOC_TREASURE_HUNT_RELAYER}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.relayer_treasure')}</li>
+                </InterlayLink>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div
+          id='feedback-container'
+          className={styles['center-container']}>
+          <div>
+            <div className={styles['title']}>{t('footer.feedback')}</div>
+            <div className={styles['footer-items-container']}>
+              <ul>
+                <InterlayLink
+                  href={USER_FEEDBACK_FORM}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.user_feedback')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={VAULT_FEEDBACK_FORM}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.vault_feedback')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={RELAYER_FEEDBACK_FORM}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.relayer_feedback')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={POLKA_BTC_UI_GITHUB_ISSUES}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.open_issue')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={INTERLAY_DISCORD}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.discuss_discord')}</li>
+                </InterlayLink>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div
+          id='docs-container'
+          className={styles['center-container']}>
+          <div>
+            <div className={styles['title']}>{t('footer.docs')}</div>
+            <div className={styles['footer-items-container']}>
+              <ul>
+                <InterlayLink
+                  href={GETTING_STARTED}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.getting_started')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={VAULTS_DOCS}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.vaults_docs')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={RELAYERS_DOCS}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.Relayer_docs')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={DEVELOPERS_DOCS}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.developers')}</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={ROADMAP}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <li>{t('footer.roadmap')}</li>
+                </InterlayLink>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div
+          id='contact-container'
+          className={styles['center-container']}>
+          <div>
+            <div className={styles['title']}>{t('footer.follow_us')}</div>
+            <div className={styles['footer-items-container']}>
+              <ul>
+                <InterlayLink
+                  href={INTERLAY_TWITTER}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  data-placement='bottom'
+                  data-original-title='Follow us on Twitter'>
+                  <li>Twitter</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={INTERLAY_MEDIUM}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  data-placement='bottom'
+                  data-original-title='Follow us on Medium'>
+                  <li>Medium</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={INTERLAY_GITHUB}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  data-placement='bottom'
+                  data-original-title='Follow us on Github'>
+                  <li>Github</li>
+                </InterlayLink>
+                <InterlayLink
+                  href={INTERLAY_LINKEDIN}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  data-placement='bottom'
+                  data-original-title='Follow us on LinkedIn'>
+                  <li>LinkedIn</li>
+                </InterlayLink>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div
+          id='newsletter-container'
+          className={styles['footer-items-container']}>
+          <div>
+            <div className={styles['title']}>{t('footer.newsletter')}</div>
+            <ul>
+              <li>
+                <form
+                  action={NEWSLETTER_ACTION}
+                  method='post'
+                  id='mc-embedded-subscribe-form'
+                  name='mc-embedded-subscribe-form'
+                  className={styles['signup-container']}
+                  target='_blank'>
+                  <div id='mc_embed_signup_scroll'>
+                    <div className='input-group'>
+                      <input
+                        type='email'
+                        className={styles['newsletter-input']}
+                        placeholder=' Enter email here'
+                        name='EMAIL'
+                        id='mce-EMAIL' />
+                      <div>
+                        <button
+                          className={styles['newsletter-button']}
+                          type='submit'
+                          name='subscribe'
+                          id='mc-embedded-subscribe'>
+                          {t('footer.subscribe')}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </li>
+              <li>
+                <div className={styles['signup-text-container']}>{t('footer.join_newsletter')}</div>
+              </li>
+              <li>
+                <div className={styles['rights-container']}>
+                  <InterlayLink
+                    href={POLKA_BTC_UI_GITHUB}
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    v&nbsp;{packageJson.version}
+                  </InterlayLink>
+                  &nbsp;&#169; {getCurrentYear()} Interlay. {t('footer.rights_reserved')}
+                </div>
+                <li>
+                  <InterlayLink
+                    href={PRIVACY_POLICY}
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    {t('footer.read_our_policy')}
+                  </InterlayLink>
+                </li>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
