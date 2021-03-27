0c0f82903df473ac7446ff5640bbd2d8a1e76cc4
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-03-28 02:12:19 +0300

    chore: mark color naming idea

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -10,9 +10,12 @@ module.exports = {
         interlayYellow: {
           DEFAULT: '#f7931a'
         },
+        // ray test touch <
+        // TODO: could be `polkadotPink`
         interlayPink: {
           DEFAULT: '#e6007a'
         },
+        // ray test touch >
         interlayBlue: {
           DEFAULT: '#1c86ee'
         },
@@ -30,7 +33,13 @@ module.exports = {
         interlayGrey: {
           light: '#e9ecef', // TODO: could be gray-200 in the default theme
           DEFAULT: '#a9a9a9'
+        },
+        // ray test touch <
+        // TODO: could be `bitcoin`
+        interlayBitcoin: {
+          DEFAULT: '#ff9900'
         }
+        // ray test touch >
       }
     }
   },
