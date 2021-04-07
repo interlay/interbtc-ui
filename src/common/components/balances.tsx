3bf123461650797fc10fa4f10d532a533151bac6
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-04-08 01:20:09 +0300

    [fix] correct the styling of Balances

--- src/common/components/balances.tsx
+++ src/common/components/balances.tsx
@@ -1,6 +1,9 @@
 
+import clsx from 'clsx';
+
 import { safeRoundFiveDecimals } from 'common/utils/utils';
 import { ReactComponent as PolkaBTCLogoIcon } from 'assets/img/polkabtc/PolkaBTCLogo.svg';
+// TODO: should use SVG
 import polkadotLogo from 'assets/img/small-polkadot-logo.png';
 
 interface Props {
@@ -15,29 +18,39 @@ const Balances = ({
   const roundedBalanceDot = safeRoundFiveDecimals(balanceDOT);
 
   return (
-    <div>
-      <span className='btc-balance-wrapper'>
-        <span className=''>
-          <PolkaBTCLogoIcon
-            className='mr-1'
-            width={50}
-            height={30} />
-          <b>{balancePolkaBTC || '0'}</b>
-        </span>{' '}
-        PolkaBTC
-      </span>
-      <span className='dot-balance-wrapper'>
+    <div
+      className={clsx(
+        'flex',
+        'flex-wrap',
+        'space-x-2',
+        'mx-2' // TODO: should use space-x-{number} at upper level
+      )}>
+      <div
+        className={clsx(
+          'flex',
+          'items-center',
+          'space-x-2'
+        )}>
+        <PolkaBTCLogoIcon
+          width={50}
+          height={30} />
+        <span className='font-bold'>{balancePolkaBTC || '0'}</span>
+        <span>PolkaBTC</span>
+      </div>
+      <div
+        className={clsx(
+          'flex',
+          'items-center',
+          'space-x-1'
+        )}>
         <img
           src={polkadotLogo}
           width='20px'
           height='20px'
-          alt='polkadot logo'
-          className='mr-1' />
-        <span className=''>
-          <b>{roundedBalanceDot ?? '0'}</b>
-        </span>{' '}
-        DOT
-      </span>
+          alt='polkadot logo' />
+        <span className='font-bold'>{roundedBalanceDot ?? '0'}</span>
+        <span>DOT</span>
+      </div>
     </div>
   );
 };
