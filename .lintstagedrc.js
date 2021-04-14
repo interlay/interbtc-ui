d0521f027e48986c9a03b2f58a2c46030f40c429
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-04-14 12:39:04 +0300

    chore: clean up

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,6 @@
 
 module.exports = {
-  // ray test touch <
-  // './src/**/*.{ts,tsx,js,jsx}': 'eslint --fix ./src/**/*.{ts,tsx,js,jsx} --ignore-path .eslintignore .',
   './src/**/*.{ts,tsx,js,jsx}': 'yarn lint-fix',
-  // ray test touch >
   // TODO: double-check
-  // './src/**/*.{ts,tsx,scss}': 'prettier --config .prettierrc --write'
+  // './src/**/*.{ts,tsx,js,jsx,scss}': 'yarn format'
 };
