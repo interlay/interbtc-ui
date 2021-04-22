bf77dcaeb3db83e9823da8643aa1dfa3add58915
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-04-22 19:56:21 +0300

    feat: implement IssueRequestsTable

--- src/pages/dashboard/IssueRequests/index.tsx
+++ src/pages/dashboard/IssueRequests/index.tsx
@@ -2,37 +2,20 @@
 import {
   useState,
   useEffect,
-  ReactElement,
   useMemo
 } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import clsx from 'clsx';
 import { satToBTC } from '@interlay/polkabtc';
-import {
-  BtcNetworkName,
-  IssueColumns
-} from '@interlay/polkabtc-stats';
 
 import MainContainer from 'parts/MainContainer';
 import PageTitle from 'parts/PageTitle';
 import TimerIncrement from 'parts/TimerIncrement';
-import DashboardTable, {
-  StyledLinkData,
-  StatusComponent,
-  StatusCategories
-} from 'common/components/dashboard-table/dashboard-table';
+import IssueRequestsTable from 'containers/IssueRequestsTable';
 import LineChartComponent from '../components/line-chart-component';
 import usePolkabtcStats from 'common/hooks/use-polkabtc-stats';
 import { StoreType } from 'common/types/util.types';
-import { DashboardIssueInfo } from 'common/types/issue.types';
-import {
-  defaultTableDisplayParams,
-  shortAddress,
-  formatDateTimePrecise
-} from 'common/utils/utils';
-import * as constants from '../../../constants';
-import { BTC_ADDRESS_API } from 'config/blockchain';
 
 function IssueRequests(): JSX.Element {
   const {
@@ -41,12 +24,9 @@ function IssueRequests(): JSX.Element {
   } = useSelector((state: StoreType) => state.general);
   const { t } = useTranslation();
   const statsApi = usePolkabtcStats();
-  // eslint-disable-next-line no-array-constructor
-  const [issueRequests, setIssueRequests] = useState(new Array<DashboardIssueInfo>());
-  const [tableParams, setTableParams] = useState(defaultTableDisplayParams<IssueColumns>());
 
   const [totalSuccessfulIssues, setTotalSuccessfulIssues] = useState('-');
-  const [totalIssues, setTotalIssues] = useState('-');
+  const [totalIssueRequests, setTotalIssueRequests] = useState(0);
 
   // eslint-disable-next-line no-array-constructor
   const [cumulativeIssuesPerDay, setCumulativeIssuesPerDay] = useState(new Array<{ date: number; sat: number }>());
@@ -58,23 +38,6 @@ function IssueRequests(): JSX.Element {
     [cumulativeIssuesPerDay]
   );
 
-  const fetchIssueRequests = useMemo(
-    () => async () => {
-      const res = await statsApi.getIssues(
-        tableParams.page,
-        tableParams.perPage,
-        tableParams.sortBy,
-        tableParams.sortAsc,
-        constants.BITCOIN_NETWORK as BtcNetworkName // not sure why cast is necessary here, but TS complains
-      );
-      setIssueRequests(res.data);
-    },
-    [
-      tableParams,
-      statsApi
-    ]
-  );
-
   const [fetchTotalSuccessfulIssues, fetchTotalIssues] = useMemo(
     () => [
       async () => {
@@ -83,41 +46,10 @@ function IssueRequests(): JSX.Element {
       },
       async () => {
         const res = await statsApi.getTotalIssues();
-        setTotalIssues(res.data);
+        setTotalIssueRequests(Number(res.data));
       }
     ],
     [statsApi] // To silence the compiler
-  );
-
-  const tableHeadings = [
-    <h1 key={1}>{t('date')}</h1>,
-    <h1 key={2}>{t('issue_page.amount')}</h1>,
-    <h1 key={3}>{t('issue_page.parachain_block')}</h1>,
-    <h1 key={4}>{t('issue_page.vault_dot_address')}</h1>,
-    <h1 key={5}>{t('issue_page.vault_btc_address')}</h1>,
-    <h1 key={6}>{t('status')}</h1>
-  ];
-
-  const tableIssueRequestRow = useMemo(
-    () => (ireq: DashboardIssueInfo): ReactElement[] => [
-      <p key={1}>{formatDateTimePrecise(new Date(ireq.timestamp))}</p>,
-      <p key={2}>{ireq.amountBTC}</p>,
-      <p key={3}>{ireq.creation}</p>,
-      <p key={4}>{shortAddress(ireq.vaultDOTAddress)}</p>,
-      <StyledLinkData
-        key={5}
-        data={shortAddress(ireq.vaultBTCAddress)}
-        target={BTC_ADDRESS_API + ireq.vaultBTCAddress}
-        newTab={true} />,
-      <StatusComponent
-        key={6}
-        {...(ireq.completed ?
-          { text: t('completed'), category: StatusCategories.Ok } :
-          ireq.cancelled ?
-            { text: t('cancelled'), category: StatusCategories.Bad } :
-            { text: t('pending'), category: StatusCategories.Warning })} />
-    ],
-    [t]
   );
 
   const fetchIssuesLastDays = useMemo(
@@ -130,14 +62,11 @@ function IssueRequests(): JSX.Element {
 
   useEffect(() => {
     try {
-      fetchIssueRequests();
       fetchIssuesLastDays();
     } catch (error) {
       console.error('[IssueRequests useEffect] error.message => ', error.message);
     }
   }, [
-    fetchIssueRequests,
-    tableParams,
     fetchIssuesLastDays
   ]);
 
@@ -252,23 +181,7 @@ function IssueRequests(): JSX.Element {
               ]} />
           </div>
         </div>
-        <div>
-          <h2
-            className={clsx(
-              'text-2xl',
-              'font-bold'
-            )}>
-            {t('issue_page.recent_requests')}
-          </h2>
-          <DashboardTable
-            richTable={true}
-            pageData={issueRequests}
-            totalPages={Math.ceil(Number(totalIssues) / tableParams.perPage)}
-            tableParams={tableParams}
-            setTableParams={setTableParams}
-            headings={tableHeadings}
-            dataPointDisplayer={tableIssueRequestRow} />
-        </div>
+        <IssueRequestsTable totalIssueRequests={totalIssueRequests} />
       </div>
     </MainContainer>
   );
