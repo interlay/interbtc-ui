9ba043de1dc92c3a9494d3731e84e2b23013adcc
Chanakya888 <samuraichanakya1@gmail.com>
2021-04-27 09:47:16 +0530

    chore: removed Last updated minute ago condition

--- src/parts/TimerIncrement/index.tsx
+++ src/parts/TimerIncrement/index.tsx
@@ -4,9 +4,6 @@ import { useState } from 'react';
 import useInterval from 'utils/hooks/use-interval';
 
 const formatTime = (seconds: number): string => {
-  if (seconds > 60) {
-    return `Last updated minutes ago`;
-  }
   return `Last updated ${seconds} seconds ago`;
 };
 
