e62d6f36a60d822858899b82d03b1b5d68be2ca1
Chanakya888 <samuraichanakya1@gmail.com>
2021-06-14 00:30:46 +0530

    feat: Balances as dropdown

--- src/common/components/balances.tsx
+++ src/common/components/balances.tsx
@@ -1,54 +1,120 @@
 
+import * as React from 'react';
 import clsx from 'clsx';
 
 import { safeRoundFiveDecimals } from 'common/utils/utils';
 import { ReactComponent as PolkabtcLogoIcon } from 'assets/img/polkabtc-logo.svg';
 import { ReactComponent as PolkadotLogoIcon } from 'assets/img/polkadot-logo.svg';
+import { ReactComponent as DropDownIcon } from 'assets/img/icons/drop-down.svg';
 
 interface Props {
   balancePolkaBTC?: string;
   balanceDOT?: string;
 }
 
-const Balances = ({
-  balancePolkaBTC,
-  balanceDOT
-}: Props): JSX.Element => {
+const Balances = ({ balancePolkaBTC, balanceDOT }: Props): JSX.Element => {
   const roundedBalanceDot = safeRoundFiveDecimals(balanceDOT);
+  const [dropDownStatus, setDropDownStatus] = React.useState(false);
 
+  const DROPDOWN_ITEMS = [
+    {
+      currency: 'DOT',
+      amount: roundedBalanceDot,
+      logo: <PolkadotLogoIcon
+        height={20}
+        width={20} />
+    }
+  ];
   return (
     <div
       className={clsx(
-        'flex',
-        'flex-wrap',
-        'space-x-2',
-        'mx-2' // TODO: should use space-x-{number} at upper level
+        'mx-4', // TODO: should use space-x-{number} at upper level
+        'max-w-max'
       )}>
       <div
         className={clsx(
+          'relative',
           'flex',
           'items-center',
-          'space-x-2'
-        )}>
-        <PolkabtcLogoIcon
-          fill='currentColor'
-          width={50}
-          height={30} />
-        <span className='font-bold'>{balancePolkaBTC || '0'}</span>
-        <span>PolkaBTC</span>
-      </div>
-      <div
-        className={clsx(
-          'flex',
-          'items-center',
-          'space-x-1'
-        )}>
-        <PolkadotLogoIcon
+          'cursor-pointer'
+        )}
+        onClick={() => {
+          setDropDownStatus(!dropDownStatus);
+        }}>
+        <div
+          className={clsx(
+            'flex',
+            'items-center',
+            'space-x-2',
+            'px-2'
+          )}>
+          <div
+            className={clsx(
+              'flex',
+              'items-center',
+              'space-x-2'
+            )}>
+            <span className='text-lg'>{balancePolkaBTC || '0'}</span>
+            <div
+              className={clsx(
+                'flex',
+                'items-center',
+                'space-x-2'
+              )}>
+              <PolkabtcLogoIcon
+                fill='currentColor'
+                width={50}
+                height={30} />
+              <span className='text-gray-500'>InterBTC</span>
+            </div>
+          </div>
+        </div>
+        <DropDownIcon
           width={20}
           height={20} />
-        <span className='font-bold'>{roundedBalanceDot ?? '0'}</span>
-        <span>DOT</span>
+        {dropDownStatus && (
+          <div
+            className={clsx(
+              'm-1',
+              'flex-col',
+              'absolute',
+              'top-full',
+              'w-full',
+              'bg-gray-100',
+              'left-0'
+            )}>
+            {DROPDOWN_ITEMS.map(dropdownItem => (
+              <div
+                className={clsx(
+                  'my-5',
+                  'px-2'
+                )}
+                key={dropdownItem.currency}>
+                <div
+                  className={clsx(
+                    'flex',
+                    'justify-between',
+                    'items-center',
+                    'space-x-1'
+                  )}>
+                  <span className='text-lg'>{dropdownItem.amount ?? '0'}</span>
+                  <div className='flex'>
+                    {dropdownItem.logo}
+                    <span
+                      className={clsx(
+                        'text-gray-500',
+                        'ml-1'
+                      )}>
+                      {dropdownItem.currency}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
+
     </div>
   );
 };
