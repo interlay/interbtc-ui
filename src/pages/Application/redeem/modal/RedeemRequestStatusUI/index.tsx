ac6844c2b91327929e43744f340bab031c2d6004
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-06-27 07:09:11 +0300

    refactor: componentize with DefaultRedeemRequest

--- src/pages/Application/redeem/modal/RedeemRequestStatusUI/index.tsx
+++ src/pages/Application/redeem/modal/RedeemRequestStatusUI/index.tsx
@@ -1,20 +1,13 @@
 
-import * as React from 'react';
-import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
-import clsx from 'clsx';
-
-import RequestWrapper from 'pages/Application/RequestWrapper';
 import CompletedRedeemRequest from './CompletedRedeemRequest';
 import ReimbursedRedeemRequest from './ReimbursedRedeemRequest';
 import RetriedRedeemRequest from './RetriedRedeemRequest';
 import PendingWithBtcTxNotFoundRedeemRequest from './PendingWithBtcTxNotFoundRedeemRequest';
-import { shortAddress } from 'common/utils/utils';
+import DefaultRedeemRequest from './DefaultRedeemRequest';
 import {
   RedeemRequest,
   RedeemRequestStatus
 } from 'common/types/redeem.types';
-import { StoreType } from 'common/types/util.types';
 
 interface Props {
   request: RedeemRequest;
@@ -23,28 +16,6 @@ interface Props {
 const RedeemRequestStatusUI = ({
   request
 }: Props): JSX.Element => {
-  const { t } = useTranslation();
-  const { polkaBtcLoaded } = useSelector((state: StoreType) => state.general);
-  const [stableBitcoinConfirmations, setStableBitcoinConfirmations] = React.useState(1);
-
-  React.useEffect(() => {
-    if (!polkaBtcLoaded) return;
-
-    (async () => {
-      try {
-        const theStableBitcoinConfirmations = await window.polkaBTC.btcRelay.getStableBitcoinConfirmations();
-
-        setStableBitcoinConfirmations(theStableBitcoinConfirmations);
-      } catch (error) {
-        // TODO: should add error handling UX
-        console.log('[RedeemRequestStatusUI useEffect] error.message => ', error.message);
-      }
-    })();
-  }, [
-    request,
-    polkaBtcLoaded
-  ]);
-
   function getStatus(status: RedeemRequestStatus): JSX.Element {
     switch (status) {
     case RedeemRequestStatus.Completed:
@@ -56,48 +27,7 @@ const RedeemRequestStatusUI = ({
     case RedeemRequestStatus.Retried:
       return <RetriedRedeemRequest request={request} />;
     default:
-      return (
-        <RequestWrapper>
-          <h2
-            className={clsx(
-              'text-3xl',
-              'font-medium'
-            )}>
-            {t('received')}
-          </h2>
-          <div
-            className={clsx(
-              'w-48',
-              'h-48',
-              'ring-4',
-              'ring-interlayTreePoppy',
-              'rounded-full',
-              'inline-flex',
-              'flex-col',
-              'items-center',
-              'justify-center'
-            )}>
-            <span className='mt-4'>
-              {t('redeem_page.waiting_for')}
-            </span>
-            <span>
-              {t('confirmations')}
-            </span>
-            <span
-              className={clsx(
-                'text-2xl',
-                'text-interlayMalachite',
-                'font-medium'
-              )}>
-              {`${request.confirmations}/${stableBitcoinConfirmations}`}
-            </span>
-          </div>
-          <p className='space-x-1'>
-            <span className='text-textSecondary'>{t('issue_page.btc_transaction')}:</span>
-            <span className='font-medium'>{shortAddress(request.btcTxId)}</span>
-          </p>
-        </RequestWrapper>
-      );
+      return <DefaultRedeemRequest request={request} />;
     }
   }
 
