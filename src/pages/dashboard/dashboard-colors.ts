b93b9a2437adfc08ed330df312abd249fbb6e257
anton-karlovskiy <antonkarlovskiy@outlook.com>
2021-07-05 13:01:41 +0300

    feat: update SCSS color variables

--- src/pages/dashboard/dashboard-colors.ts
+++ src/pages/dashboard/dashboard-colors.ts
@@ -3,40 +3,35 @@ export interface Style {
   filter?: string;
 }
 
+// ray test touch <<
 export function getAccents(color: string): Style {
   const accent = { color: '#000000', filter: 'none' };
   switch (color) {
   case 'd_yellow':
-    accent.color = '#f7931a';
-    accent.filter = 'invert(77%) sepia(49%) saturate(5098%) hue-rotate(350deg) brightness(102%) contrast(94%)';
+    accent.color = '#ff9900';
     break;
   case 'd_pink':
-    accent.color = '#e6007a';
-    accent.filter = 'invert(16%) sepia(99%) saturate(6939%) hue-rotate(321deg) brightness(90%) contrast(102%)';
+    accent.color = '#075abc';
     break;
   case 'd_blue':
-    accent.color = '#1c86ee';
-    accent.filter = 'invert(39%) sepia(98%) saturate(1790%) hue-rotate(192deg) brightness(96%) contrast(95%)';
+    accent.color = '#075abc';
     break;
   case 'd_orange':
-    accent.color = '#f95738';
-    accent.filter = 'invert(44%) sepia(57%) saturate(2809%) hue-rotate(339deg) brightness(99%) contrast(97%)';
+    accent.color = '#db5aad';
     break;
   case 'd_green':
-    accent.color = '#4ac948';
-    accent.filter = 'invert(68%) sepia(64%) saturate(530%) hue-rotate(66deg) brightness(89%) contrast(85%)';
+    accent.color = '#a2e75e';
     break;
   case 'd_red':
-    accent.color = '#f32013';
+    accent.color = '#ef4444';
     break;
   case 'd_white':
     accent.color = '#ffffff';
-    accent.filter = 'invert(100%) sepia(3%) saturate(3%) hue-rotate(71deg) brightness(106%) contrast(100%)';
     break;
   default:
-    accent.color = '#a9a9a9';
-    accent.filter = 'invert(82%) sepia(7%) saturate(9%) hue-rotate(142deg) brightness(83%) contrast(85%)';
+    accent.color = '#6b7280';
     break;
   }
   return accent;
 }
+// ray test touch >>
