0f29bc1756b48925b754e1cf89a947c36c5aa4a2
crypto-engineer <anton@interlay.io>
2021-10-13 10:29:35 +0300

    feat: set up storybook dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,46 @@
 
-import '../src/index.css';
+import { themes } from '@storybook/theming';
+import clsx from 'clsx';
 
-export const parameters = {
+import '../src/index.css';
+import { CLASS_NAMES } from '../src/utils/constants/styles';
+import {
+  POLKADOT,
+  KUSAMA
+} from '../src/utils/constants/relay-chain-names';
+
+const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       date: /Date$/
     }
+  },
+  darkMode: {
+    stylePreview: true,
+    current: CLASS_NAMES.LIGHT,
+    // Override the default dark theme
+    dark: { ...themes.dark },
+    // Override the default light theme
+    light: { ...themes.normal },
+    darkClass: CLASS_NAMES.DARK,
+    lightClass: CLASS_NAMES.LIGHT
   }
+};
+
+const decorators = [
+  Story => (
+    <div
+      className={clsx(
+        { 'text-interlayPrimaryInLightMode': process.env.REACT_APP_RELAY_CHAIN_NAME === POLKADOT },
+        { 'dark:text-kintsugiPrimaryInDarkMode': process.env.REACT_APP_RELAY_CHAIN_NAME === KUSAMA }
+      )}>
+      <Story />
+    </div>
+  )
+];
+
+export {
+  parameters,
+  decorators
 };
