4b9adce661aa8d12f5c2d8d3d194d18f2a6241de
crypto-engineer <anton@interlay.io>
2021-10-20 19:10:47 +0300

    refactor: replace with InterlayTabGroup in Bridge

--- src/pages/Bridge/index.tsx
+++ src/pages/Bridge/index.tsx
@@ -10,10 +10,12 @@ import RedeemForm from './RedeemForm';
 import TransferForm from './TransferForm';
 import BurnForm from './BurnForm';
 import MainContainer from 'parts/MainContainer';
-import Tabs, {
-  Tab,
-  TabPanel
-} from 'components/Tabs';
+import InterlayTabGroup, {
+  InterlayTabList,
+  InterlayTabPanels,
+  InterlayTab,
+  InterlayTabPanel
+} from 'components/UI/InterlayTabGroup';
 import { COLLATERAL_TOKEN } from 'config/relay-chains';
 import { KUSAMA } from 'utils/constants/relay-chain-names';
 import useQueryParams from 'utils/hooks/use-query-params';
@@ -99,7 +101,7 @@ const Bridge = (): JSX.Element | null => {
 
   const selectedTabIndex = TAB_ITEMS.findIndex(tabItem => tabItem.id === selectedTabId);
 
-  const handleTabSelect = (index: number) => () => {
+  const handleTabSelect = (index: number) => {
     updateQueryParameters({
       [QUERY_PARAMETERS.TAB]: TAB_ITEMS[index].id
     });
@@ -118,76 +120,43 @@ const Bridge = (): JSX.Element | null => {
           'p-10',
           'md:rounded-lg'
         )}>
-        {/* ray test touch << */}
-        <>
-          <Tabs
-            className={clsx(
-              'grid',
-              { 'grid-cols-3': TAB_ITEMS.length === 3 },
-              { 'grid-cols-4': TAB_ITEMS.length === 4 },
-              'rounded-lg',
-              'bg-interlayPaleSky-200'
-            )}>
-            {TAB_ITEMS.map((tabItem, index) => {
-              const selected = selectedTabIndex === index;
-
-              return (
-                <Tab
-                  anchorClassName={clsx(
-                    'font-medium',
-                    'px-4',
-                    'py-2.5',
-                    'uppercase',
-                    selected ?
-                      clsx(
-                        'rounded-lg',
-                        'text-white',
-                        'transition',
-                        'bg-interlayDenim'
-                      ) : 'opacity-30'
-                  )}
-                  key={tabItem.id}
-                  id={tabItem.id}
-                  onSelect={handleTabSelect(index)}>
-                  {t(tabItem.label)}
-                </Tab>
-              );
-            })}
-          </Tabs>
+        <InterlayTabGroup
+          defaultIndex={selectedTabIndex}
+          onChange={index => {
+            handleTabSelect(index);
+          }}>
+          <InterlayTabList>
+            {TAB_ITEMS.map((tabItem => (
+              <InterlayTab
+                key={tabItem.id}
+                className='uppercase'>
+                {t(tabItem.label)}
+              </InterlayTab>
+            )))}
+          </InterlayTabList>
           <hr
             className={clsx(
               'border-t-2',
               'my-2',
               'border-interlayDenim'
             )} />
-        </>
-        <TabPanel
-          index={0}
-          selectedIndex={selectedTabIndex}
-          id={TAB_IDS.issue}>
-          <IssueForm />
-        </TabPanel>
-        <TabPanel
-          index={1}
-          selectedIndex={selectedTabIndex}
-          id={TAB_IDS.redeem}>
-          <RedeemForm />
-        </TabPanel>
-        <TabPanel
-          index={2}
-          selectedIndex={selectedTabIndex}
-          id={TAB_IDS.transfer}>
-          <TransferForm />
-        </TabPanel>
-        {burnable && (
-          <TabPanel
-            index={3}
-            selectedIndex={selectedTabIndex}
-            id={TAB_IDS.burn}>
-            <BurnForm />
-          </TabPanel>
-        )}
-        {/* ray test touch >> */}
+          <InterlayTabPanels className='mt-2'>
+            <InterlayTabPanel>
+              <IssueForm />
+            </InterlayTabPanel>
+            <InterlayTabPanel>
+              <RedeemForm />
+            </InterlayTabPanel>
+            <InterlayTabPanel>
+              <TransferForm />
+            </InterlayTabPanel>
+            {burnable && (
+              <InterlayTabPanel>
+                <BurnForm />
+              </InterlayTabPanel>
+            )}
+          </InterlayTabPanels>
+        </InterlayTabGroup>
       </div>
     </MainContainer>
   );
