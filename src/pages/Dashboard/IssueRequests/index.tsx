3c41a22c6ef26845963e63218b0886db64c46d12
crypto-engineer <anton@interlay.io>
2021-11-03 06:55:56 +0300

    feat: integrate react-query into IssueRequests

--- src/pages/Dashboard/IssueRequests/index.tsx
+++ src/pages/Dashboard/IssueRequests/index.tsx
@@ -1,16 +1,19 @@
-import {
-  useState,
-  useEffect
-} from 'react';
+
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import clsx from 'clsx';
+import {
+  useErrorHandler,
+  withErrorBoundary
+} from 'react-error-boundary';
+import { useQuery } from 'react-query';
 import { BitcoinAmount } from '@interlay/monetary-js';
 
 import LineChartComponent from '../components/line-chart-component';
 import PageTitle from 'parts/PageTitle';
 import TimerIncrement from 'parts/TimerIncrement';
 import IssueRequestsTable from 'containers/IssueRequestsTable';
+import ErrorFallback from 'components/ErrorFallback';
 import Hr1 from 'components/hrs/Hr1';
 import { WRAPPED_TOKEN_SYMBOL } from 'config/relay-chains';
 import {
@@ -21,60 +24,106 @@ import {
   displayMonetaryAmount,
   getUsdAmount
 } from 'common/utils/utils';
-import useInterbtcIndex from 'common/hooks/use-interbtc-index';
+import genericFetcher, {
+  GENERIC_FETCHER
+} from 'services/fetchers/generic-fetcher';
 import { StoreType } from 'common/types/util.types';
+
+// TODO: should be imported
+interface BTCTimeData {
+  date: Date;
+  btc: BitcoinAmount;
+}
 
 function IssueRequests(): JSX.Element {
   const {
     totalWrappedTokenAmount,
-    prices
+    prices,
+    bridgeLoaded
   } = useSelector((state: StoreType) => state.general);
   const { t } = useTranslation();
-  const statsApi = useInterbtcIndex();
 
-  const [totalSuccessfulIssues, setTotalSuccessfulIssues] = useState('-');
-  const [totalIssueRequests, setTotalIssueRequests] = useState(0);
-
-  // eslint-disable-next-line no-array-constructor
-  const [cumulativeIssuesPerDay, setCumulativeIssuesPerDay] = useState(new Array<{ date: number; sat: number; }>());
-
-  useEffect(() => {
-    if (!statsApi) return;
-
-    try {
-      (() => async () => {
-        const res = await statsApi.getRecentDailyIssues({ daysBack: 6 });
-        setCumulativeIssuesPerDay(res);
-      })();
-      (async () => {
-        const res = await statsApi.getTotalSuccessfulIssues();
-        if (res) setTotalSuccessfulIssues(res.toString());
-      })();
-      (async () => {
-        const res = await statsApi.getTotalIssues();
-        setTotalIssueRequests(res);
-      })();
-    } catch (error) {
-      console.error('[IssueRequests useEffect] error.message => ', error.message);
+  const {
+    isIdle: cumulativeIssuesPerDayIdle,
+    isLoading: cumulativeIssuesPerDayLoading,
+    data: cumulativeIssuesPerDay,
+    error: cumulativeIssuesPerDayError
+  } = useQuery<Array<BTCTimeData>, Error>(
+    [
+      GENERIC_FETCHER,
+      'interBtcIndex',
+      'getRecentDailyIssues',
+      { daysBack: 6 }
+    ],
+    genericFetcher<Array<BTCTimeData>>(),
+    {
+      enabled: !!bridgeLoaded
     }
-  }, [statsApi]);
+  );
+  useErrorHandler(cumulativeIssuesPerDayError);
 
-  const pointIssuesPerDay = cumulativeIssuesPerDay.map((dataPoint, index) => {
-    if (index === 0) {
-      return 0;
-    } else {
-      return dataPoint.sat - cumulativeIssuesPerDay[index - 1].sat;
+  const {
+    isIdle: totalSuccessfulIssuesIdle,
+    isLoading: totalSuccessfulIssuesLoading,
+    data: totalSuccessfulIssues,
+    error: totalSuccessfulIssuesError
+  } = useQuery<number, Error>(
+    [
+      GENERIC_FETCHER,
+      'interBtcIndex',
+      'getTotalSuccessfulIssues'
+    ],
+    genericFetcher<number>(),
+    {
+      enabled: !!bridgeLoaded
     }
-  });
+  );
+  useErrorHandler(totalSuccessfulIssuesError);
 
-  return (
-    <>
-      <div>
-        <PageTitle
-          mainTitle={t('issue_page.issue_requests')}
-          subTitle={<TimerIncrement />} />
-        <Hr1 className='mt-2' />
-      </div>
+  const {
+    isIdle: totalIssuesIdle,
+    isLoading: totalIssuesLoading,
+    data: totalIssues,
+    error: totalIssuesError
+  } = useQuery<number, Error>(
+    [
+      GENERIC_FETCHER,
+      'interBtcIndex',
+      'getTotalIssues'
+    ],
+    genericFetcher<number>(),
+    {
+      enabled: !!bridgeLoaded
+    }
+  );
+  useErrorHandler(totalIssuesError);
+
+  const renderUpperContent = () => {
+    // TODO: should use skeleton loaders
+    if (cumulativeIssuesPerDayIdle || cumulativeIssuesPerDayLoading) {
+      return <>Loading...</>;
+    }
+    if (totalSuccessfulIssuesIdle || totalSuccessfulIssuesLoading) {
+      return <>Loading...</>;
+    }
+    if (cumulativeIssuesPerDay === undefined) {
+      throw new Error('Something went wrong!');
+    }
+
+    const converted = cumulativeIssuesPerDay.map(item => ({
+      date: item.date.getTime(),
+      sat: Number(item.btc.toString())
+    }));
+
+    const pointIssuesPerDay = converted.map((dataPoint, i) => {
+      if (i === 0) {
+        return 0;
+      } else {
+        return dataPoint.sat - converted[i - 1].sat;
+      }
+    });
+
+    return (
       <div
         className={clsx(
           'sm:flex',
@@ -122,7 +171,7 @@ function IssueRequests(): JSX.Element {
             {t('dashboard.issue.issue_requests')}
           </h5>
           <h5>
-            {totalSuccessfulIssues === '-' ? t('no_data') : totalSuccessfulIssues}
+            {totalSuccessfulIssues}
           </h5>
         </div>
         <div
@@ -131,6 +180,8 @@ function IssueRequests(): JSX.Element {
             'rounded',
             'sm:flex-1'
           )}>
+          {/* ray test touch < */}
+          {/* TODO: create a component */}
           <LineChartComponent
             color={[
               'd_interlayDenim',
@@ -145,7 +196,7 @@ function IssueRequests(): JSX.Element {
               })
             ]}
             yLabels={
-              cumulativeIssuesPerDay
+              converted
                 .slice(1)
                 .map(dataPoint => new Date(dataPoint.date).toISOString().substring(0, 10))
             }
@@ -161,16 +212,51 @@ function IssueRequests(): JSX.Element {
               }
             ]}
             data={[
-              cumulativeIssuesPerDay
+              converted
                 .slice(1)
                 .map(dataPoint => Number(BitcoinAmount.from.Satoshi(dataPoint.sat).str.BTC())),
               pointIssuesPerDay.slice(1).map(sat => Number(BitcoinAmount.from.Satoshi(sat).str.BTC()))
             ]} />
+          {/* ray test touch > */}
         </div>
       </div>
-      <IssueRequestsTable totalIssueRequests={totalIssueRequests} />
+    );
+  };
+
+  const renderLowerContent = () => {
+    // TODO: should use skeleton loaders
+    if (totalIssuesIdle || totalIssuesLoading) {
+      return <>Loading...</>;
+    }
+    if (totalIssues === undefined) {
+      throw new Error('Something went wrong!');
+    }
+
+    return (
+      <IssueRequestsTable totalIssueRequests={totalIssues} />
+    );
+  };
+
+  return (
+    <>
+      <div>
+        <PageTitle
+          mainTitle={t('issue_page.issue_requests')}
+          subTitle={<TimerIncrement />} />
+        <Hr1 className='mt-2' />
+      </div>
+      {/* ray test touch < */}
+      {/* TODO: create a component */}
+      {renderUpperContent()}
+      {renderLowerContent()}
+      {/* ray test touch > */}
     </>
   );
 }
 
-export default IssueRequests;
+export default withErrorBoundary(IssueRequests, {
+  FallbackComponent: ErrorFallback,
+  onReset: () => {
+    window.location.reload();
+  }
+});
