28151eb71993f45b7238f3f1d9233e8dd4d78358
Tom Jeatt <hello@tomjeatt.com>
2021-11-24 16:29:57 +0000

    feat: add governance token and dropdown to topbar

--- src/parts/Sidebar/SidebarContent/index.tsx
+++ src/parts/Sidebar/SidebarContent/index.tsx
@@ -16,7 +16,7 @@ import {
   KUSAMA
 } from 'utils/constants/relay-chain-names';
 import { PAGES } from 'utils/constants/links';
-import { ReactComponent as InterlayLogoIcon } from 'assets/img/interlay-logo.svg';
+import { ReactComponent as InterlayLogoIcon } from 'assets/img/interlay-logo-with-text.svg';
 
 const DarkModeToggle = React.lazy(() =>
   import(/* webpackChunkName: 'dark-mode-toggle' */ 'containers/DarkModeToggle')
