47d06e459b3440d253350de995677f2fde0bf633
Thomas Jeatt <hello@tomjeatt.com>
2021-12-06 08:57:18 +0000

    refactor: move balance selector UI to own component and fix initial balance bug.

--- src/parts/Topbar/Balances/index.tsx
+++ src/parts/Topbar/Balances/index.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 import {
   BitcoinAmount,
   MonetaryAmount,
@@ -10,14 +9,6 @@ import {
   CurrencyUnit
 } from '@interlay/interbtc-api';
 
-import Select, {
-  SelectButton,
-  SelectOptions,
-  SelectOption,
-  SelectBody,
-  SelectCheck,
-  SelectText
-} from 'components/Select';
 import {
   WRAPPED_TOKEN_SYMBOL,
   WrappedTokenLogoIcon,
@@ -28,6 +19,7 @@ import {
 } from 'config/relay-chains';
 import { displayMonetaryAmount } from 'common/utils/utils';
 import { TokenType } from 'common/types/util.types';
+import BalanceSelector from './BalanceSelector';
 
 interface Props {
   wrappedTokenBalance?: BitcoinAmount;
@@ -48,108 +40,40 @@ const Balances = ({
   collateralTokenBalance,
   governanceTokenBalance
 }: Props): JSX.Element => {
-  const tokenOptions: Array<TokenOption> = [
-    {
-      type: TokenType.COLLATERAL,
-      balance: displayMonetaryAmount(collateralTokenBalance),
-      icon: <CollateralTokenLogoIcon
-        width={26} />,
-      symbol: COLLATERAL_TOKEN_SYMBOL
-    },
-    {
-      type: TokenType.WRAPPED,
-      balance: displayMonetaryAmount(wrappedTokenBalance),
-      icon: <WrappedTokenLogoIcon
-        width={26} />,
-      symbol: WRAPPED_TOKEN_SYMBOL
-    },
-    {
-      type: TokenType.GOVERNANCE,
-      balance: displayMonetaryAmount(governanceTokenBalance),
-      icon: <GovernanceTokenLogoIcon
-        width={26} />,
-      symbol: GOVERNANCE_TOKEN_SYMBOL
-    }
-  ];
+  const [tokenOptions, setTokenOptions] = useState<Array<TokenOption> | undefined>(undefined);
 
-  const getTokenOption = (type: TokenType) => tokenOptions.find(token => token.type === type);
+  useEffect(() => {
+    const tokenOptions: Array<TokenOption> = [
+      {
+        type: TokenType.COLLATERAL,
+        balance: displayMonetaryAmount(collateralTokenBalance),
+        icon: <CollateralTokenLogoIcon
+          width={26} />,
+        symbol: COLLATERAL_TOKEN_SYMBOL
+      },
+      {
+        type: TokenType.WRAPPED,
+        balance: displayMonetaryAmount(wrappedTokenBalance),
+        icon: <WrappedTokenLogoIcon
+          width={26} />,
+        symbol: WRAPPED_TOKEN_SYMBOL
+      },
+      {
+        type: TokenType.GOVERNANCE,
+        balance: displayMonetaryAmount(governanceTokenBalance),
+        icon: <GovernanceTokenLogoIcon
+          width={26} />,
+        symbol: GOVERNANCE_TOKEN_SYMBOL
+      }
+    ];
 
-  const [currentToken, setCurrentToken] = useState<TokenOption | undefined>(getTokenOption(TokenType.COLLATERAL));
-
-  const handleSelectToken = (selectedToken: TokenType) => {
-    setCurrentToken(getTokenOption(selectedToken));
-  };
+    setTokenOptions(tokenOptions);
+  }, [collateralTokenBalance, governanceTokenBalance, wrappedTokenBalance]);
 
   return (
-    <div
-      className={clsx(
-        'flex',
-        'space-x-2'
-      )}>
-      {currentToken &&
-        <Select
-          value={currentToken.type}
-          onChange={handleSelectToken}>
-          {({ open }) => (
-            <>
-              <SelectBody
-                className={clsx(
-                  'w-44'
-                )}>
-                <SelectButton>
-                  <span
-                    className={clsx(
-                      'flex',
-                      'items-center',
-                      'space-x-3'
-                    )}>
-                    {currentToken.icon}
-                    <SelectText>
-                      {currentToken.balance} {currentToken.symbol}
-                    </SelectText>
-                  </span>
-                </SelectButton>
-                <SelectOptions open={open}>
-                  {tokenOptions.map((tokenOption: TokenOption) => {
-                    return (
-                      <SelectOption
-                        key={tokenOption.type}
-                        value={tokenOption.type}>
-                        {({
-                          selected,
-                          active
-                        }) => {
-                          console.log(selected);
-                          return (
-                            <>
-                              <div
-                                className={clsx(
-                                  'flex',
-                                  'items-center',
-                                  'space-x-3'
-                                )}>
-                                {tokenOption.icon}
-                                <SelectText selected={selected}>
-                                  {tokenOption.balance} {tokenOption.symbol}
-                                </SelectText>
-                              </div>
-                              {selected ? (
-                                <SelectCheck
-                                  active={active} />
-                              ) : null}
-                            </>
-                          );
-                        }}
-                      </SelectOption>
-                    );
-                  })}
-                </SelectOptions>
-              </SelectBody>
-            </>
-          )}
-        </Select>
-      }
-    </div>
+    <>
+      {tokenOptions ? <BalanceSelector tokenOptions={tokenOptions} /> : null}
+    </>
   );
 };
 
