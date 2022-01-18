cb4b36b21e0f0400a53f83c7ee5b943f381b2f83
Thomas Jeatt <hello@tomjeatt.com>
2022-01-18 15:44:56 +0000

    refactor: remove redundant query parameters

--- src/pages/Transfer/index.tsx
+++ src/pages/Transfer/index.tsx
@@ -5,16 +5,8 @@ import clsx from 'clsx';
 import TransferForm from './TransferForm';
 import MainContainer from 'parts/MainContainer';
 import Panel from 'components/Panel';
-import useUpdateQueryParameters, { QueryParameters } from 'utils/hooks/use-update-query-parameters';
 
 const Transfer = (): JSX.Element | null => {
-  const updateQueryParameters = useUpdateQueryParameters();
-
-  const updateQueryParametersRef = React.useRef<(newQueryParameters: QueryParameters) => void>();
-  // MEMO: inspired by https://epicreact.dev/the-latest-ref-pattern-in-react/
-  React.useLayoutEffect(() => {
-    updateQueryParametersRef.current = updateQueryParameters;
-  });
   return (
     <MainContainer>
       <Panel
