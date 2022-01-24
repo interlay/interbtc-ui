6ab6b97d7e3029a7614b3a7213f12bf5dbceed60
crypto-engineer <anton@interlay.io>
2022-01-24 13:29:11 +0300

    feat: try a simplier approach

--- src/components/NumberInput/index.tsx
+++ src/components/NumberInput/index.tsx
@@ -3,41 +3,18 @@ import * as React from 'react';
 import InterlayInput, { Props as InterlayInputProps } from 'components/UI/InterlayInput';
 
 type Ref = HTMLInputElement;
-const NumberInput = React.forwardRef<Ref, InterlayInputProps>((props, ref): JSX.Element => {
-  // `onWheel` prop can't be used with `preventDefault` because
-  // React implements passive event listeners.
-  const disableChangeOnWheel = (event: MouseEvent) => {
-    event.preventDefault();
-  };
-
-  const inputParent = React.useRef<HTMLDivElement | null>(null);
-
-  React.useEffect(() => {
-    if (!inputParent || !inputParent.current) return;
-
-    const currentInputParent = inputParent.current;
-
-    currentInputParent.addEventListener('wheel', (event: MouseEvent) =>
-      disableChangeOnWheel(event), { passive: false });
-
-    return () => {
-      currentInputParent.removeEventListener('wheel', (event: MouseEvent) =>
-        disableChangeOnWheel(event));
-    };
-  });
-  return (
-    <div ref={inputParent}>
-      <InterlayInput
-        ref={ref}
-        type='number'
-        step='any'
-        pattern='[-+]?[0-9]*[.,]?[0-9]+'
-        placeholder='0.00'
-        spellCheck='false'
-        {...props} />
-    </div>
-  );
-});
+const NumberInput = React.forwardRef<Ref, InterlayInputProps>((props, ref): JSX.Element => (
+  <InterlayInput
+    ref={ref}
+    type='number'
+    step='any'
+    pattern='[-+]?[0-9]*[.,]?[0-9]+'
+    placeholder='0.00'
+    spellCheck='false'
+    // MEMO: inspired by https://stackoverflow.com/questions/63224459/disable-scrolling-on-input-type-number-in-react
+    onWheel={event => event.currentTarget.blur()}
+    {...props} />
+));
 NumberInput.displayName = 'NumberInput';
 
 export type Props = InterlayInputProps;
