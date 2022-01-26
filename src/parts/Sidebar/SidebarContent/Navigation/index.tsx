eae9daf9bbe740f865aad54d09a44516a71c984f
Thomas Jeatt <hello@tomjeatt.com>
2022-01-26 14:18:21 +0000

    refactor: consistent React imports

--- src/parts/Sidebar/SidebarContent/Navigation/index.tsx
+++ src/parts/Sidebar/SidebarContent/Navigation/index.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { matchPath } from 'react-router';
