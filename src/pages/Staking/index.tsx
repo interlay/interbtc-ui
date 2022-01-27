75a6a00d3209d971d07c8116905a4ae93a44aa55
crypto-engineer <anton@interlay.io>
2022-01-27 21:29:01 +0300

    feat: implement additional information UIs

--- src/pages/Staking/index.tsx
+++ src/pages/Staking/index.tsx
@@ -9,7 +9,10 @@ import InformationUI from './InformationUI';
 import MainContainer from 'parts/MainContainer';
 import Panel from 'components/Panel';
 import TokenField from 'components/TokenField';
-import { GOVERNANCE_TOKEN_SYMBOL } from 'config/relay-chains';
+import {
+  GOVERNANCE_TOKEN_SYMBOL,
+  VOTE_GOVERNANCE_TOKEN_SYMBOL
+} from 'config/relay-chains';
 
 const STAKING_GOVERNANCE_TOKEN_AMOUNT = 'staking-governance-token-amount';
 
@@ -40,6 +43,18 @@ const Staking = (): JSX.Element => {
           label='New unlock Date'
           value='Dec 16, 2023'
           tooltip='New unlock Date' />
+        <InformationUI
+          label='New total stake'
+          value={`40.00 ${VOTE_GOVERNANCE_TOKEN_SYMBOL}`}
+          tooltip='New total stake' />
+        <InformationUI
+          label='Estimated APY'
+          value='12.24%'
+          tooltip='Estimated APY' />
+        <InformationUI
+          label={`Estimated ${GOVERNANCE_TOKEN_SYMBOL} Rewards`}
+          value={`156.43  ${GOVERNANCE_TOKEN_SYMBOL}`}
+          tooltip='Estimated Rewards' />
       </Panel>
     </MainContainer>
   );
