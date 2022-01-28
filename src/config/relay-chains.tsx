777ba876daf03d4fde5cd42f2fe92e5e8a7c43b9
Thomas Jeatt <hello@tomjeatt.com>
2022-01-28 08:20:39 +0000

    fix: consistent file names

--- src/config/relay-chains.tsx
+++ src/config/relay-chains.tsx
@@ -94,7 +94,7 @@ case POLKADOT: {
   GovernanceTokenLogoIcon = InterlayLogoIcon;
   PUBLIC_ASSETS_FOLDER_NAME = 'interlay';
   APP_DOMAIN = 'https://bridge.interlay.io';
-  OPEN_GRAPH_IMAGE_FILE_NAME = 'interbtc-meta-image.jpg';
+  OPEN_GRAPH_IMAGE_FILE_NAME = 'interlay-meta-image.jpg';
   break;
 }
 // Kintsugi
