4fd700c93faf4b771e4cc73efb68f49ab05be240
crypto-engineer <anton@interlay.io>
2022-01-31 13:47:46 +0300

    fix: correct API changes in Vault

--- src/pages/Vault/index.tsx
+++ src/pages/Vault/index.tsx
@@ -119,7 +119,7 @@ const Vault = (): JSX.Element => {
           totalRedeemRequests
         ] = await Promise.allSettled([
           window.bridge.interBtcApi.vaults.get(vaultId, collateralIdLiteral),
-          window.bridge.interBtcApi.pools.getFeesWrapped(
+          window.bridge.interBtcApi.vaults.getFeesWrapped(
             newAccountId(window.bridge.interBtcApi.api, selectedVaultAddress),
             collateralIdLiteral,
             wrappedIdLiteral
@@ -127,7 +127,7 @@ const Vault = (): JSX.Element => {
           window.bridge.interBtcApi.vaults.getIssuedAmount(vaultId, collateralIdLiteral),
           window.bridge.interBtcApi.vaults.getVaultCollateralization(vaultId, collateralIdLiteral),
           window.bridge.interBtcApi.vaults.getAPY(vaultId, collateralIdLiteral),
-          window.bridge.interBtcApi.vaults.getIssuableAmount(vaultId, collateralIdLiteral),
+          window.bridge.interBtcApi.issue.getVaultIssuableAmount(vaultId, collateralIdLiteral),
           window
             .bridge
             .interBtcIndex
