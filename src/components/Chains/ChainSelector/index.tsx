1154a4e3bba60a48a6366221ee857a98c9b8f5ca
Thomas Jeatt <hello@tomjeatt.com>
2022-02-01 16:22:56 +0000

    chore: return JSX element implicitly

--- src/components/Chains/ChainSelector/index.tsx
+++ src/components/Chains/ChainSelector/index.tsx
@@ -26,81 +26,75 @@ const ChainSelector = ({
   chainOptions,
   currentChain,
   onChange
-}: Props): JSX.Element => {
-  return (
-    <>
-      {currentChain && (
-        <Select
-          variant={SELECT_VARIANTS.formField}
-          key={currentChain.name}
-          value={currentChain}
-          onChange={onChange}>
-          {({ open }) => (
-            <>
-              <SelectBody
-                className={clsx(
-                  'w-full'
-                )}>
-                <SelectButton variant={SELECT_VARIANTS.formField}>
-                  <span
-                    className={clsx(
-                      'flex',
-                      'items-center',
-                      'space-x-3',
-                      'text-xl',
-                      'py-2'
-                    )}>
-                    {currentChain.icon}
-                    <SelectText>
-                      <span className='capitalize'>
-                        {currentChain.name}
-                      </span>
-                    </SelectText>
-                  </span>
-                </SelectButton>
-                <SelectOptions open={open}>
-                  {chainOptions.map((chainOption: ChainOption) => {
-                    return (
-                      <SelectOption
-                        key={chainOption.name}
-                        value={chainOption}>
-                        {({
-                          selected,
-                          active
-                        }) => (
-                          <>
-                            <div
-                              className={clsx(
-                                'flex',
-                                'items-center',
-                                'space-x-3',
-                                'text-xl'
-                              )}>
-                              {chainOption.icon}
-                              <SelectText>
-                                <span className='capitalize'>
-                                  {chainOption.name}
-                                </span>
-                              </SelectText>
-                            </div>
-                            {selected ? (
-                              <SelectCheck
-                                active={active} />
-                            ) : null}
-                          </>
-                        )}
-                      </SelectOption>
-                    );
-                  })}
-                </SelectOptions>
-              </SelectBody>
-            </>
-          )}
-        </Select>
-      )}
-    </>
-  );
-};
+}: Props): JSX.Element => (
+  <Select
+    variant={SELECT_VARIANTS.formField}
+    key={currentChain.name}
+    value={currentChain}
+    onChange={onChange}>
+    {({ open }) => (
+      <>
+        <SelectBody
+          className={clsx(
+            'w-full'
+          )}>
+          <SelectButton variant={SELECT_VARIANTS.formField}>
+            <span
+              className={clsx(
+                'flex',
+                'items-center',
+                'space-x-3',
+                'text-xl',
+                'py-2'
+              )}>
+              {currentChain.icon}
+              <SelectText>
+                <span className='capitalize'>
+                  {currentChain.name}
+                </span>
+              </SelectText>
+            </span>
+          </SelectButton>
+          <SelectOptions open={open}>
+            {chainOptions.map((chainOption: ChainOption) => {
+              return (
+                <SelectOption
+                  key={chainOption.name}
+                  value={chainOption}>
+                  {({
+                    selected,
+                    active
+                  }) => (
+                    <>
+                      <div
+                        className={clsx(
+                          'flex',
+                          'items-center',
+                          'space-x-3',
+                          'text-xl'
+                        )}>
+                        {chainOption.icon}
+                        <SelectText>
+                          <span className='capitalize'>
+                            {chainOption.name}
+                          </span>
+                        </SelectText>
+                      </div>
+                      {selected ? (
+                        <SelectCheck
+                          active={active} />
+                      ) : null}
+                    </>
+                  )}
+                </SelectOption>
+              );
+            })}
+          </SelectOptions>
+        </SelectBody>
+      </>
+    )}
+  </Select>
+);
 
 export type { ChainOption };
 export default ChainSelector;
