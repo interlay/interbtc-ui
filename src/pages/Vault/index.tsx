69ea2b2175b41c5b599262ed7c4821914836da2d
Thomas Jeatt <hello@tomjeatt.com>
2022-02-08 11:42:05 +0000

    fix: set lockedBTC correctly

--- src/pages/Vault/index.tsx
+++ src/pages/Vault/index.tsx
@@ -315,7 +315,7 @@ const Vault = (): JSX.Element => {
           onClose={handleUpdateCollateralModalClose}
           collateralUpdateStatus={collateralUpdateStatus}
           vaultAddress={selectedVaultAddress}
-          hasLockedBTC={lockedBTC.eq(BitcoinAmount.zero)} />
+          hasLockedBTC={lockedBTC.gt(BitcoinAmount.zero)} />
       )}
       <RequestReplacementModal
         onClose={handleRequestReplacementModalClose}
