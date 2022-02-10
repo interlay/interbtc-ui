894e98b174bb65111cc724b2eee56a96331558da
Daniel Savu <savudani04@yahoo.ro>
2022-02-10 17:10:56 +0000

    fix: webpack build

--- craco.config.js
+++ craco.config.js
@@ -22,6 +22,21 @@ module.exports = {
         type: 'javascript/auto'
       });
 
+      webpackConfig.module.rules.push({
+        test: /\.js$/,
+        loader: require.resolve('@open-wc/webpack-import-meta-loader'),
+      });
+
+      webpackConfig.module.rules.push({
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        }
+      });
+
       // Fixes dtrace-provider compilation bug described here:
       // https://github.com/chrisa/node-dtrace-provider/issues/114
       webpackConfig.externals = [
