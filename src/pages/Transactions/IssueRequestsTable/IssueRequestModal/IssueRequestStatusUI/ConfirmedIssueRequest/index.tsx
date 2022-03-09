6ba6719ab0dbdd84088c514a83bdd792c1d32c08
Dominik Harz <dominik@interlay.io>
2022-03-09 17:24:25 +0900

    fix: remove 0x prefix from issue request

--- src/pages/Transactions/IssueRequestsTable/IssueRequestModal/IssueRequestStatusUI/ConfirmedIssueRequest/index.tsx
+++ src/pages/Transactions/IssueRequestsTable/IssueRequestModal/IssueRequestStatusUI/ConfirmedIssueRequest/index.tsx
@@ -49,7 +49,7 @@ const ConfirmedIssueRequest = ({
       if (!variables.backingPayment.btcTxId) {
         throw new Error('Bitcoin transaction ID not identified yet.');
       }
-      return window.bridge.issue.execute('0x' + variables.id, variables.backingPayment.btcTxId);
+      return window.bridge.issue.execute(variables.id, variables.backingPayment.btcTxId);
     },
     {
       onSuccess: (_, variables) => {
