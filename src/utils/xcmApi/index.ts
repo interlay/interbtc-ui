9ecf875d424163b09b9df2ff5efa0f4634818de4
Thomas Jeatt <hello@tomjeatt.com>
2022-03-09 16:43:29 +0000

    wip: xcm transfer function

--- src/utils/xcmApi/index.ts
+++ src/utils/xcmApi/index.ts
@@ -2,6 +2,7 @@ import {
   createSubstrateAPI,
   DefaultTransactionAPI
 } from '@interlay/interbtc-api';
+import { web3FromAddress } from '@polkadot/extension-dapp';
 
 // import { PARACHAIN_URL } from '../../constants';
 
@@ -12,6 +13,8 @@ const PARACHAIN_URL = 'wss://kusama-rpc.polkadot.io';
 // this will be extended to handle the reverse
 const xcmTransfer = async (): Promise<void> => {
   const api = await createSubstrateAPI(PARACHAIN_URL);
+  const destinationAddress = '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d'; // Alice
+
   const transactionAPI = new DefaultTransactionAPI(api, window.bridge.account);
 
   const dest = api.createType('XcmVersionedMultiLocation', {
@@ -25,21 +28,44 @@ const xcmTransfer = async (): Promise<void> => {
     })
   });
 
-  const benificiary = api.createType('XcmVersionedMultiLocation', {
+  const beneficiary = api.createType('XcmVersionedMultiLocation', {
     v1: api.createType('XcmV1MultiLocation', {
       parents: 0,
       interior: api.createType('XcmV1MultilocationJunctions', {
         x1: api.createType('XcmV1Junction', {
           accountId32: {
             network: api.createType('XcmV0JunctionNetworkId', { any: true }),
-            id: window.bridge.account
+            id: destinationAddress
           }
         })
       })
     })
   });
 
-  console.log('api', api, 'transactionAPI', transactionAPI, 'dest', dest, 'benificiary', benificiary);
+  const assets = api.createType('XcmVersionedMultiAssets', {
+    v1: api.createType('XcmV1MultiassetMultiAssets', [{
+      id: api.createType('XcmV1MultiassetAssetId', {
+        concrete: api.createType('XcmV1MultiLocation', {
+          parents: 0,
+          interior: api.createType('XcmV1MultilocationJunctions', {
+            here: true
+          })
+        })
+      }),
+      fun: api.createType('XcmV1MultiassetFungibility', {
+        fungible: 0
+      })
+    }])
+  });
+
+  if (window.bridge.account) {
+    // Signer needs to be explicitly set for this instance of the api
+    const { signer } = await web3FromAddress(window.bridge.account.toString());
+    api.setSigner(signer);
+
+    const xcmTransaction = api.tx.xcmPallet.reserveTransferAssets(dest, beneficiary, assets, 0);
+    await transactionAPI.sendLogged(xcmTransaction, undefined, undefined);
+  }
 };
 
 export { xcmTransfer };
