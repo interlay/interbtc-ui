05ffe09c95189951a669e80dc59493bd15b1559e
Thomas Jeatt <hello@tomjeatt.com>
2022-03-09 14:25:19 +0000

    wip: xcmTransfer utility

---
+++ src/utils/xcmApi/index.ts
@@ -0,0 +1,38 @@
+import {
+  createSubstrateAPI,
+  DefaultTransactionAPI
+} from '@interlay/interbtc-api';
+
+// import { PARACHAIN_URL } from '../../constants';
+
+// TODO: move this to config
+const PARACHAIN_URL = 'wss://kusama-rpc.polkadot.io';
+
+// Initial function transfers KSM from kusama -> kintsugi
+// this will be extended to
+const xcmTransfer = async (): Promise<void> => {
+  const api = await createSubstrateAPI(PARACHAIN_URL);
+  const transactionAPI = new DefaultTransactionAPI(api, window.bridge.account);
+
+  const dest = api.createType('XcmVersionedMultiLocation', {
+    v1: api.createType('XcmV1MultiLocation', {
+      parents: 0,
+      interior: api.createType('XcmV1MultilocationJunctions', {
+        x2: [
+          api.createType('XcmV1Junction', {
+            parachain: 2092
+          }),
+          api.createType('XcmV1Junction', {
+            accountId32: {
+              network: api.createType('XcmV0JunctionNetworkId', { any: true }),
+              id: window.bridge.account
+            }
+          })]
+      })
+    })
+  });
+
+  console.log('api', api, 'transactionAPI', transactionAPI, 'dest', dest);
+};
+
+export { xcmTransfer };
