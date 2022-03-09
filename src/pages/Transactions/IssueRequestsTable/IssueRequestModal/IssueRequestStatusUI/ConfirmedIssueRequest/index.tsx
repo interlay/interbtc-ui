63d58b6b7d31ca50c2ed388f478d96acf5c70351
Dominik Harz <dominik@interlay.io>
2022-03-09 16:27:14 +0900

    fix: manual issue execution

--- src/pages/Transactions/IssueRequestsTable/IssueRequestModal/IssueRequestStatusUI/ConfirmedIssueRequest/index.tsx
+++ src/pages/Transactions/IssueRequestsTable/IssueRequestModal/IssueRequestStatusUI/ConfirmedIssueRequest/index.tsx
@@ -46,7 +46,7 @@ const ConfirmedIssueRequest = ({
   // TODO: should type properly (`Relay`)
   const executeMutation = useMutation<void, Error, any>(
     (variables: any) => {
-      if (!variables.btcTxId) {
+      if (!variables.backingPayment.btcTxId) {
         throw new Error('Bitcoin transaction ID not identified yet.');
       }
       return window.bridge.issue.execute('0x' + variables.id, variables.backingPayment.btcTxId);
