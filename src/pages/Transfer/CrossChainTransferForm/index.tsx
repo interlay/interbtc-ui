a4db8c72b3531fe78b77f5db3bd2776fa25f5322
Thomas Jeatt <hello@tomjeatt.com>
2022-03-10 11:58:56 +0000

    chore: revert change to xcm form

--- src/pages/Transfer/CrossChainTransferForm/index.tsx
+++ src/pages/Transfer/CrossChainTransferForm/index.tsx
@@ -14,11 +14,6 @@ import { COLLATERAL_TOKEN_SYMBOL } from 'config/relay-chains';
 import { displayMonetaryAmount } from 'common/utils/utils';
 import { StoreType } from 'common/types/util.types';
 
-// This is a hardcoded test transaction so don't merge
-// eslint-disable-next-line max-len
-import InterlayDenimOrKintsugiMidnightOutlinedButton from 'components/buttons/InterlayDenimOrKintsugiMidnightOutlinedButton';
-import { xcmTransfer } from 'utils/xcmApi';
-
 const CrossChainTransferForm = (): JSX.Element => {
   const { t } = useTranslation();
 
@@ -27,36 +22,31 @@ const CrossChainTransferForm = (): JSX.Element => {
   } = useSelector((state: StoreType) => state.general);
 
   return (
-    <>
-      <InterlayDenimOrKintsugiMidnightOutlinedButton onClick={xcmTransfer}>
-        RUN TEST TRANSFER
-      </InterlayDenimOrKintsugiMidnightOutlinedButton>
-      <form className='space-y-8'>
-        <FormTitle>
-          {t('transfer_page.cross_chain_transfer_form.title')}
-        </FormTitle>
+    <form className='space-y-8'>
+      <FormTitle>
+        {t('transfer_page.cross_chain_transfer_form.title')}
+      </FormTitle>
+      <div>
+        <AvailableBalanceUI
+          label={t('transfer_page.cross_chain_transfer_form.balance')}
+          balance={displayMonetaryAmount(collateralTokenTransferableBalance)} />
         <div>
-          <AvailableBalanceUI
-            label={t('transfer_page.cross_chain_transfer_form.balance')}
-            balance={displayMonetaryAmount(collateralTokenTransferableBalance)} />
-          <div>
-            <TokenField
-              label={COLLATERAL_TOKEN_SYMBOL}
-              approxUSD='≈ $ 0' />
-          </div>
-          <Chains label={t('transfer_page.cross_chain_transfer_form.from_chain')} />
+          <TokenField
+            label={COLLATERAL_TOKEN_SYMBOL}
+            approxUSD='≈ $ 0' />
         </div>
-        <div>
-          <Chains label={t('transfer_page.cross_chain_transfer_form.to_chain')} />
-        </div>
-        <div>
-          <Accounts label={t('transfer_page.cross_chain_transfer_form.target_account')} />
-        </div>
-        <SubmitButton>
+        <Chains label={t('transfer_page.cross_chain_transfer_form.from_chain')} />
+      </div>
+      <div>
+        <Chains label={t('transfer_page.cross_chain_transfer_form.to_chain')} />
+      </div>
+      <div>
+        <Accounts label={t('transfer_page.cross_chain_transfer_form.target_account')} />
+      </div>
+      <SubmitButton>
           Submit
-        </SubmitButton>
-      </form>
-    </>
+      </SubmitButton>
+    </form>
   );
 };
 
