d037add72a81fce2e10b319192c6fb073a816744
Thomas Jeatt <hello@tomjeatt.com>
2022-03-10 14:54:05 +0000

    refactor: improved comment

--- src/pages/Transfer/CrossChainTransferForm/index.tsx
+++ src/pages/Transfer/CrossChainTransferForm/index.tsx
@@ -10,9 +10,15 @@ import TokenField from 'components/TokenField';
 import ErrorFallback from 'components/ErrorFallback';
 import FormTitle from 'components/FormTitle';
 import SubmitButton from 'components/SubmitButton';
-import { COLLATERAL_TOKEN_SYMBOL } from 'config/relay-chains';
+import { COLLATERAL_TOKEN_SYMBOL, COLLATERAL_TOKEN } from 'config/relay-chains';
 import { displayMonetaryAmount } from 'common/utils/utils';
 import { StoreType } from 'common/types/util.types';
+
+// This is a hardcoded test transaction so don't merge
+// eslint-disable-next-line max-len
+import InterlayDenimOrKintsugiMidnightOutlinedButton from 'components/buttons/InterlayDenimOrKintsugiMidnightOutlinedButton';
+import { xcmTransfer } from 'utils/xcmApi/transfer';
+import { newMonetaryAmount } from '@interlay/interbtc-api';
 
 const CrossChainTransferForm = (): JSX.Element => {
   const { t } = useTranslation();
@@ -21,32 +27,48 @@ const CrossChainTransferForm = (): JSX.Element => {
     collateralTokenTransferableBalance
   } = useSelector((state: StoreType) => state.general);
 
+  const handleTestTransaction = () => {
+    if (!window.bridge.account) return;
+
+    xcmTransfer(
+      window.bridge.account,
+      '14mJeAo9uZiqyVF7M9DMEYWSnJqGLsPpmxiCcJX8NHjaxcX9',
+      newMonetaryAmount(0.01, COLLATERAL_TOKEN)
+    );
+  };
+
   return (
-    <form className='space-y-8'>
-      <FormTitle>
-        {t('transfer_page.cross_chain_transfer_form.title')}
-      </FormTitle>
-      <div>
-        <AvailableBalanceUI
-          label={t('transfer_page.cross_chain_transfer_form.balance')}
-          balance={displayMonetaryAmount(collateralTokenTransferableBalance)} />
+    <>
+      <InterlayDenimOrKintsugiMidnightOutlinedButton
+        onClick={handleTestTransaction}>
+        RUN TEST TRANSFER
+      </InterlayDenimOrKintsugiMidnightOutlinedButton>
+      <form className='space-y-8'>
+        <FormTitle>
+          {t('transfer_page.cross_chain_transfer_form.title')}
+        </FormTitle>
         <div>
-          <TokenField
-            label={COLLATERAL_TOKEN_SYMBOL}
-            approxUSD='≈ $ 0' />
+          <AvailableBalanceUI
+            label={t('transfer_page.cross_chain_transfer_form.balance')}
+            balance={displayMonetaryAmount(collateralTokenTransferableBalance)} />
+          <div>
+            <TokenField
+              label={COLLATERAL_TOKEN_SYMBOL}
+              approxUSD='≈ $ 0' />
+          </div>
+          <Chains label={t('transfer_page.cross_chain_transfer_form.from_chain')} />
         </div>
-        <Chains label={t('transfer_page.cross_chain_transfer_form.from_chain')} />
-      </div>
-      <div>
-        <Chains label={t('transfer_page.cross_chain_transfer_form.to_chain')} />
-      </div>
-      <div>
-        <Accounts label={t('transfer_page.cross_chain_transfer_form.target_account')} />
-      </div>
-      <SubmitButton>
+        <div>
+          <Chains label={t('transfer_page.cross_chain_transfer_form.to_chain')} />
+        </div>
+        <div>
+          <Accounts label={t('transfer_page.cross_chain_transfer_form.target_account')} />
+        </div>
+        <SubmitButton>
           Submit
-      </SubmitButton>
-    </form>
+        </SubmitButton>
+      </form>
+    </>
   );
 };
 
