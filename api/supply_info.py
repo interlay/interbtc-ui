77e93aa852feeb14aa5cd77e6e6db4d4c8b0a1e6
ns212 <nikolai@interlay.io>
2022-03-16 21:57:56 +0000

    Add supply-info serverless function

--- api/supply_info.py
+++ api/supply_info.py
@@ -53,5 +53,11 @@ def get_kbtc_supply():
     return str(kBTC_supply)
 
 
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+
 if __name__ == "__main__":
     app.run()
