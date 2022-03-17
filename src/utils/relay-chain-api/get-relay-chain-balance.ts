e16392a0ff8e73557681f2e6ca27cfe767875b73
Thomas Jeatt <hello@tomjeatt.com>
2022-03-17 11:21:23 +0000

    chore: add TODO

--- src/utils/relay-chain-api/get-relay-chain-balance.ts
+++ src/utils/relay-chain-api/get-relay-chain-balance.ts
@@ -8,6 +8,7 @@ const getRelayChainBalance = async (
   api: ApiPromise,
   address: AddressOrPair
 ): Promise<RelayChainMonetaryAmount> => {
+  // TODO: resolve type error related to Codec type and cast properly
   const account = await api.query.system.account(address) as any;
 
   return newMonetaryAmount(account.data.free, COLLATERAL_TOKEN);
