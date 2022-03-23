375c815f0bb8dde3bef21a7331f9f1cff57e2a7b
Thomas Jeatt <hello@tomjeatt.com>
2022-03-23 13:53:36 +0000

    refactor: group all relay chain api consts together

--- src/utils/relay-chain-api/constants.ts
+++ src/utils/relay-chain-api/constants.ts
@@ -2,16 +2,17 @@
 // is being kept together until we decide whether to keep it in the UI
 // or move it to the lib. If we keep it in the UI, these values will be
 // moved to the configuration and const files.
+const PARACHAIN_ID = process.env.REACT_APP_PARACHAIN_ID;
 const RELAY_CHAIN_TRANSFER_FEE =
   process.env.REACT_APP_RELAY_CHAIN_NAME === 'kusama' ?
     '106666660' :
     process.env.REACT_APP_RELAY_CHAIN_NAME === 'polkadot' ?
       '320000000' :
       '';
-
 const TRANSFER_WEIGHT = '4000000000';
 
 export {
-  TRANSFER_WEIGHT,
-  RELAY_CHAIN_TRANSFER_FEE
+  PARACHAIN_ID,
+  RELAY_CHAIN_TRANSFER_FEE,
+  TRANSFER_WEIGHT
 };
