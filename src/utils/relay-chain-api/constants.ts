0cd3babe083d46b347c533805017d3953ed37da0
Thomas Jeatt <hello@tomjeatt.com>
2022-03-25 13:56:00 +0000

    fix: set transfer fee on mainnet

--- src/utils/relay-chain-api/constants.ts
+++ src/utils/relay-chain-api/constants.ts
@@ -1,15 +1,22 @@
-// NOTE: all code relating to the relay chain api and xcm transfers
-// is being kept together until we decide whether to keep it in the UI
-// or move it to the lib. If we keep it in the UI, these values will be
-// moved to the configuration and const files.
+// NOTE: all relay chain api and xcm transfers config will be moved to crosschain-api
+import { BitcoinNetwork } from 'types/bitcoin';
+import { BITCOIN_NETWORK } from '../../constants';
+
 const PARACHAIN_ID = process.env.REACT_APP_PARACHAIN_ID;
+
 const RELAY_CHAIN_TRANSFER_FEE =
-  process.env.REACT_APP_RELAY_CHAIN_NAME === 'kusama' ?
-    '106666660' :
-    process.env.REACT_APP_RELAY_CHAIN_NAME === 'polkadot' ?
-      '320000000' :
-      '';
+  // First condition sets fee for both environments in testnet
+  BITCOIN_NETWORK === BitcoinNetwork.Testnet ?
+    '320000000' :
+    process.env.REACT_APP_RELAY_CHAIN_NAME === 'kusama' ?
+      '106666660' :
+      process.env.REACT_APP_RELAY_CHAIN_NAME === 'polkadot' ?
+        '320000000' :
+        '';
+
 const TRANSFER_WEIGHT = '4000000000';
+
+console.log('RELAY_CHAIN_TRANSFER_FEE', RELAY_CHAIN_TRANSFER_FEE);
 
 export {
   PARACHAIN_ID,
