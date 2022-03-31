28f8293de09b6c49e3b7222b75a3029d185bdcc7
crypto-engineer <anton@interlay.io>
2022-03-31 17:18:03 +0300

    feat: update the layout of GetGovernanceTokenUI

--- src/parts/Sidebar/SidebarContent/GetGovernanceTokenUI/index.tsx
+++ src/parts/Sidebar/SidebarContent/GetGovernanceTokenUI/index.tsx
@@ -1,14 +1,14 @@
 
-// ray test touch <<
 import InterlayDefaultOutlinedButton from 'components/buttons/InterlayDefaultOutlinedButton';
 
 import { GOVERNANCE_TOKEN_SYMBOL } from 'config/relay-chains';
 
 const GetGovernanceTokenUI = (): JSX.Element => {
   return (
-    <InterlayDefaultOutlinedButton>Get {GOVERNANCE_TOKEN_SYMBOL}</InterlayDefaultOutlinedButton>
+    <InterlayDefaultOutlinedButton className='m-4'>
+      Get {GOVERNANCE_TOKEN_SYMBOL}
+    </InterlayDefaultOutlinedButton>
   );
 };
 
 export default GetGovernanceTokenUI;
-// ray test touch >>
