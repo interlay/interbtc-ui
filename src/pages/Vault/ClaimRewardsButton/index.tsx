7e430faad233ce311ead410f1cea97bc30d64323
crypto-engineer <anton@interlay.io>
2022-04-12 20:21:36 +0300

    feat: integrate react-error-boundary into ClaimRewardsButton

--- src/pages/Vault/ClaimRewardsButton/index.tsx
+++ src/pages/Vault/ClaimRewardsButton/index.tsx
@@ -1,6 +1,11 @@
 
 import { useTranslation } from 'react-i18next';
+import {
+  // useErrorHandler,
+  withErrorBoundary
+} from 'react-error-boundary';
 
+import ErrorFallback from 'components/ErrorFallback';
 import
 InterlayDenimOrKintsugiSupernovaContainedButton
   from 'components/buttons/InterlayDenimOrKintsugiSupernovaContainedButton';
@@ -8,6 +13,27 @@ import { GOVERNANCE_TOKEN_SYMBOL } from 'config/relay-chains';
 
 const ClaimRewardsButton = (): JSX.Element => {
   const { t } = useTranslation();
+
+  // ray test touch <
+  // const {
+  //   data: governanceReward,
+  //   error: governanceRewardError
+  // } = useQuery<GovernanceTokenMonetaryAmount, Error>(
+  //   [
+  //     GENERIC_FETCHER,
+  //     'vaults',
+  //     'getGovernanceReward',
+  //     vaultAccountId,
+  //     COLLATERAL_TOKEN_ID_LITERAL,
+  //     GOVERNANCE_TOKEN_SYMBOL
+  //   ],
+  //   genericFetcher<GovernanceTokenMonetaryAmount>(),
+  //   {
+  //     enabled: !!bridgeLoaded
+  //   }
+  // );
+  // useErrorHandler(governanceRewardError);
+  // ray test touch >
 
   return (
     <InterlayDenimOrKintsugiSupernovaContainedButton>
@@ -18,4 +44,9 @@ const ClaimRewardsButton = (): JSX.Element => {
   );
 };
 
-export default ClaimRewardsButton;
+export default withErrorBoundary(ClaimRewardsButton, {
+  FallbackComponent: ErrorFallback,
+  onReset: () => {
+    window.location.reload();
+  }
+});
