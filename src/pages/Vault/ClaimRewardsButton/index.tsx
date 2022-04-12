9d66f593f584f78572b2cbdf2e22e1e14fd9899d
crypto-engineer <anton@interlay.io>
2022-04-12 20:00:40 +0300

    feat: implement the UI for ClaimRewardsButton

--- src/pages/Vault/ClaimRewardsButton/index.tsx
+++ src/pages/Vault/ClaimRewardsButton/index.tsx
@@ -1,12 +1,21 @@
 
+import { useTranslation } from 'react-i18next';
+
+import
+InterlayDenimOrKintsugiSupernovaContainedButton
+  from 'components/buttons/InterlayDenimOrKintsugiSupernovaContainedButton';
+import { GOVERNANCE_TOKEN_SYMBOL } from 'config/relay-chains';
+
 const ClaimRewardsButton = (): JSX.Element => {
-  // ray test touch <
+  const { t } = useTranslation();
+
   return (
-    <>
-      ClaimRewardsButton
-    </>
+    <InterlayDenimOrKintsugiSupernovaContainedButton>
+      {t('vault.claim_governance_token_rewards', {
+        governanceTokenSymbol: GOVERNANCE_TOKEN_SYMBOL
+      })}
+    </InterlayDenimOrKintsugiSupernovaContainedButton>
   );
-  // ray test touch >
 };
 
 export default ClaimRewardsButton;
