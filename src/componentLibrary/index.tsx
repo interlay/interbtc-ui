0339f2d7b236d2ae211a575c30798a1a3ede6fc8
Thomas Jeatt <hello@tomjeatt.com>
2022-04-25 10:25:34 +0100

    chore: export component props

--- src/componentLibrary/index.tsx
+++ src/componentLibrary/index.tsx
@@ -5,6 +5,7 @@ export { CTA } from './CTA';
 export type { CTAProps } from './CTA';
 
 export { CoinPair } from './CoinPair';
+export type { CoinPairProps } from './CoinPair';
 
 export { CoinIcon } from './CoinIcon';
 export type { CoinIconProps } from './CoinIcon';
