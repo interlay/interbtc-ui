46350aac48dab0a115c0ac266e91f310b21adbfd
Thomas Jeatt <hello@tomjeatt.com>
2022-05-18 14:20:08 +0100

    fix: import vault config correctly

--- src/utils/hooks/api/use-get-vaults.tsx
+++ src/utils/hooks/api/use-get-vaults.tsx
@@ -1,15 +1,15 @@
 import { useQueries, UseQueryResult } from 'react-query';
 import { AccountId } from '@polkadot/types/interfaces';
-import { CollateralIdLiteral, newAccountId, VaultExt } from '@interlay/interbtc-api';
+import { CurrencyIdLiteral, newAccountId, VaultExt } from '@interlay/interbtc-api';
 import { BitcoinUnit } from '@interlay/monetary-js';
 
 import { StoreType } from 'common/types/util.types';
-import { VAULT_COLLATERAL_TOKENS } from 'utils/constants/currency';
+import { VAULT_COLLATERAL } from 'config/vaults';
 import { useSelector } from 'react-redux';
 
 const getVaults = async (
   accountId: AccountId,
-  token: CollateralIdLiteral
+  token: CurrencyIdLiteral
 ) => await window.bridge.vaults.get(accountId, token);
 
 // TODO: when introducing KINT support we should consider whether to parse the data from parallel
@@ -24,7 +24,7 @@ const useGetVaults = ({ address }: { address: string; }): Array<VaultExt<Bitcoin
 
   // TODO: updating react-query to > 3.28.0 will allow us to type this properly
   const vaults = useQueries<Array<UseQueryResult<unknown, unknown>>>(
-    VAULT_COLLATERAL_TOKENS.map(token => {
+    VAULT_COLLATERAL.map(token => {
       return {
         queryKey: ['vaults', address, token],
         queryFn: () => getVaults(newAccountId(window.bridge.api, address), token),
