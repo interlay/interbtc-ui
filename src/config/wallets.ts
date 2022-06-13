bb5915d17c4ac170407f6d59ae0ad90500d79857
crypto-engineer <anton@interlay.io>
2022-06-13 12:41:40 +0300

    chore: clean up

--- src/config/wallets.ts
+++ src/config/wallets.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { ReactComponent as PolkadotExtensionLogoIcon } from '@/assets/img/polkadot-extension-logo.svg';
-import { ReactComponent as SubWalletLogoIcon } from '@/assets/img/subwallet-logo.svg';
+import { ReactComponent as SubWalletLogoIcon } from '@/assets/img/sub-wallet-logo.svg';
 import { ReactComponent as TalismanWalletLogoIcon } from '@/assets/img/talisman-wallet-logo.svg';
 
 enum WalletSourceName {
