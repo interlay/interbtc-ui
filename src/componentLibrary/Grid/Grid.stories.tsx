c919c75332ceb979ca01e6caa6a589abfd817f38
Thomas Jeatt <hello@tomjeatt.com>
2022-06-13 15:06:22 +0100

    chore: tweak story

--- src/componentLibrary/Grid/Grid.stories.tsx
+++ src/componentLibrary/Grid/Grid.stories.tsx
@@ -4,7 +4,15 @@ import { Grid, GridItem } from './';
 
 export const Default: ComponentStory<typeof Grid> = () => (
   <Grid>
-    <GridItem mobile={{ span: 4, start: 1 }} desktop={{ span: 4, start: 4 }}>
+    <GridItem mobile={{ span: 4, start: 1 }} desktop={{ span: 6, start: 1 }}>
+      <h2>Grid content</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nam minima non modi consequuntur corporis est
+        itaque, exercitationem amet, fugiat optio, facilis repellendus inventore vero perferendis. Possimus porro eaque
+        facere.
+      </p>
+    </GridItem>
+    <GridItem mobile={{ span: 4, start: 1 }} desktop={{ span: 6, start: 7 }}>
       <h2>Grid content</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nam minima non modi consequuntur corporis est
