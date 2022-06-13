7cb9e49f15b3370d0fbd7214bdde13202c3a61cc
Thomas Jeatt <hello@tomjeatt.com>
2022-06-13 11:54:43 +0100

    refactor: remove placeholder styles

--- src/componentLibrary/Grid/Grid.style.tsx
+++ src/componentLibrary/Grid/Grid.style.tsx
@@ -3,10 +3,8 @@ import { theme } from 'componentLibrary';
 
 export const GridContainer = styled.div`
   display: grid;
-  gap: 20px;
+  gap: ${theme.spacing.spacing5};
   grid-template-columns: repeat(4, 1fr);
-  height: 600px;
-  width: 900px;
 
   @media screen and (min-width: ${theme.layout.breakpoints.lg}) {
     grid-template-columns: repeat(12, 1fr);
