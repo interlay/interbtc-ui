c412ebf3176ac30c832892550d559fa71bcfba0b
crypto-engineer <anton@interlay.io>
2022-06-15 01:09:59 +0300

    chore: clean up

--- src/common/types/actions.types.ts
+++ src/common/types/actions.types.ts
@@ -1,4 +1,4 @@
-import { Issue, CollateralUnit } from '@interlay/interbtc-api';
+import { CollateralUnit } from '@interlay/interbtc-api';
 import { BitcoinAmount, MonetaryAmount, Currency } from '@interlay/monetary-js';
 
 import { GovernanceTokenMonetaryAmount } from 'config/relay-chains';
@@ -142,18 +142,6 @@ export interface TogglePremiumRedeem {
 
 // ray test touch <
 export type RedeemActions = ChangeAddress | InitState | TogglePremiumRedeem;
-// ray test touch >
-
-// ray test touch <
-// ISSUE
-export const STORE_ISSUE_REQUEST = 'STORE_ISSUE_REQUEST';
-// ray test touch >
-
-// ray test touch <
-export interface StoreIssueRequest {
-  type: typeof STORE_ISSUE_REQUEST;
-  request: Issue;
-}
 // ray test touch >
 
 // VAULT
