1aa247d2d9a68fd2b7dbd88062add9226d98a1f0
Thomas Jeatt <hello@tomjeatt.com>
2022-06-20 13:22:17 +0100

    fix: add vault wrapped token to vault config

--- src/config/vaults.ts
+++ src/config/vaults.ts
@@ -3,14 +3,17 @@ import { CurrencyIdLiteral } from '@interlay/interbtc-api';
 import { POLKADOT, KUSAMA } from 'utils/constants/relay-chain-names';
 
 let VAULT_COLLATERAL: Array<CurrencyIdLiteral> = [];
+let VAULT_WRAPPED: CurrencyIdLiteral;
 
 switch (process.env.REACT_APP_RELAY_CHAIN_NAME) {
   case POLKADOT: {
     VAULT_COLLATERAL = [CurrencyIdLiteral.DOT, CurrencyIdLiteral.INTR];
+    VAULT_WRAPPED = CurrencyIdLiteral.INTERBTC;
     break;
   }
   case KUSAMA: {
     VAULT_COLLATERAL = [CurrencyIdLiteral.KSM, CurrencyIdLiteral.KINT];
+    VAULT_WRAPPED = CurrencyIdLiteral.KBTC;
     break;
   }
   default: {
@@ -18,4 +21,4 @@ switch (process.env.REACT_APP_RELAY_CHAIN_NAME) {
   }
 }
 
-export { VAULT_COLLATERAL };
+export { VAULT_COLLATERAL, VAULT_WRAPPED };
