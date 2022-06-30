0606897c284cdd46c79baa515172614079c6f6d3
crypto-engineer <anton@interlay.io>
2022-06-30 22:40:03 +0300

    feat: update Input

--- src/component-library/Input/Input.tsx
+++ src/component-library/Input/Input.tsx
@@ -1,11 +1,14 @@
 // ray test touch <
+import * as React from 'react';
+
 import { BaseInput } from './Input.style';
 
 type InputProps = React.ComponentPropsWithRef<'input'>;
 
-const Input = (): JSX.Element => {
-  return <BaseInput />;
-};
+const Input = React.forwardRef<HTMLInputElement, InputProps>(((props, ref): JSX.Element => {
+  return <BaseInput ref={ref} {...props} />;
+}));
+Input.displayName = 'Input';
 
 export { Input };
 export type { InputProps };
