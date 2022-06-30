842d24f55ad24ade6c6df6adaa84110c62ef0a81
crypto-engineer <anton@interlay.io>
2022-06-30 22:25:05 +0300

    feat: update BaseInput

--- src/component-library/Input/Input.style.tsx
+++ src/component-library/Input/Input.style.tsx
@@ -1,8 +1,9 @@
-// ray test touch <<
 import styled from 'styled-components';
 import { theme } from 'component-library';
 
 const BaseInput = styled.input`
+  /* MEMO: inspired by https://www.w3schools.com/howto/howto_css_hide_arrow_number.asp */
+  /* Chrome, Safari, Edge, Opera */
   &::-webkit-outer-spin-button, &::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
@@ -10,7 +11,12 @@ const BaseInput = styled.input`
   &[type='number'] {
     -moz-appearance: textfield;
   }
-  // TODO: focus styling is missing
+
+  // TODO: focus styling is missing. see https://www.a11yproject.com/posts/never-remove-css-outlines/#:~:text=Using%20the%20CSS%20rule%20%3Afocus,with%20the%20link%20or%20control.
+  :focus {
+    outline: none;
+  }
+
   color: ${theme.colors.textPrimary}; // TODO: this might not be needed as it's inherited
   background-color: transparent;
   display: block;
@@ -23,7 +29,7 @@ const BaseInput = styled.input`
     color: ${theme.colors.textTertiary};
   }
   border: ${theme.border.default};
+  padding: ${theme.spacing.spacing2} ${theme.spacing.spacing3};
 `;
 
 export { BaseInput };
-// ray test touch >>
