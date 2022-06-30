00d8da894a4a4ce2268ce4f47b5de21c60f74221
Thomas Jeatt <hello@tomjeatt.com>
2022-06-30 15:02:26 +0100

    chore: improve typing in get vaults hook

--- src/utils/hooks/api/use-get-vaults.tsx
+++ src/utils/hooks/api/use-get-vaults.tsx
@@ -2,29 +2,33 @@ import { useEffect, useState } from 'react';
 import { useQueries, UseQueryResult } from 'react-query';
 import { useErrorHandler } from 'react-error-boundary';
 import { AccountId } from '@polkadot/types/interfaces';
-import { CurrencyIdLiteral, newAccountId } from '@interlay/interbtc-api';
+import { CurrencyIdLiteral, newAccountId, VaultExt } from '@interlay/interbtc-api';
+import { BitcoinUnit } from '@interlay/monetary-js';
 
 import { StoreType } from 'common/types/util.types';
 import { VAULT_COLLATERAL } from 'config/vaults';
 import { useSelector } from 'react-redux';
+
+type VaultResponse = Array<VaultExt<BitcoinUnit>>;
 
 const getVaults = async (
   accountId: AccountId,
   token: CurrencyIdLiteral
 ) => await window.bridge.vaults.get(accountId, token);
 
-const useGetVaults = ({ address }: { address: string; }): Array<any> => {
+const useGetVaults = ({ address }: { address: string; }): VaultResponse => {
   const [queriesComplete, setQueriesComplete] = useState<boolean>(false);
-  const [queryError, setQueryError] = useState<any | undefined>(undefined);
+  const [queryError, setQueryError] = useState<Error | undefined>(undefined);
 
   const { bridgeLoaded } = useSelector((state: StoreType) => state.general);
 
   useErrorHandler(queryError);
 
-  const vaults = useQueries<Array<UseQueryResult<unknown, unknown>>>(
+// TODO: upgrade react-query to handle instances of casting
+  const vaults = useQueries<Array<UseQueryResult<VaultResponse, Error>>>(
     VAULT_COLLATERAL.map(token => {
       return {
-        queryKey: ['vaults', token],
+        queryKey: ['vaults', address, token],
         queryFn: async () => await getVaults(newAccountId(window.bridge.api, address), token),
         options: {
           enabled: !!bridgeLoaded
@@ -38,7 +42,7 @@ const useGetVaults = ({ address }: { address: string; }): Array<any> => {
     
     for (const vault of vaults) {
       if (vault.error) {
-        setQueryError(vault.error);
+        setQueryError(vault.error as Error);
 
         return;
       }
@@ -48,7 +52,8 @@ const useGetVaults = ({ address }: { address: string; }): Array<any> => {
     setQueriesComplete(haveQueriesCompleted);
   }, [vaults]);
 
-  return queriesComplete ? vaults.map(vault => vault.data) : [];
+  // TODO: casting can be removed after react-query update
+  return queriesComplete ? vaults.map(vault => vault.data) as VaultResponse : [];
 };
 
 export { useGetVaults };
