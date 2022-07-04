135d62af5f0bd79ca1d68f87653eb64c163f9769
Peter Slaný <peter@interlay.io>
2022-07-04 14:41:53 +0200

    refactor: refactored component library colors and globally defined font

--- src/componentLibrary/Balance/Balance.style.tsx
+++ src/componentLibrary/Balance/Balance.style.tsx
@@ -4,17 +4,16 @@ import { theme } from 'componentLibrary';
 
 const BalanceWrapper = styled.dl`
   display: flex;
-  font-family: ${theme.font.primary};
   font-weight: ${theme.fontWeight.book};
 `;
 
 const BalanceLabel = styled.dt`
-  color: ${theme.colors.textSecondary};
+  color: ${theme.colors.textPrimary};
 `;
 
 const BalanceValue = styled.dd`
   margin-left: ${theme.spacing.spacing1};
-  color: ${theme.colors.textAccent};
+  color: ${theme.colors.textSecondary};
 `;
 
 export { BalanceLabel, BalanceValue, BalanceWrapper };
