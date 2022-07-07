79acd8a56ba2e893d3d3c6aa5b02e52c3583d36c
Peter Slaný <peter@interlay.io>
2022-07-07 15:57:51 +0200

    refactor: changed util name

--- src/utils/helpers/transaction.ts
+++ src/utils/helpers/transaction.ts
@@ -3,7 +3,7 @@ import { AddressOrPair } from "@polkadot/api/types"
 
 import { GovernanceTokenMonetaryAmount, GOVERNANCE_TOKEN } from "config/relay-chains";
 
-const getStakingTransactionFee = async (account: AddressOrPair): Promise<GovernanceTokenMonetaryAmount> => {
+const getStakingTransactionFeeReserve = async (account: AddressOrPair): Promise<GovernanceTokenMonetaryAmount> => {
     const transactions = [
         window.bridge.api.tx.escrow.withdraw(),
         window.bridge.api.tx.escrowAnnuity.withdrawRewards()
@@ -13,4 +13,4 @@ const getStakingTransactionFee = async (account: AddressOrPair): Promise<Governa
     return newMonetaryAmount(transactionsPaymentInfo.partialFee.toString(), GOVERNANCE_TOKEN);
 }
 
-export { getStakingTransactionFee }
+export { getStakingTransactionFeeReserve }
