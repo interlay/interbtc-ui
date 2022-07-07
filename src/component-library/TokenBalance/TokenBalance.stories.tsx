c3d51b4615b80b24d21f668309c3e0c2acee9b37
crypto-engineer <anton@interlay.io>
2022-07-07 18:45:18 +0300

    feat: add exports to component-library

--- src/component-library/TokenBalance/TokenBalance.stories.tsx
+++ src/component-library/TokenBalance/TokenBalance.stories.tsx
@@ -1,13 +1,12 @@
 import { Story, Meta } from '@storybook/react';
 
-import { CurrencySymbols } from 'types/currency';
 import { TokenBalance, TokenBalanceProps } from '.';
 
 const Template: Story<TokenBalanceProps> = (args) => <TokenBalance {...args} />;
 
 const Default = Template.bind({});
 Default.args = {
-  tokenSymbol: CurrencySymbols.IBTC,
+  tokenSymbol: 'KSM',
   value: '123.22',
   valueInUSD: '23.796'
 };
