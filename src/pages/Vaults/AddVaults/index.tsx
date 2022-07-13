17d2cbe24d4282f4d1c09c2a4e279224c3ab93c3
Thomas Jeatt <hello@tomjeatt.com>
2022-07-13 10:00:55 +0100

    wip: placeholder modal

--- src/pages/Vaults/AddVaults/index.tsx
+++ src/pages/Vaults/AddVaults/index.tsx
@@ -1,5 +1,7 @@
-import { NewVaultsTable } from 'component-library';
+import { useState } from 'react';
+import { NewVaultsTable, Modal } from 'component-library';
 import { useGetAvailableVaults } from 'utils/hooks/api/use-get-available-vaults';
+import { CollateralIdLiteral } from '@interlay/interbtc-api';
 
 interface AddVaultProps {
   vaults: any;
@@ -9,22 +11,37 @@ const isVaultInstalled = (vaults: any, currentVault: any) =>
   vaults?.some((vault: any) => vault.collateralId === currentVault.collateralCurrency);
 
 const AddVaults = ({ vaults }: AddVaultProps): JSX.Element => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [selectedCurrency, setSelectedCurrency] = useState<CollateralIdLiteral | undefined>(undefined);
+
+  const handleOpenModal = (currency: CollateralIdLiteral) => {
+    setModalOpen(true);
+    setSelectedCurrency(currency);
+  };
+
+  const onModalClose = () => {
+    setModalOpen(false);
+    setSelectedCurrency(undefined);
+  };
   const availableVaults = useGetAvailableVaults();
 
   return (
-    <NewVaultsTable
-      data={availableVaults.map((vault) => ({
-        collateralCurrency: vault.collateralCurrency,
-        wrappedCurrency: vault.wrappedCurrency,
-        minCollateralAmount: vault.minimumCollateral.toNumber().toFixed(2),
-        collateralRate: vault.secureCollateralThreshold.toNumber().toFixed(2),
-        isActive: true,
-        isInstalled: isVaultInstalled(vaults, vault),
-        ctaOnClick: () => {
-          return undefined;
-        }
-      }))}
-    />
+    <>
+      <NewVaultsTable
+        data={availableVaults.map((vault) => ({
+          collateralCurrency: vault.collateralCurrency,
+          wrappedCurrency: vault.wrappedCurrency,
+          minCollateralAmount: vault.minimumCollateral.toNumber().toFixed(2),
+          collateralRate: vault.secureCollateralThreshold.toNumber().toFixed(2),
+          isActive: true,
+          isInstalled: isVaultInstalled(vaults, vault),
+          ctaOnClick: () => handleOpenModal(vault.collateralCurrency)
+        }))}
+      />
+      <Modal open={modalOpen} onClose={onModalClose}>
+        {selectedCurrency}
+      </Modal>
+    </>
   );
 };
 
