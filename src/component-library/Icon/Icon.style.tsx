d4c6a0e238118cd48d3bd6bda4ee5c0a8944da9b
crypto-engineer <anton@interlay.io>
2022-07-18 22:12:07 +0300

    chore: clean up

--- src/component-library/Icon/Icon.style.tsx
+++ src/component-library/Icon/Icon.style.tsx
@@ -1,12 +1,9 @@
 import styled from 'styled-components';
-// ray test touch <
 import { ReactComponent as CloseIcon } from '@material-icons/svg/svg/close/baseline.svg';
 import { ReactComponent as CheckmarkIcon } from '@material-icons/svg/svg/check/baseline.svg';
-// ray test touch >
 
 import { theme } from 'component-library';
 
-// ray test touch <
 const BaseCloseIcon = styled(CloseIcon)`
   fill: ${theme.colors.textPrimary};
 `;
@@ -16,4 +13,3 @@ const BaseCheckmarkIcon = styled(CheckmarkIcon)`
 `;
 
 export { BaseCloseIcon, BaseCheckmarkIcon };
-// ray test touch >
