b91f1a561d9c272517363c68ef105eef3873ceef
Daniel Simão <rui.daniel.simao@gmail.com>
2022-08-08 14:32:34 +0100

    feat: add table

---
+++ src/component-library/Table/TableColumnHeader.tsx
@@ -0,0 +1,30 @@
+import { useTableColumnHeader } from '@react-aria/table';
+import { mergeProps } from '@react-aria/utils';
+import { TableState } from '@react-stately/table';
+import { GridNode } from '@react-types/grid';
+import { HTMLAttributes, useRef } from 'react';
+
+import { StyledTableColumnHeader } from './Table.style';
+
+type Props = {
+  state: TableState<Record<string, any>>;
+  column: GridNode<Record<string, any>>;
+};
+
+type NativeAttrs = Omit<HTMLAttributes<HTMLTableCellElement>, keyof Props>;
+
+type TableColumnHeaderProps = Props & NativeAttrs;
+
+const TableColumnHeader = ({ column, state, ...props }: TableColumnHeaderProps): JSX.Element => {
+  const ref = useRef<HTMLTableCellElement>(null);
+  const { columnHeaderProps } = useTableColumnHeader({ node: column }, state, ref);
+
+  return (
+    <StyledTableColumnHeader colSpan={column.colspan} ref={ref} {...mergeProps(props, columnHeaderProps)}>
+      {column.rendered}
+    </StyledTableColumnHeader>
+  );
+};
+
+export { TableColumnHeader };
+export type { TableColumnHeaderProps };
---
+++ src/component-library/Table/TableRow.tsx
@@ -0,0 +1,34 @@
+import { useTableRow } from '@react-aria/table';
+import { mergeProps } from '@react-aria/utils';
+import { TableState } from '@react-stately/table';
+import { GridNode } from '@react-types/grid';
+import { HTMLAttributes, useRef } from 'react';
+
+type Props = {
+  state: TableState<Record<string, any>>;
+  item: GridNode<Record<string, any>>;
+};
+
+type NativeAttrs = Omit<HTMLAttributes<HTMLTableRowElement>, keyof Props>;
+
+type TableRowProps = Props & NativeAttrs;
+
+const TableRow = ({ item, children, state, ...props }: TableRowProps): JSX.Element => {
+  const ref = useRef<HTMLTableRowElement>(null);
+  const { rowProps } = useTableRow(
+    {
+      node: item
+    },
+    state,
+    ref
+  );
+
+  return (
+    <tr ref={ref} {...mergeProps(props, rowProps)}>
+      {children}
+    </tr>
+  );
+};
+
+export { TableRow };
+export type { TableRowProps };
