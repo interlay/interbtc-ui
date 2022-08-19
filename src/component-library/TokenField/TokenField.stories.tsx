32a4836b81126b1158e0f40cf313ea1e83b9e425
crypto-engineer <anton@interlay.io>
2022-08-19 12:08:36 +0300

    chore: clean up

--- src/component-library/TokenField/TokenField.stories.tsx
+++ src/component-library/TokenField/TokenField.stories.tsx
@@ -9,18 +9,18 @@ const Template: Story<TokenFieldProps> = (args) => <TokenField {...args} />;
 const WithBalance = Template.bind({});
 WithBalance.args = {
   tokenSymbol: 'KSM',
-  valueInUSD: formatUSD(100.0),
+  valueInUSD: formatUSD(100.00),
   defaultValue: 100.0, // `value`
   balance: {
     value: '1000.00',
-    valueInUSD: formatUSD(1000.0)
+    valueInUSD: formatUSD(1000.00)
   }
 };
 
 const WithoutBalance = Template.bind({});
 WithoutBalance.args = {
   tokenSymbol: 'KSM',
-  valueInUSD: formatUSD(100.0),
+  valueInUSD: formatUSD(100.00),
   defaultValue: 100.0 // `value`
 };
 
