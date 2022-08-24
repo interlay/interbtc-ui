72ddbadcc9c025c2afea42d356266f4b772c4ac9
Peter Slaný <peter@interlay.io>
2022-08-24 17:00:30 +0200

    wip test: start mocking api

--- src/__tests__/Burn/Burn.test.tsx
+++ src/__tests__/Burn/Burn.test.tsx
@@ -1,15 +1,40 @@
 import '@testing-library/jest-dom';
 
-import interBtcApi from '@interlay/interbtc-api';
-import { Route } from 'react-router';
+import * as interBtcApi from '@interlay/interbtc-api';
+import { InterBtcApi, newMonetaryAmount } from '@interlay/interbtc-api';
+import { BitcoinAmount } from '@interlay/monetary-js';
+import { MockProvider } from '@polkadot/rpc-provider/mock';
+import { mocked } from 'ts-jest';
 
 import App from '../../App';
 import { cleanup, render, screen, waitForElementToBeRemoved } from '../test-utils';
 afterEach(cleanup);
-// const interbtcApi = jest.createMockFromModule('@interlay/interbtc-api');
-jest.mock('@interlay/interbtc-api');
 
-interBtcApi.DefaultRedeemAPI.describe('Burn page', () => {
+jest.mock('@interlay/interbtc-api');
+const mockedApi = mocked(interBtcApi);
+
+const mockProvider = () => {
+  const registry = interBtcApi.createAPIRegistry();
+  return new MockProvider(registry);
+};
+
+type RecursivePartial<T> = {
+  [P in keyof T]?: RecursivePartial<T[P]>;
+};
+
+const mockedApiValue: RecursivePartial<InterBtcApi> = {
+  tokens: {
+    total: async (currency) => newMonetaryAmount('1000000000000', currency)
+  },
+  redeem: {
+    getMaxBurnableTokens: async () => new BitcoinAmount('100000000')
+  }
+};
+
+// Replace provider with mock provider to not connect
+mockedApi.createInterBtcApi.mockResolvedValue(mockedApiValue as InterBtcApi);
+// mockedApi.createInterBtcApi.mockResolvedValue();
+describe('Burn page', () => {
   beforeEach(async () => {
     // TODO: mock the lib, so there's liquidated vault
     // return from window.bridge.redeem.getMaxBurnableTokens() > 0
@@ -17,9 +42,9 @@ interBtcApi.DefaultRedeemAPI.describe('Burn page', () => {
 
   it('should display burn tab when there is liquidated vault', async () => {
     render(<App />, { path: '/bridge?tab=burn' });
-    // await waitForElementToBeRemoved(async () => await screen.findByRole('progressbar'), { timeout: 10000 });
-    await screen.findByText('Redeem');
-    const burnTab = screen.getByText('Redeem');
+    await waitForElementToBeRemoved(async () => await screen.findByRole('progressbar'), { timeout: 10000 });
+    await screen.findByText('Burn');
+    const burnTab = screen.getByText('Burn');
 
     expect(burnTab).toBeVisible();
   });
