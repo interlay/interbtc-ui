cdc60f4fa81696de38c14c522ea6c3e83c65947e
Peter Slaný <peter@interlay.io>
2022-08-31 11:31:59 +0200

    wip test: mock lib

--- src/__tests__/Burn/Burn.test.tsx
+++ src/__tests__/Burn/Burn.test.tsx
@@ -1,16 +1,68 @@
 import '@testing-library/jest-dom';
 
 import * as interBtcApi from '@interlay/interbtc-api';
-import { InterBtcApi, newMonetaryAmount } from '@interlay/interbtc-api';
-import { BitcoinAmount } from '@interlay/monetary-js';
+import { atomicToBaseAmount, InterBtcApi, newMonetaryAmount } from '@interlay/interbtc-api';
+import { ExchangeRate } from '@interlay/monetary-js';
+import { Bitcoin, BitcoinAmount } from '@interlay/monetary-js';
 import { MockProvider } from '@polkadot/rpc-provider/mock';
-import { mocked } from 'ts-jest';
+import Big from 'big.js';
+import { mocked } from 'ts-jest/utils';
 
 import App from '../../App';
-import { cleanup, render, screen, waitForElementToBeRemoved } from '../test-utils';
+import { act, cleanup, render, screen, userEvent } from '../test-utils';
+
+// jest.mock('@polkadot/extension-dapp');
+
 afterEach(cleanup);
 
-jest.mock('@interlay/interbtc-api');
+const DEFAULT_BTC_BLOCK_HEIGHT = 1000;
+const DEFAULT_TOKEN_AMOUNT = '1000000000000';
+
+const SYSTEM_STATUS_RUNNING = { isRunning: true };
+
+const DEFAULT_ISSUE_FEE = new Big(0.005);
+const DEFAULT_ISSUE_GRIEFING_COLLATERAL_RATE = new Big(0.00005);
+
+const DEFAULT_EXCHANGE_RATE = '1000000';
+
+const DEFAULT_MAX_BURNABLE_TOKENS = atomicToBaseAmount('100000000', Bitcoin); // 1 BTC
+const DEFAULT_BURN_EXCHANGE_RATE = '150000';
+
+const mockedApiValue: RecursivePartial<InterBtcApi> = {
+  tokens: {
+    total: async (currency: interBtcApi.CurrencyExt) => newMonetaryAmount(DEFAULT_TOKEN_AMOUNT, currency)
+  },
+  btcRelay: { getLatestBlockHeight: async () => DEFAULT_BTC_BLOCK_HEIGHT },
+  electrsAPI: { getLatestBlockHeight: async () => DEFAULT_BTC_BLOCK_HEIGHT },
+  system: {
+    getStatusCode: async () => SYSTEM_STATUS_RUNNING // Change to isError or isShutdown in order to simulate parachain being down.
+  },
+  fee: {
+    getIssueFee: async () => DEFAULT_ISSUE_FEE,
+    getIssueGriefingCollateralRate: async () => DEFAULT_ISSUE_GRIEFING_COLLATERAL_RATE
+  },
+  oracle: {
+    getExchangeRate: (currency: interBtcApi.CurrencyExt) =>
+      new ExchangeRate(Bitcoin, currency, Big(DEFAULT_EXCHANGE_RATE))
+  },
+  issue: {
+    getDustValue: async () => BitcoinAmount.zero()
+  },
+  redeem: {
+    getMaxBurnableTokens: async (_currency: interBtcApi.CurrencyExt) => DEFAULT_MAX_BURNABLE_TOKENS,
+    getBurnExchangeRate: (collateralCurrency: interBtcApi.CollateralCurrencyExt) =>
+      new ExchangeRate(Bitcoin, collateralCurrency, Big(DEFAULT_BURN_EXCHANGE_RATE))
+  }
+};
+
+jest.mock('@interlay/interbtc-api', () => {
+  const actualInterBtcApi = jest.requireActual('@interlay/interbtc-api');
+
+  return {
+    ...actualInterBtcApi,
+    createInterBtcApi: jest.fn(async (..._argv) => mockedApiValue as InterBtcApi)
+  };
+});
 const mockedApi = mocked(interBtcApi);
 
 const mockProvider = () => {
@@ -22,18 +74,9 @@ type RecursivePartial<T> = {
   [P in keyof T]?: RecursivePartial<T[P]>;
 };
 
-const mockedApiValue: RecursivePartial<InterBtcApi> = {
-  tokens: {
-    total: async (currency) => newMonetaryAmount('1000000000000', currency)
-  },
-  redeem: {
-    getMaxBurnableTokens: async () => new BitcoinAmount('100000000')
-  }
-};
-
 // Replace provider with mock provider to not connect
-mockedApi.createInterBtcApi.mockResolvedValue(mockedApiValue as InterBtcApi);
-// mockedApi.createInterBtcApi.mockResolvedValue();
+// mockedApi.createInterBtcApi.mockImplementation(async (..._) => mockedApiValue as InterBtcApi);
+
 describe('Burn page', () => {
   beforeEach(async () => {
     // TODO: mock the lib, so there's liquidated vault
@@ -41,15 +84,19 @@ describe('Burn page', () => {
   });
 
   it('should display burn tab when there is liquidated vault', async () => {
+    // await waitForElementToBeRemoved(async () => await screen.findByRole('progressbar'), { timeout: 10000 });
     render(<App />, { path: '/bridge?tab=burn' });
-    await waitForElementToBeRemoved(async () => await screen.findByRole('progressbar'), { timeout: 10000 });
-    await screen.findByText('Burn');
-    const burnTab = screen.getByText('Burn');
+    const burnTab = await screen.findByText('Burn');
 
     expect(burnTab).toBeVisible();
   });
 
-  it.todo('should burn the IBTC');
+  it('should burn the IBTC', async () => {
+    render(<App />, { path: '/bridge?tab=burn' });
+    const burnTab = await screen.findByText('Burn');
+    userEvent.click(burnTab);
+    await screen.findByText('Please enter the amount');
+  });
 
   it.todo('should display collateral $ value to be received higher than burned $ value');
 });
--- src/__tests__/test-utils.tsx
+++ src/__tests__/test-utils.tsx
@@ -44,4 +44,5 @@ const customRender = (ui: ReactElement, options?: CustomRenderOptions) => {
 };
 
 export * from '@testing-library/react';
+export { default as userEvent } from '@testing-library/user-event';
 export { customRender as render };
