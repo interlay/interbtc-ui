d9c07c94fc328fb6b2f5a8d1f611bb424a825a6f
crypto-engineer <anton@interlay.io>
2022-09-02 06:32:30 +0200

    feat: add SubstrateProvider to SubstrateContextDemo

--- src/pages/SubstrateContextDemo/index.tsx
+++ src/pages/SubstrateContextDemo/index.tsx
@@ -1,6 +1,9 @@
 // ray test touch <
+import { SubstrateProvider } from '@/substrate-lib/substrate-context';
+
+// http://localhost:3000/substrate-context-demo?rpc=wss://rpc.polkadot.io
 const SubstrateContextDemo = (): JSX.Element => {
-  return <>SubstrateContextDemo</>;
+  return <SubstrateProvider>SubstrateContextDemo</SubstrateProvider>;
 };
 
 export default SubstrateContextDemo;
