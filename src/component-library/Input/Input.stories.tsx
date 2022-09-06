3bcb4a679a6ff2db80945cac0ace4de2e3e62760
Rui Simão <rui.daniel.simao@gmail.com>
2022-09-06 11:05:23 +0100

    feat: implement Label and HelperText

--- src/component-library/Input/Input.stories.tsx
+++ src/component-library/Input/Input.stories.tsx
@@ -1,8 +1,32 @@
 import { Meta, Story } from '@storybook/react';
+import { FieldError, useForm } from 'react-hook-form';
 
 import { Input, InputProps } from '.';
 
-const Template: Story<InputProps> = (args) => <Input {...args} />;
+const errorMessages = (error?: FieldError) => (error?.types ? Object.values(error.types).flat() : error?.message);
+
+const Template: Story<InputProps> = (args) => {
+  const { register, formState } = useForm<{ test: string }>({ mode: 'onChange', criteriaMode: 'all' });
+  return (
+    <Input
+      {...args}
+      {...register('test', {
+        minLength: {
+          value: 100,
+          message: 'faul'
+        },
+        maxLength: {
+          value: 1,
+          message: 'This input exceed maxLength.'
+        },
+        validate: {
+          a: () => false || 'Error message'
+        }
+      })}
+      errorMessage={errorMessages(formState.errors.test)}
+    />
+  );
+};
 
 const Default = Template.bind({});
 Default.args = {
