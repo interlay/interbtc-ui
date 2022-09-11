7e184ff08363632b542cd87d1d8fd75a5f20efc1
crypto-engineer <anton@interlay.io>
2022-09-11 12:14:09 +0200

    fix: update SubstrateLoadingAndErrorHandlingWrapper

--- src/substrate-lib/components/SubstrateLoadingAndErrorHandlingWrapper/index.tsx
+++ src/substrate-lib/components/SubstrateLoadingAndErrorHandlingWrapper/index.tsx
@@ -3,16 +3,7 @@ import { toast, ToastContainer } from 'react-toastify';
 
 import { isBridgeLoaded } from '@/common/actions/general.actions';
 import InterlayHelmet from '@/parts/InterlayHelmet';
-import {
-  // ray test touch <
-  // ActionType,
-  // ray test touch >
-  ApiStatus,
-  // ray test touch <
-  // KeyringStatus,
-  // ray test touch >
-  useSubstrateState
-} from '@/substrate-lib/substrate-context';
+import { ActionType, ApiStatus, KeyringStatus, useSubstrateState } from '@/substrate-lib/substrate-context';
 
 interface SubstrateLoadingAndErrorHandlingWrapperProps {
   children: React.ReactNode;
@@ -23,15 +14,7 @@ const SubstrateLoadingAndErrorHandlingWrapper = ({
 }: SubstrateLoadingAndErrorHandlingWrapperProps): JSX.Element => {
   const dispatch = useDispatch();
 
-  const {
-    apiStatus,
-    apiError,
-    // ray test touch <
-    // keyringStatus,
-    // keyring,
-    // ray test touch >
-    api
-  } = useSubstrateState();
+  const { apiStatus, apiError, keyringStatus, keyring, api } = useSubstrateState();
 
   switch (apiStatus) {
     case ApiStatus.Idle:
@@ -59,29 +42,25 @@ const SubstrateLoadingAndErrorHandlingWrapper = ({
       throw new Error('Invalid ApiStatus!');
   }
 
-  // ray test touch <
-  // switch (keyringStatus) {
-  //   case KeyringStatus.Idle:
-  //   case KeyringStatus.Loading:
-  //     // TODO: improve styling
-  //     return <>Loading accounts (please review any extension&apos;s authorization)</>;
-  //   case KeyringStatus.Ready:
-  //     break;
-  //   case KeyringStatus.Error:
-  //     throw new Error(`${ActionType.SetKeyringError}!`);
-  //   default:
-  //     throw new Error('Invalid KeyringStatus!');
-  // }
-  // ray test touch >
+  switch (keyringStatus) {
+    case KeyringStatus.Idle:
+    case KeyringStatus.Loading:
+      // TODO: improve styling
+      return <>Loading accounts (please review any extension&apos;s authorization)</>;
+    case KeyringStatus.Ready:
+      break;
+    case KeyringStatus.Error:
+      throw new Error(`${ActionType.SetKeyringError}!`);
+    default:
+      throw new Error('Invalid KeyringStatus!');
+  }
 
-  // ray test touch <
-  // if (keyring === undefined || api === undefined) {
-  //   throw new Error('Something went wrong!');
-  // }
+  if (keyring === undefined || api === undefined) {
+    throw new Error('Something went wrong!');
+  }
   if (api === undefined) {
     throw new Error('Something went wrong!');
   }
-  // ray test touch >
 
   return (
     <>
