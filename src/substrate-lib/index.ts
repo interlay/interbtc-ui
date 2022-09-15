43e4ba384293e994f28abc6c28cd5626041ed046
crypto-engineer <anton@interlay.io>
2022-09-15 09:55:12 +0200

    chore: clean up

--- src/substrate-lib/index.ts
+++ src/substrate-lib/index.ts
@@ -1,6 +1,4 @@
-// ray test touch <
 import { SubstrateProvider, useSubstrate, useSubstrateState } from './substrate-context';
 import utils from './utils';
 
 export { SubstrateProvider, useSubstrate, useSubstrateState, utils };
-// ray test touch >
