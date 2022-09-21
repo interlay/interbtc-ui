18c4ddb768c675e93897562ad7aff061fbd4e1af
crypto-engineer <anton@interlay.io>
2022-09-21 09:08:10 +0200

    chore: clean up

--- src/substrate-lib/index.ts
+++ src/substrate-lib/index.ts
@@ -1,4 +1,4 @@
 import { SubstrateProvider, useSubstrate, useSubstrateState } from './substrate-context';
 import utils from './utils';
 
-export { SubstrateProvider, useSubstrate, useSubstrateState, utils };
+export { SubstrateProvider, utils as substrateUtils, useSubstrate, useSubstrateState };
