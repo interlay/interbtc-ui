0c38e096e1a8d2643baed16ff3becaed0675b911
Rui Simão <rui.daniel.simao@gmail.com>
2022-09-24 17:38:30 +0100

    fix(Text): types

--- src/component-library/Text/Strong/Strong.tsx
+++ src/component-library/Text/Strong/Strong.tsx
@@ -1,7 +1,7 @@
 import { Text } from '../style';
 import { TextProps } from '../types';
 
-const Strong = ({ color, size, children, ...props }: TextProps<HTMLHeadingElement>): JSX.Element => (
+const Strong = ({ color, size, children, ...props }: TextProps<HTMLElement>): JSX.Element => (
   <Text as='strong' $color={color} $size={size} {...props}>
     {children}
   </Text>
