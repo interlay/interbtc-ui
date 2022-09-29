36a82f6318443562950d28efa5c1db833e48be6f
Thomas Jeatt <hello@tomjeatt.com>
2022-09-29 12:05:25 +0100

    refactor: implicit return

--- src/utils/hooks/api/use-get-collateral-currencies.tsx
+++ src/utils/hooks/api/use-get-collateral-currencies.tsx
@@ -1,10 +1,8 @@
 import { CollateralCurrencyExt, getCollateralCurrencies } from '@interlay/interbtc-api';
 import { useQuery, UseQueryResult } from 'react-query';
 
-const getCurrencies = async (): Promise<Array<CollateralCurrencyExt>> => {
-  const collateralCurrencies = await getCollateralCurrencies(window.bridge.api, window.bridge.assetRegistry);
-  return [...collateralCurrencies];
-};
+const getCurrencies = async (): Promise<Array<CollateralCurrencyExt>> =>
+  getCollateralCurrencies(window.bridge.api, window.bridge.assetRegistry);
 
 const useGetCollateralCurrencies = (bridgeLoaded: boolean): UseQueryResult<Array<CollateralCurrencyExt>> => {
   return useQuery({ queryKey: 'getCollateralCurrencies', queryFn: getCurrencies, enabled: bridgeLoaded });
