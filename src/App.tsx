d7920fe11f0de7e48073493c52e07c9030050d9c
Daniel Simão <rui.daniel.simao@gmail.com>
2022-10-04 10:23:23 +0100

    feat(Interlend): add layout and navigation (#546)

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ const Dashboard = React.lazy(() => import(/* webpackChunkName: 'dashboard' */ '@
 const Vaults = React.lazy(() => import(/* webpackChunkName: 'vaults' */ '@/pages/Vaults'));
 // TODO: last task will be to delete legacy dashboard and rename vault dashboard
 const Vault = React.lazy(() => import(/* webpackChunkName: 'vault' */ '@/pages/Vaults/Vault'));
+const Loans = React.lazy(() => import(/* webpackChunkName: 'loans' */ '@/pages/Loans'));
 const NoMatch = React.lazy(() => import(/* webpackChunkName: 'no-match' */ '@/pages/NoMatch'));
 
 type UnsubscriptionRef = (() => void) | null;
@@ -404,6 +405,9 @@ const App = (): JSX.Element => {
                   </Route>
                   <Route path={PAGES.TRANSFER}>
                     <Transfer />
+                  </Route>
+                  <Route path={PAGES.LOANS}>
+                    <Loans />
                   </Route>
                   <Redirect exact from={PAGES.HOME} to={PAGES.BRIDGE} />
                   <Route path='*'>
