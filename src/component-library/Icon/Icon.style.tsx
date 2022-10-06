79e1e64d5089cece8e0ce00726c4d7b53f30dab7
Daniel Simão <rui.daniel.simao@gmail.com>
2022-10-06 16:52:17 +0100

    feat(Loans): add loans overview page and lend components (#552)

--- src/component-library/Icon/Icon.style.tsx
+++ src/component-library/Icon/Icon.style.tsx
@@ -3,6 +3,8 @@ import { ReactComponent as CheckCircleIcon } from '@material-icons/svg/svg/check
 import { ReactComponent as CloseIcon } from '@material-icons/svg/svg/close/baseline.svg';
 import styled from 'styled-components';
 
+import { ReactComponent as ChevronRight } from '@/assets/img/icons/chevron-right.svg';
+
 import { theme } from '../theme';
 
 const BaseCloseIcon = styled(CloseIcon)`
@@ -19,4 +21,7 @@ const BaseCheckCircleIcon = styled(CheckCircleIcon)`
   fill: ${theme.colors.textSecondary};
 `;
 
-export { BaseCheckCircleIcon, BaseCheckmarkIcon, BaseCloseIcon };
+// TODO: our icon component should not add a fixed width, height, fill etc..., all of that should be customizable
+const BaseChevronRight = styled(ChevronRight)``;
+
+export { BaseCheckCircleIcon, BaseCheckmarkIcon, BaseChevronRight, BaseCloseIcon };
--- src/pages/Loans/LoansOverview/LoansOverview.tsx
+++ src/pages/Loans/LoansOverview/LoansOverview.tsx
@@ -1,14 +1,29 @@
-import { H1, P } from '@/component-library';
+import { useTranslation } from 'react-i18next';
+
+import { H1, P, Stack } from '@/component-library';
 import MainContainer from '@/parts/MainContainer';
+import { useGetLoansData } from '@/utils/hooks/api/loans/use-get-loans-data';
+
+import { LoansInsights, LoansMarkets } from './components';
 
 const LoansOverview = (): JSX.Element => {
+  const { t } = useTranslation();
+  const { positions, supply } = useGetLoansData();
+  const { apyEarned, borrow: borrowBalance, loanStatus, supply: supplyBalance } = positions;
+
   return (
     <MainContainer>
-      <H1>Interlend</H1>
-      <P>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys
-        standard dummy text ever since the 1500s,{' '}
-      </P>
+      <H1>{t('loans.brand_name')}</H1>
+      <Stack spacing='double'>
+        <Stack>
+          <P>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industrys standard dummy text ever since the 1500s,{' '}
+          </P>
+          <LoansInsights apyEarned={apyEarned} borrow={borrowBalance} supply={supplyBalance} loanStatus={loanStatus} />
+        </Stack>
+        <LoansMarkets borrow={supply.assets} supply={supply} />
+      </Stack>
     </MainContainer>
   );
 };
