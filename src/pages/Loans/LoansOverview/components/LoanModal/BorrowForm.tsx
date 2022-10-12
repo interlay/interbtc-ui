f4f101f3521d9429c8610f88aa94543004795d8e
Peter Slaný <peter@interlay.io>
2022-10-12 15:22:18 +0200

    feat: add borrow limit and utils to compute it quickly

--- src/pages/Loans/LoansOverview/components/LoanModal/BorrowForm.tsx
+++ src/pages/Loans/LoansOverview/components/LoanModal/BorrowForm.tsx
@@ -1,9 +1,14 @@
 import { LoanAsset } from '@interlay/interbtc-api';
+import { MonetaryAmount } from '@interlay/monetary-js';
 import { useId } from '@react-aria/utils';
+import Big from 'big.js';
+import { useState } from 'react';
 import { TFunction, useTranslation } from 'react-i18next';
 
+import { formatNumber, formatUSD } from '@/common/utils/utils';
 import { CTA, H3, P, Stack, TokenInput } from '@/component-library';
 import { BorrowAction } from '@/pages/Loans/types';
+import { useGetAccountLoansOverview } from '@/utils/hooks/api/loans/use-get-account-loans-overview';
 
 import { StyledDItem, StyledDl } from './LoanModal.style';
 
@@ -25,31 +30,49 @@ const BorrowForm = ({ asset, variant }: BorrowFormProps): JSX.Element => {
   const titleId = useId();
   const { t } = useTranslation();
   const content = getContentMap(t)[variant];
+  const { borrowLimitUSDValue, getNewBorrowLimitUSDValue } = useGetAccountLoansOverview();
+
+  const [newBorrowLimit, setNewBorrowLimit] = useState<Big>(borrowLimitUSDValue || Big(0));
+
+  const handleInputAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const transactionType = variant === 'borrow' ? 'withdraw' : 'deposit';
+    const monetaryAmount = new MonetaryAmount(asset.currency, event.target.value || 0);
+    const newBorrowLimit = getNewBorrowLimitUSDValue(transactionType, asset.currency, monetaryAmount);
+    setNewBorrowLimit(newBorrowLimit || Big(0));
+  };
 
   return (
     <Stack spacing='double'>
       <div>
         <H3 id={titleId}>
-          {content.title} {asset.currency}
+          {content.title} {asset.currency.name}
         </H3>
         <P>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</P>
       </div>
       <Stack>
-        <TokenInput valueInUSD='$0.00' tokenSymbol={asset.currency.ticker} balance={100} balanceInUSD={100} />
+        <TokenInput
+          onChange={handleInputAmountChange}
+          valueInUSD='$0.00'
+          tokenSymbol={asset.currency.ticker}
+          balance={100}
+          balanceInUSD={100}
+        />
         <StyledDl>
           <StyledDItem>
             <dt>APY</dt>
-            <dd>11.84%</dd>
+            <dd>{formatNumber(asset.borrowApy.toNumber())}%</dd>
           </StyledDItem>
-          {variant === 'borrow' && (
+          {variant === 'borrow' && asset.borrowReward && (
             <StyledDItem>
-              <dt>INTR Rewards?</dt>
-              <dd>14.14%</dd>
+              <dt>{asset.borrowReward.currency.ticker} Rewards</dt>
+              <dd>{formatNumber(asset.borrowReward.apy.toNumber())}%</dd>
             </StyledDItem>
           )}
           <StyledDItem>
             <dt>Borrow Limit</dt>
-            <dd>$0.00 - $45.10</dd>
+            <dd>
+              {formatUSD(borrowLimitUSDValue?.toNumber() || 0)} -&gt; {formatUSD(newBorrowLimit.toNumber())}
+            </dd>
           </StyledDItem>
         </StyledDl>
         <CTA size='large'>{content.title}</CTA>
