ba896f7f12184e93b6313ba181752bb80f4f4fdb
Peter Slaný <peter@interlay.io>
2022-10-12 15:22:18 +0200

    feat: add borrow limit and utils to compute it quickly

--- src/utils/hooks/api/loans/use-get-account-loans-overview.tsx
+++ src/utils/hooks/api/loans/use-get-account-loans-overview.tsx
@@ -17,13 +17,22 @@ interface AccountLoansOverview {
   lentAssetsUSDValue: Big | undefined;
   totalEarnedInterestUSDValue: Big | undefined;
   borrowedAssetsUSDValue: Big | undefined;
+  borrowLimitUSDValue: Big | undefined;
   collateralRatio: Big | undefined;
   getNewCollateralRatio: (
-    type: 'borrow' | 'supply',
+    type: 'withdraw' | 'deposit',
     currency: CurrencyExt,
     amount: MonetaryAmount<CurrencyExt>
   ) => Big | undefined;
-  getMaxBorrowableAmount: (currency: CurrencyExt) => MonetaryAmount<CurrencyExt> | undefined;
+  getNewBorrowLimitUSDValue: (
+    type: 'withdraw' | 'deposit',
+    currency: CurrencyExt,
+    amount: MonetaryAmount<CurrencyExt>
+  ) => Big | undefined;
+  getMaxBorrowableAmount: (
+    currency: CurrencyExt,
+    availableCapacity: MonetaryAmount<CurrencyExt>
+  ) => MonetaryAmount<CurrencyExt> | undefined;
 }
 
 const useGetAccountLoansOverview = (): AccountLoansOverview => {
@@ -55,17 +64,21 @@ const useGetAccountLoansOverview = (): AccountLoansOverview => {
     collateralRatio = collateralAssetsUSDValue.gt(0) ? borrowedAssetsUSDValue.div(collateralAssetsUSDValue) : Big(0);
   }
 
+  const borrowLimitUSDValue = collateralAssetsUSDValue
+    ? collateralAssetsUSDValue.sub(borrowedAssetsUSDValue || 0)
+    : Big(0);
+
   /**
    * This method computes how the collateral ratio will change if
-   * asset is borrowed or supplied.
-   * @param type Type of transaction that will be done: 'supply' or 'borrow'.
-   * @param currency Currency which will be supplied or borrowed.
+   * asset is withdrawn or deposited.
+   * @param type Type of transaction that will be done: 'deposit' or 'withdraw'.
+   * @param currency Currency which will be deposited or withdrawn.
    * @param amount Amount of `currency` that will be used.
    * @note Call only after the prices and positions are loaded.
-   * @returns New collateral ratio after the supplying or borrowing is done.
+   * @returns New collateral ratio after the transaction is done.
    */
   const getNewCollateralRatio = useCallback(
-    (type: 'borrow' | 'supply', currency: CurrencyExt, amount: MonetaryAmount<CurrencyExt>): Big | undefined => {
+    (type: 'withdraw' | 'deposit', currency: CurrencyExt, amount: MonetaryAmount<CurrencyExt>): Big | undefined => {
       if (prices === undefined || borrowedAssetsUSDValue === undefined || collateralAssetsUSDValue === undefined) {
         return undefined;
       }
@@ -75,10 +88,10 @@ const useGetAccountLoansOverview = (): AccountLoansOverview => {
       const amountUSDValue = Big(convertMonetaryAmountToValueInUSD(amount, currencyPrice) || 0);
 
       const newBorrowedAssetsUSDValue =
-        type === 'borrow' ? borrowedAssetsUSDValue.add(amountUSDValue) : borrowedAssetsUSDValue;
+        type === 'withdraw' ? borrowedAssetsUSDValue.add(amountUSDValue) : borrowedAssetsUSDValue;
 
       const newCollateralAssetsUSDValue =
-        type === 'supply' ? collateralAssetsUSDValue.add(amountUSDValue) : collateralAssetsUSDValue;
+        type === 'deposit' ? collateralAssetsUSDValue.add(amountUSDValue) : collateralAssetsUSDValue;
 
       return newCollateralAssetsUSDValue.gt(0) ? newCollateralAssetsUSDValue.div(newBorrowedAssetsUSDValue) : Big(0);
     },
@@ -86,13 +99,47 @@ const useGetAccountLoansOverview = (): AccountLoansOverview => {
   );
 
   /**
+   * This method computes how the borrow limit will change if
+   * asset is withdrawn or deposited to protocol.
+   * @param type Type of transaction to be done: 'withdraw' or 'deposit'.
+   * @param currency Currency which will be deposited or withdrawn.
+   * @param amount Amount of `currency` that will be used.
+   * @note Call only after the prices and positions are loaded.
+   * @returns New borrow limit in USD after the transaction is done.
+   */
+  const getNewBorrowLimitUSDValue = useCallback(
+    (type: 'withdraw' | 'deposit', currency: CurrencyExt, amount: MonetaryAmount<CurrencyExt>): Big | undefined => {
+      if (prices === undefined || borrowedAssetsUSDValue === undefined || collateralAssetsUSDValue === undefined) {
+        return undefined;
+      }
+
+      // TODO: Remove type casting after useGetPrices hook is refactored.
+      const currencyPrice = getTokenPrice(prices, currency.ticker as CurrencyIdLiteral)?.usd;
+      const amountUSDValue = Big(convertMonetaryAmountToValueInUSD(amount, currencyPrice) || 0);
+
+      const newBorrowedAssetsUSDValue =
+        type === 'withdraw' ? borrowedAssetsUSDValue.add(amountUSDValue) : borrowedAssetsUSDValue;
+
+      const newCollateralAssetsUSDValue =
+        type === 'deposit' ? collateralAssetsUSDValue.add(amountUSDValue) : collateralAssetsUSDValue;
+
+      return newCollateralAssetsUSDValue.sub(newBorrowedAssetsUSDValue || 0);
+    },
+    [prices, borrowedAssetsUSDValue, collateralAssetsUSDValue]
+  );
+
+  /**
    * Get maximum amount of currency that user can borrow with currently provided collateral.
-   * @param currency Currency
+   * @param currency Currency of which max borrowable amount to get.
+   * @param availableCapacity Total capacity that can be borrowed from the protocol.
    * @returns maximum amount of currency that user can borrow with currently provided collateral.
    * @returns undefined if prices and assets are not loaded yet
    */
   const getMaxBorrowableAmount = useCallback(
-    (currency: CurrencyExt): MonetaryAmount<CurrencyExt> | undefined => {
+    (
+      currency: CurrencyExt,
+      availableCapacity: MonetaryAmount<CurrencyExt>
+    ): MonetaryAmount<CurrencyExt> | undefined => {
       if (collateralAssetsUSDValue === undefined || borrowedAssetsUSDValue === undefined || prices === undefined) {
         return undefined;
       }
@@ -107,7 +154,11 @@ const useGetAccountLoansOverview = (): AccountLoansOverview => {
       const availableCollateralUSDValue = collateralAssetsUSDValue.sub(borrowedAssetsUSDValue);
       const maxBorrowableCurrencyAmount = availableCollateralUSDValue.div(currencyUSDPrice);
 
-      return new MonetaryAmount(currency, maxBorrowableCurrencyAmount);
+      const maxBorrowableAmountByCollateral =
+        new MonetaryAmount(currency, maxBorrowableCurrencyAmount) || new MonetaryAmount(currency, 0);
+      return availableCapacity.gt(maxBorrowableAmountByCollateral)
+        ? maxBorrowableAmountByCollateral
+        : availableCapacity;
     },
     [collateralAssetsUSDValue, borrowedAssetsUSDValue, prices]
   );
@@ -118,8 +169,10 @@ const useGetAccountLoansOverview = (): AccountLoansOverview => {
     lentAssetsUSDValue,
     totalEarnedInterestUSDValue,
     borrowedAssetsUSDValue,
+    borrowLimitUSDValue,
     collateralRatio,
     getNewCollateralRatio,
+    getNewBorrowLimitUSDValue,
     getMaxBorrowableAmount
   };
 };
