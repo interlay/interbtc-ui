9d1cb5dbd642eeec47171ff6573cd90d4f8797a8
crypto-engineer <anton@interlay.io>
2022-10-13 17:32:03 +0200

    feat: update AccountModalContentWrapper

--- src/parts/AccountModal/ModalContent/AccountModalContentWrapper.tsx
+++ src/parts/AccountModal/ModalContent/AccountModalContentWrapper.tsx
@@ -1,8 +1,17 @@
 import clsx from 'clsx';
 import * as React from 'react';
+// ray test touch <
+import { Trans } from 'react-i18next';
 
+// ray test touch >
 import CloseIconButton from '@/components/buttons/CloseIconButton';
+// ray test touch <
+import InterlayLink from '@/components/UI/InterlayLink';
+// ray test touch >
 import { InterlayModalTitle } from '@/components/UI/InterlayModal';
+// ray test touch <
+import { TERMS_AND_CONDITIONS_LINK } from '@/config/relay-chains';
+// ray test touch >
 
 interface AccountContentWrapperProps {
   title: string;
@@ -22,6 +31,18 @@ const AccountModalContentWrapper = ({
       {title}
     </InterlayModalTitle>
     <CloseIconButton ref={focusRef} onClick={onClose} />
+    {/* ray test touch < */}
+    <p className={clsx('my-4', 'text-sm')}>
+      <Trans i18nKey='exclude_us_citizens'>
+        By proceeding you confirm that you have read and accepted the{' '}
+        <InterlayLink className='underline' target='_blank' rel='noreferrer' href={TERMS_AND_CONDITIONS_LINK}>
+          terms and conditions
+        </InterlayLink>
+        , and represent and warrant that you are not a Resident of the United States or a &quot;U.S. person&quot; within
+        the meaning of Rule 902(k) under the United States Securities Act of 1933 (the &quot;Securities Act&quot;).
+      </Trans>
+    </p>
+    {/* ray test touch > */}
     <div className='space-y-4'>{children}</div>
   </>
 );
