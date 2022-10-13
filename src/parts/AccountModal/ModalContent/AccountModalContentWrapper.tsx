ba256452fdbde566b57ecca16f104393698ed212
crypto-engineer <anton@interlay.io>
2022-10-13 17:33:17 +0200

    chore: clean up

--- src/parts/AccountModal/ModalContent/AccountModalContentWrapper.tsx
+++ src/parts/AccountModal/ModalContent/AccountModalContentWrapper.tsx
@@ -1,17 +1,11 @@
 import clsx from 'clsx';
 import * as React from 'react';
-// ray test touch <
 import { Trans } from 'react-i18next';
 
-// ray test touch >
 import CloseIconButton from '@/components/buttons/CloseIconButton';
-// ray test touch <
 import InterlayLink from '@/components/UI/InterlayLink';
-// ray test touch >
 import { InterlayModalTitle } from '@/components/UI/InterlayModal';
-// ray test touch <
 import { TERMS_AND_CONDITIONS_LINK } from '@/config/relay-chains';
-// ray test touch >
 
 interface AccountContentWrapperProps {
   title: string;
@@ -31,7 +25,6 @@ const AccountModalContentWrapper = ({
       {title}
     </InterlayModalTitle>
     <CloseIconButton ref={focusRef} onClick={onClose} />
-    {/* ray test touch < */}
     <p className={clsx('my-4', 'text-sm')}>
       <Trans i18nKey='exclude_us_citizens'>
         By proceeding you confirm that you have read and accepted the{' '}
@@ -42,7 +35,6 @@ const AccountModalContentWrapper = ({
         the meaning of Rule 902(k) under the United States Securities Act of 1933 (the &quot;Securities Act&quot;).
       </Trans>
     </p>
-    {/* ray test touch > */}
     <div className='space-y-4'>{children}</div>
   </>
 );
