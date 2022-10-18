fa2c839d911346c949ed233d54f6bc5f7959e562
Peter Slaný <peter@interlay.io>
2022-10-18 13:36:32 +0200

    feat(interlend): display available balance, lent amount, capacity and borrowed amount in loan modals

--- src/pages/Loans/LoansOverview/components/LoanModal/LoanModal.tsx
+++ src/pages/Loans/LoansOverview/components/LoanModal/LoanModal.tsx
@@ -1,4 +1,4 @@
-import { LoanAsset, LoanPosition } from '@interlay/interbtc-api';
+import { LendPosition, LoanAsset, LoanPosition } from '@interlay/interbtc-api';
 import { useTranslation } from 'react-i18next';
 
 import { Modal, ModalProps, Tabs, TabsItem } from '@/component-library';
@@ -10,14 +10,14 @@ import { LendForm } from './LendForm';
 type Props = {
   variant: LoanType;
   asset?: LoanAsset;
-  position?: LoanPosition;
+  position: LoanPosition | undefined;
 };
 
 type InheritAttrs = Omit<ModalProps, keyof Props | 'children'>;
 
 type LoanModalProps = Props & InheritAttrs;
 
-const LoanModal = ({ variant = 'lend', asset, ...props }: LoanModalProps): JSX.Element | null => {
+const LoanModal = ({ variant = 'lend', asset, position, ...props }: LoanModalProps): JSX.Element | null => {
   const { t } = useTranslation();
 
   if (!asset) {
@@ -29,10 +29,10 @@ const LoanModal = ({ variant = 'lend', asset, ...props }: LoanModalProps): JSX.E
       <Modal {...props}>
         <Tabs>
           <TabsItem title={t('loans.borrow')}>
-            <BorrowForm asset={asset} variant='borrow' />
+            <BorrowForm asset={asset} variant='borrow' position={position} />
           </TabsItem>
           <TabsItem title={t('loans.repay')}>
-            <BorrowForm asset={asset} variant='repay' />
+            <BorrowForm asset={asset} variant='repay' position={position} />
           </TabsItem>
         </Tabs>
       </Modal>
@@ -43,10 +43,10 @@ const LoanModal = ({ variant = 'lend', asset, ...props }: LoanModalProps): JSX.E
     <Modal {...props}>
       <Tabs>
         <TabsItem title={t('loans.lend')}>
-          <LendForm asset={asset} variant='lend' />
+          <LendForm asset={asset} variant='lend' position={position as LendPosition} />
         </TabsItem>
         <TabsItem title={t('loans.withdraw')}>
-          <LendForm asset={asset} variant='withdraw' />
+          <LendForm asset={asset} variant='withdraw' position={position as LendPosition} />
         </TabsItem>
       </Tabs>
     </Modal>
