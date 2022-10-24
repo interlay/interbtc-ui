2f1fd56c74255d67bc98842171fd9c83d2b05eef
Rui Simão <rui.daniel.simao@gmail.com>
2022-10-24 14:22:14 +0100

    fix: code review

--- src/lib/form-validation/types.ts
+++ src/lib/form-validation/types.ts
@@ -1,6 +1,14 @@
 import { TFunction } from 'i18next';
 import * as z from 'zod';
 
+/**
+ * This type contains the structure of a validation
+ * @function validate contains one or multiple conditions
+ * that should return true if `params` respect condition restrictions
+ * @function issue should return the necessary meta data for zod `addIssue`
+ * and it's here also where error `message` is declared
+ * @note This kind of validation should only be used in reusable validations
+ */
 type Validation<V = unknown, I = unknown> = {
   validate: (params: V) => boolean;
   issue: (t: TFunction, params?: I) => z.IssueData;
