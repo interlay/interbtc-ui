f4ac74cdf88527bffef4804d35bf88d70adfc349
crypto-engineer <anton@interlay.io>
2022-10-29 08:32:26 +0200

    refactor: update getExchangeRate

--- src/utils/helpers/oracle.ts
+++ src/utils/helpers/oracle.ts
@@ -1,15 +1,21 @@
 // ray test touch <
 import { CurrencyExt } from '@interlay/interbtc-api';
-import { Currency, ExchangeRate } from '@interlay/monetary-js';
+import { Bitcoin, Currency, ExchangeRate } from '@interlay/monetary-js';
 
-import { GOVERNANCE_TOKEN_SYMBOL } from '@/config/relay-chains';
+import { GOVERNANCE_TOKEN_SYMBOL, RELAY_CHAIN_NATIVE_TOKEN_SYMBOL } from '@/config/relay-chains';
 
-const getExchangeRate = async (token: CurrencyExt): Promise<ExchangeRate<Currency, CurrencyExt>> => {
-  if (token.ticker === GOVERNANCE_TOKEN_SYMBOL) {
-    throw new Error(`No exchange rate for given currency: ${token.ticker}`);
+const getExchangeRate = async (
+  collateralCurrency: CurrencyExt,
+  wrappedCurrency?: Bitcoin
+): Promise<ExchangeRate<Currency, CurrencyExt>> => {
+  if (collateralCurrency.ticker === GOVERNANCE_TOKEN_SYMBOL) {
+    throw new Error(`No exchange rate for given currency: ${collateralCurrency.ticker}`);
+  }
+  if (collateralCurrency.ticker === RELAY_CHAIN_NATIVE_TOKEN_SYMBOL) {
+    throw new Error(`No exchange rate for given currency: ${collateralCurrency.ticker}`);
   }
 
-  return await window.bridge.oracle.getExchangeRate(token);
+  return await window.bridge.oracle.getExchangeRate(collateralCurrency, wrappedCurrency);
 };
 
 export { getExchangeRate };
