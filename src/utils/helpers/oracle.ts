2efa5fa5950fe6157cfc4d6afabba03ca473f7d0
crypto-engineer <anton@interlay.io>
2022-11-01 16:14:34 +0100

    docs: add a comment

--- src/utils/helpers/oracle.ts
+++ src/utils/helpers/oracle.ts
@@ -1,6 +1,7 @@
 import { CurrencyExt } from '@interlay/interbtc-api';
 import { Bitcoin, Currency, ExchangeRate } from '@interlay/monetary-js';
 
+// This is for simulating when the oracle is down.
 // import { GOVERNANCE_TOKEN_SYMBOL, RELAY_CHAIN_NATIVE_TOKEN_SYMBOL } from '@/config/relay-chains';
 
 const getExchangeRate = async (
