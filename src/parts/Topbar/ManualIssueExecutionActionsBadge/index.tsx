b38ee11ab5001c562356ea8d1fba7f1408badb52
crypto-engineer <anton@interlay.io>
2022-11-10 21:16:18 +0100

    chore: clean up

--- src/parts/Topbar/ManualIssueExecutionActionsBadge/index.tsx
+++ src/parts/Topbar/ManualIssueExecutionActionsBadge/index.tsx
@@ -5,14 +5,10 @@ import ErrorFallback from '@/components/ErrorFallback';
 import { useManualIssueRequests } from '@/services/hooks/issue-requests';
 import { PAGES } from '@/utils/constants/links';
 
-// ray test touch <
 // TODO:
 // - Follow the same folder structures as `component-library`.
-// - active/inactive behavior
 // - Make it close to its design.
-// ray test touch >
 const ManualIssueExecutionActionsBadge = (): JSX.Element => {
-  // ray test touch <
   const {
     isIdle: manualIssueRequestsIdle,
     isLoading: manualIssueRequestsLoading,
@@ -24,7 +20,6 @@ const ManualIssueExecutionActionsBadge = (): JSX.Element => {
   const disabled = manualIssueRequestsIdle || manualIssueRequestsLoading || manualIssueRequests?.length === 0;
 
   const countOfActionsLabel = manualIssueRequests ? manualIssueRequests.length : '-';
-  // ray test touch >
 
   return (
     <CTALink variant='outlined' to={PAGES.ACTIONS} disabled={disabled}>
