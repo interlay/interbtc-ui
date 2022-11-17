006b5a7b2eb801e5da6c29b2a84cfa0116944c5c
crypto-engineer <anton@interlay.io>
2022-11-17 04:04:15 +0100

    refactor: use screen.getByRole rather than screen.findByText

--- src/test/pages/Issue.test.tsx
+++ src/test/pages/Issue.test.tsx
@@ -4,11 +4,21 @@ import App from '@/App';
 
 import { render, screen } from '../test-utils';
 
+// ray test touch <
+const path = '/bridge?tab=issue';
+// ray test touch >
+
 describe('Issue page', () => {
   it('should display issue tab', async () => {
-    await render(<App />, { path: '/bridge?tab=issue' });
+    await render(<App />, { path });
 
-    const issueTab = await screen.findByText(/Issue/i);
+    const issueTab = screen.getByRole('tab', { name: /issue/i });
     expect(issueTab).toBeVisible();
   });
+
+  // ray test touch <
+  // it('should issue the IBTC', async () => {
+  //   await render(<App />, { path });
+  // });
+  // ray test touch >
 });
