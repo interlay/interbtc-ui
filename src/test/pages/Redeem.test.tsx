b3cab6b5b7004181f10535968ced9c4869d1c523
crypto-engineer <anton@interlay.io>
2022-11-17 10:02:49 +0100

    test: update Redeem test

--- src/test/pages/Redeem.test.tsx
+++ src/test/pages/Redeem.test.tsx
@@ -1,16 +1,31 @@
-// ray test touch <
 import '@testing-library/jest-dom';
 
 import App from '@/App';
 
-import { render, screen } from '../test-utils';
+import { render, screen, userEvent } from '../test-utils';
 
-describe('Redeem page', () => {
+const path = '/bridge?tab=redeem';
+
+describe('redeemTab page', () => {
   it('should display the redeem tab', async () => {
-    await render(<App />, { path: '/bridge?tab=redeem' });
+    await render(<App />, { path });
 
     const redeemTab = screen.getByRole('tab', { name: /redeem/i });
     expect(redeemTab).toBeVisible();
   });
+
+  it('should redeem the IBTC', async () => {
+    await render(<App />, { path });
+
+    const redeemTab = screen.getByRole('tab', { name: /redeem/i });
+    userEvent.click(redeemTab);
+
+    // ray test touch <
+    // Input 0.0001 IBTC
+    // const amountToRedeemInput = await screen.findByRole('textbox');
+    // await act(async () => {
+    //   userEvent.type(amountToRedeemInput, '0.0001');
+    // });
+    // ray test touch >
+  });
 });
-// ray test touch >
