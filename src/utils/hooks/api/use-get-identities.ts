ad01ed68d6f0d295600243aeda0a9a8e820236f1
Thomas Jeatt <hello@tomjeatt.com>
2022-11-18 12:37:41 +0000

    chore: bump dependencies and fix breaking change

--- src/utils/hooks/api/use-get-identities.ts
+++ src/utils/hooks/api/use-get-identities.ts
@@ -11,7 +11,10 @@ const getIdentities = async (): Promise<Map<string, string>> => {
   const accountDisplayMapping: Map<string, string> = new Map();
   identities.forEach((identity) => {
     const formattedAccountId = encodeAddress(identity[0].args.toString(), SS58_FORMAT);
-    const registration: Registration = identity[1].unwrap();
+    // This is a workaround for a TS error triggered by a missing type. We should
+    // move this method to the lib where the generated types already exists.
+    const castIdentity = identity[1] as any;
+    const registration: Registration = castIdentity.unwrap();
     const displayName = registration.info.display.asRaw.toHuman()?.toString();
     accountDisplayMapping.set(formattedAccountId, displayName ? displayName : '');
   });
