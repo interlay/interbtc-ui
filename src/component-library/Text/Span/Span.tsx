8d4bc208b3dee0fb6a40841475a1f30fa9fcd56b
Daniel Simão <rui.daniel.simao@gmail.com>
2022-11-24 10:47:29 +0000

    feat(Tooltip): add component (#697)

--- src/component-library/Text/Span/Span.tsx
+++ src/component-library/Text/Span/Span.tsx
@@ -1,10 +1,14 @@
+import { forwardRef } from 'react';
+
 import { TextProps } from '../types';
 import { SpanText } from './Span.style';
 
-const Span = ({ color, children, ...props }: TextProps<HTMLElement>): JSX.Element => (
-  <SpanText color={color} {...props}>
-    {children}
-  </SpanText>
+const Span = forwardRef<HTMLElement, TextProps<HTMLElement>>(
+  ({ color, children, ...props }, ref): JSX.Element => (
+    <SpanText ref={ref} color={color} {...props}>
+      {children}
+    </SpanText>
+  )
 );
 
 Span.displayName = 'Span';
