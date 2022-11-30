c0003e29f33ae8b9d5923132f4964ca66b16d5a7
Thomas Jeatt <hello@tomjeatt.com>
2022-11-30 12:09:55 +0000

    chore: remove deprecated xcm api

--- src/utils/relay-chain-api/types.ts
+++
@@ -1,8 +0,0 @@
-import { CollateralCurrencyExt } from '@interlay/interbtc-api';
-import { MonetaryAmount } from '@interlay/monetary-js';
-import { ApiPromise } from '@polkadot/api';
-
-type RelayChainApi = ApiPromise;
-type RelayChainMonetaryAmount = MonetaryAmount<CollateralCurrencyExt>;
-
-export type { RelayChainApi, RelayChainMonetaryAmount };
