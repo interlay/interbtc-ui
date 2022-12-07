432b52a9d5057fb8621ec4084196dcb203529163
crypto-engineer <anton@interlay.io>
2022-12-07 10:10:26 +0100

    test: remove a redundant test case

--- src/test/pages/Issue.test.tsx
+++ src/test/pages/Issue.test.tsx
@@ -5,18 +5,9 @@ import App from '@/App';
 import { mockIssueRequest } from '../mocks/@interlay/interbtc-api';
 import { act, render, screen, userEvent, waitFor } from '../test-utils';
 
-const path = '/bridge?tab=issue';
-
 describe('Issue page', () => {
-  it('should display the issue tab', async () => {
-    await render(<App />, { path });
-
-    const issueTab = screen.getByRole('tab', { name: /issue/i });
-    expect(issueTab).toBeVisible();
-  });
-
   it('should issue the IBTC', async () => {
-    await render(<App />, { path });
+    await render(<App />, { path: '/bridge?tab=issue' });
 
     const issueTab = screen.getByRole('tab', { name: /issue/i });
     userEvent.click(issueTab);
