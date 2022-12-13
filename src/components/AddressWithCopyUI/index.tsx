1b3277d3a60c1efcaaaef1d78c6dc95b860445a7
crypto-engineer <anton@interlay.io>
2022-12-13 21:30:52 +0100

    feat: update AddressWithCopyUI props

--- src/components/AddressWithCopyUI/index.tsx
+++ src/components/AddressWithCopyUI/index.tsx
@@ -3,14 +3,14 @@ import clsx from 'clsx';
 import { shortAddress } from '@/common/utils/utils';
 import CopyToClipboardButton from '@/components/CopyToClipboardButton';
 
-interface Props {
+interface Props extends React.ComponentPropsWithRef<'div'> {
   address: string;
   className?: string;
 }
 
-const AddressWithCopyUI = ({ address, className }: Props): JSX.Element => {
+const AddressWithCopyUI = ({ address, className, ...rest }: Props): JSX.Element => {
   return (
-    <div className={clsx('flex', 'items-center', 'space-x-1', 'font-medium', className)}>
+    <div className={clsx('flex', 'items-center', 'space-x-1', 'font-medium', className)} {...rest}>
       <span>{shortAddress(address)}</span>
       <CopyToClipboardButton text={address} />
     </div>
