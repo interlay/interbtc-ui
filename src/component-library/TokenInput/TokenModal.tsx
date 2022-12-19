507de3a4fe7c341bef789fa9be62ea1f5aa49c8c
Rui Simão <rui.daniel.simao@gmail.com>
2022-12-19 17:26:46 +0000

    feat: add List

--- src/component-library/TokenInput/TokenModal.tsx
+++ src/component-library/TokenInput/TokenModal.tsx
@@ -1,4 +1,7 @@
+import { Flex } from '../Flex';
+import { List, ListItem } from '../List';
 import { Modal, ModalProps } from '../Modal';
+import { H3, Span } from '../Text';
 
 type CurrencyItem = {
   currency: string;
@@ -15,7 +18,22 @@ type InheritAttrs = Omit<ModalProps, keyof Props | 'children'>;
 
 type TokenModalProps = Props & InheritAttrs;
 
-const TokenModal = ({ ...props }: TokenModalProps): JSX.Element => <Modal {...props}>Test</Modal>;
+const TokenModal = ({ currencies = [], ...props }: TokenModalProps): JSX.Element => (
+  <Modal {...props}>
+    <H3>Select Token</H3>
+    <List>
+      {currencies.map((currency, key) => (
+        <ListItem key={key}>
+          <Span>{currency.currency}</Span>
+          <Flex>
+            <Span>{currency.balance}</Span>
+            <Span>{currency.usd}</Span>
+          </Flex>
+        </ListItem>
+      ))}
+    </List>
+  </Modal>
+);
 
 export { TokenModal };
 export type { CurrencyItem, TokenModalProps };
