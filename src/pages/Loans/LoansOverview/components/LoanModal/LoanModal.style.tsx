8ddbbb62f518f33587b59d9edae582ecf6ee30b6
Rui Simão <rui.daniel.simao@gmail.com>
2023-01-05 12:45:50 +0000

    fix(Modal): close btn

--- src/pages/Loans/LoansOverview/components/LoanModal/LoanModal.style.tsx
+++ src/pages/Loans/LoansOverview/components/LoanModal/LoanModal.style.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Tabs, theme } from '@/component-library';
 
 const StyledTabs = styled(Tabs)`
-  padding: ${theme.spacing.spacing12} ${theme.modal.header.paddingX} ${theme.modal.footer.paddingBottom};
+  padding: ${theme.spacing.spacing14} ${theme.modal.header.paddingX} ${theme.modal.footer.paddingBottom};
 `;
 
 const StyledWrapper = styled.div`
