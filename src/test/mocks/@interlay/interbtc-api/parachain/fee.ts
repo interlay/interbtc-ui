6f8396750afaa57898358203839c2fd537fe6779
crypto-engineer <anton@interlay.io>
2023-01-09 00:10:54 +0100

    refactor: move redeem fee related mocks to fee.ts

--- src/test/mocks/@interlay/interbtc-api/parachain/fee.ts
+++ src/test/mocks/@interlay/interbtc-api/parachain/fee.ts
@@ -1,14 +1,28 @@
+import { BitcoinAmount } from '@interlay/monetary-js';
 import Big from 'big.js';
 
 import { ISSUE_BRIDGE_FEE_RATE } from '@/config/parachain';
+import { REDEEM_BRIDGE_FEE_RATE } from '@/config/parachain';
 
 const DEFAULT_ISSUE_GRIEFING_COLLATERAL_RATE = new Big(0.00005);
+
+const CURRENT_INCLUSION_FEE = new BitcoinAmount(0.0000038);
 
 const mockFeeGetIssueFee = jest.fn(() => new Big(ISSUE_BRIDGE_FEE_RATE));
 
 const mockFeeGetIssueGriefingCollateralRate = jest.fn(() => DEFAULT_ISSUE_GRIEFING_COLLATERAL_RATE);
 
-// ray test touch <<
-// ray test touch >>
+const mockRedeemGetPremiumRedeemFeeRate = jest.fn(() => Big(0));
 
-export { mockFeeGetIssueFee, mockFeeGetIssueGriefingCollateralRate };
+const mockRedeemGetFeeRate = jest.fn(() => Big(REDEEM_BRIDGE_FEE_RATE));
+
+const mockRedeemGetCurrentInclusionFee = jest.fn(() => CURRENT_INCLUSION_FEE);
+
+export {
+  CURRENT_INCLUSION_FEE,
+  mockFeeGetIssueFee,
+  mockFeeGetIssueGriefingCollateralRate,
+  mockRedeemGetCurrentInclusionFee,
+  mockRedeemGetFeeRate,
+  mockRedeemGetPremiumRedeemFeeRate
+};
--- src/test/mocks/@interlay/interbtc-api/parachain/redeem.ts
+++ src/test/mocks/@interlay/interbtc-api/parachain/redeem.ts
@@ -1,14 +1,11 @@
 import { CollateralCurrencyExt, CurrencyExt, newMonetaryAmount } from '@interlay/interbtc-api';
-import { Bitcoin, BitcoinAmount, ExchangeRate } from '@interlay/monetary-js';
+import { Bitcoin, ExchangeRate } from '@interlay/monetary-js';
 import Big from 'big.js';
 
-import { REDEEM_BRIDGE_FEE_RATE } from '@/config/parachain';
 import { WRAPPED_TOKEN } from '@/config/relay-chains';
 
 const DEFAULT_MAX_BURNABLE_TOKENS = '100000000'; // 1 BTC
 const DEFAULT_BURN_EXCHANGE_RATE = '150000';
-
-const CURRENT_INCLUSION_FEE = new BitcoinAmount(0.0000038);
 
 const zeroWrappedTokenAmount = newMonetaryAmount(0, WRAPPED_TOKEN);
 
@@ -23,24 +20,12 @@ const mockRedeemBurn = jest.fn();
 
 const mockRedeemGetDustValue = jest.fn(() => zeroWrappedTokenAmount);
 
-// ray test touch <<
-const mockRedeemGetPremiumRedeemFeeRate = jest.fn(() => Big(0));
-
-const mockRedeemGetFeeRate = jest.fn(() => Big(REDEEM_BRIDGE_FEE_RATE));
-
-const mockRedeemGetCurrentInclusionFee = jest.fn(() => CURRENT_INCLUSION_FEE);
-// ray test touch >>
-
 const mockRedeemRequest = jest.fn();
 
 export {
-  CURRENT_INCLUSION_FEE,
   mockRedeemBurn,
   mockRedeemGetBurnExchangeRate,
-  mockRedeemGetCurrentInclusionFee,
   mockRedeemGetDustValue,
-  mockRedeemGetFeeRate,
   mockRedeemGetMaxBurnableTokens,
-  mockRedeemGetPremiumRedeemFeeRate,
   mockRedeemRequest
 };
