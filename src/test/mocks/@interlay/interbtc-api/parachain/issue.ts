c79f52e5efac6cc1aeb4e446e5fee69b9bd75afd
crypto-engineer <anton@interlay.io>
2023-01-11 20:13:12 +0100

    chore: clean up

--- src/test/mocks/@interlay/interbtc-api/parachain/issue.ts
+++ src/test/mocks/@interlay/interbtc-api/parachain/issue.ts
@@ -3,18 +3,15 @@ import { BitcoinAmount } from '@interlay/monetary-js';
 
 import { WRAPPED_TOKEN } from '@/config/relay-chains';
 
-// ray test touch <
 const MOCK_ISSUE_REQUEST_LIMITS = {
   singleVaultMaxIssuable: newMonetaryAmount('56527153', WRAPPED_TOKEN),
   totalMaxIssuable: newMonetaryAmount('493817337', WRAPPED_TOKEN)
 };
-// ray test touch >
 
 const mockIssueGetDustValue = jest.fn(() => BitcoinAmount.zero());
 
-// ray test touch <
 const mockIssueGetRequestLimits = jest.fn(() => MOCK_ISSUE_REQUEST_LIMITS);
-// ray test touch >
+
 const mockIssueRequest = jest.fn();
 
 export { MOCK_ISSUE_REQUEST_LIMITS, mockIssueGetDustValue, mockIssueGetRequestLimits, mockIssueRequest };
