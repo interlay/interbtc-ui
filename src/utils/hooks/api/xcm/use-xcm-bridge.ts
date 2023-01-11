7641e033d8d26800451c618444fdf6e2488e0671
Thomas Jeatt <hello@tomjeatt.com>
2023-01-11 15:02:57 +0000

    chore: configure endpoints

--- src/utils/hooks/api/xcm/use-xcm-bridge.ts
+++ src/utils/hooks/api/xcm/use-xcm-bridge.ts
@@ -24,12 +24,22 @@ const getEndpoints = (chains: ChainName[]) => {
           'wss://kusama.api.onfinality.io/public-ws',
           'wss://kusama-rpc.dwellir.com'
         ],
-        kintsugi: ['wss://api-kusama.interlay.io/parachain', 'wss://kintsugi.api.onfinality.io/public-ws']
+        kintsugi: ['wss://api-kusama.interlay.io/parachain', 'wss://kintsugi.api.onfinality.io/public-ws'],
+        statemine: [
+          'wss://statemine-rpc.polkadot.io',
+          'wss://statemine.api.onfinality.io/public-ws',
+          'wss://statemine-rpc.dwellir.com'
+        ]
       };
     case chains.includes('polkadot'):
       return {
         polkadot: ['wss://rpc.polkadot.io', 'wss://polkadot.api.onfinality.io/public-ws'],
-        interlay: ['wss://api.interlay.io/parachain', 'wss://interlay.api.onfinality.io/public-ws']
+        interlay: ['wss://api.interlay.io/parachain', 'wss://interlay.api.onfinality.io/public-ws'],
+        statemint: [
+          'wss://statemint-rpc.polkadot.io',
+          'wss://statemint.api.onfinality.io/public-ws',
+          'wss://statemint-rpc.dwellir.com'
+        ]
       };
 
     default:
