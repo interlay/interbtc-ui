5d4a7521a2f0b47f5bfd6a96800fd277c8e271cf
Rui Simão <rui.daniel.simao@gmail.com>
2023-01-17 12:37:11 +0000

    feat(Accordion): add size prop

--- src/component-library/Accordion/AccordionItem.tsx
+++ src/component-library/Accordion/AccordionItem.tsx
@@ -5,6 +5,7 @@ import { TreeState } from '@react-stately/tree';
 import { Node } from '@react-types/shared';
 import { useRef } from 'react';
 
+import { FontSize } from '../utils/prop-types';
 import {
   StyledAccordionItemButton,
   StyledAccordionItemHeading,
@@ -16,8 +17,12 @@ import { AccordionItemRegion } from './AccordionItemRegion';
 type AccordionItemProps<T> = {
   item: Node<T>;
   state: TreeState<T>;
+  size?: FontSize;
 };
-const AccordionItem = <T extends Record<string, unknown>>(props: AccordionItemProps<T>): JSX.Element => {
+const AccordionItem = <T extends Record<string, unknown>>({
+  size = 'base',
+  ...props
+}: AccordionItemProps<T>): JSX.Element => {
   const ref = useRef<HTMLButtonElement>(null);
   const { state, item } = props;
   const { buttonProps, regionProps } = useAccordionItem<T>(props, state, ref);
@@ -27,7 +32,7 @@ const AccordionItem = <T extends Record<string, unknown>>(props: AccordionItemPr
 
   return (
     <StyledAccordionItemWrapper $isDisabled={isDisabled}>
-      <StyledAccordionItemHeading size='base'>
+      <StyledAccordionItemHeading size={size}>
         <FocusRing within>
           <StyledAccordionItemButton
             {...mergeProps(buttonProps, focusProps)}
