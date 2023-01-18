96a36a4c759b9e789dfe42fa6fa28198495f4b92
Rui Simão <rui.daniel.simao@gmail.com>
2023-01-18 10:51:00 +0000

    final

--- src/pages/AMM/Swap/components/SwapInfo/SwapInfo.style.tsx
+++ src/pages/AMM/Swap/components/SwapInfo/SwapInfo.style.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
-import { Card, theme } from '@/component-library';
+import { theme } from '@/component-library';
 
-const StyledCard = styled(Card)`
+const StyledCard = styled.div`
   background-color: ${theme.card.secondaryBg};
   padding: ${theme.spacing.spacing2};
+  border-radius: ${theme.rounded.md};
 `;
 
 export { StyledCard };
