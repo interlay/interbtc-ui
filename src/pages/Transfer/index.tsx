caef271f53a938e54bb40969fb49cc6ddbdd7020
Thomas Jeatt <hello@tomjeatt.com>
2023-01-31 09:52:33 +0000

    refactor: remove redundant hook calls

--- src/pages/Transfer/index.tsx
+++ src/pages/Transfer/index.tsx
@@ -1,23 +1,13 @@
 import clsx from 'clsx';
-import * as React from 'react';
 
 import { Tabs, TabsItem } from '@/component-library';
 import Panel from '@/components/Panel';
 import MainContainer from '@/parts/MainContainer';
-import useUpdateQueryParameters, { QueryParameters } from '@/utils/hooks/use-update-query-parameters';
 
 import CrossChainTransferForm from './CrossChainTransferForm';
 import TransferForm from './TransferForm';
 
 const Transfer = (): JSX.Element | null => {
-  const updateQueryParameters = useUpdateQueryParameters();
-
-  const updateQueryParametersRef = React.useRef<(newQueryParameters: QueryParameters) => void>();
-
-  React.useLayoutEffect(() => {
-    updateQueryParametersRef.current = updateQueryParameters;
-  });
-
   return (
     <MainContainer>
       <Panel className={clsx('mx-auto', 'w-full', 'md:max-w-xl', 'p-10')}>
