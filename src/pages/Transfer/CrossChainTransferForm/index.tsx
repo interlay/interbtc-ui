877d9e5375573280a379b42a32d57f985ee6ed7a
Thomas Jeatt <hello@tomjeatt.com>
2023-02-01 11:37:43 +0000

    wip: updated form UI

--- src/pages/Transfer/CrossChainTransferForm/index.tsx
+++ src/pages/Transfer/CrossChainTransferForm/index.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 
 import { ParachainStatus, StoreType } from '@/common/types/util.types';
 import { Flex, TokenInput } from '@/component-library';
-import Accounts from '@/components/Accounts';
+import { SelectTrigger } from '@/component-library/Select/SelectTrigger';
 import SubmitButton from '@/components/SubmitButton';
 import { useSubstrateSecureState } from '@/lib/substrate';
 
@@ -16,39 +16,44 @@ const CrossChainTransferForm = (): JSX.Element => {
   return (
     <>
       <form className='space-y-8'>
-        <Flex direction='row' gap='spacing4' justifyContent='space-between'></Flex>
-        <div>
-          <TokenInput
-            placeholder='0.00'
-            ticker={'KINT'}
-            aria-label={t('forms.field_amount', {
-              field: `KINT ${t('deposit').toLowerCase()}`
-            })}
-            balance={0}
-            balanceDecimals={8}
-            valueUSD={0}
-            value={0}
-            name={'KINT'}
-            tokens={[
-              {
-                balance: 0,
-                balanceUSD: '0',
-                ticker: 'KSM'
-              },
-              {
-                balance: 0,
-                balanceUSD: '0',
-                ticker: 'KINT'
-              }
-            ]}
-            // onChange={handleChange}
-            // errorMessage={'Error'}
-          />
-        </div>
-        <Accounts label={t('transfer_page.cross_chain_transfer_form.target_account')} />
-        <SubmitButton disabled={parachainStatus === (ParachainStatus.Loading || ParachainStatus.Shutdown)}>
-          {selectedAccount ? t('transfer') : t('connect_wallet')}
-        </SubmitButton>
+        <Flex direction='column' gap='spacing4'>
+          <Flex direction='row' gap='spacing4' justifyContent='space-between'>
+            <SelectTrigger />
+            <SelectTrigger />
+          </Flex>
+          <div>
+            <TokenInput
+              placeholder='0.00'
+              ticker={'KINT'}
+              aria-label={t('forms.field_amount', {
+                field: `KINT ${t('deposit').toLowerCase()}`
+              })}
+              balance={0}
+              balanceDecimals={8}
+              valueUSD={0}
+              value={0}
+              name={'KINT'}
+              tokens={[
+                {
+                  balance: 0,
+                  balanceUSD: '0',
+                  ticker: 'KSM'
+                },
+                {
+                  balance: 0,
+                  balanceUSD: '0',
+                  ticker: 'KINT'
+                }
+              ]}
+              // onChange={handleChange}
+              // errorMessage={'Error'}
+            />
+          </div>
+          <SelectTrigger />
+          <SubmitButton disabled={parachainStatus === (ParachainStatus.Loading || ParachainStatus.Shutdown)}>
+            {selectedAccount ? t('transfer') : t('connect_wallet')}
+          </SubmitButton>
+        </Flex>
       </form>
     </>
   );
