cec6cc8418b9367094a31134fc0e224e65b4d4fc
Thomas Jeatt <hello@tomjeatt.com>
2023-02-02 14:22:02 +0000

    wip: modal open and close actions

--- src/components/AccountSelector/AccountSelector.tsx
+++ src/components/AccountSelector/AccountSelector.tsx
@@ -1,14 +1,22 @@
+import { useRef, useState } from 'react';
+
 import { SelectTrigger } from '@/component-library/Select/SelectTrigger';
 
 import { AccountSelectorModal } from './AccountSelectorModal';
 
-const AccountSelector = (): JSX.Element => (
-  <>
-    <SelectTrigger placeholder='Select account' />
-    <AccountSelectorModal isOpen={true}>
-      <p>A child</p>
-    </AccountSelectorModal>
-  </>
-);
+const AccountSelector = (): JSX.Element => {
+  const [isOpen, setOpen] = useState(false);
+
+  const tokenButtonRef = useRef<HTMLButtonElement>(null);
+
+  return (
+    <>
+      <SelectTrigger onPress={() => setOpen(true)} ref={tokenButtonRef} placeholder='Select account' />
+      <AccountSelectorModal onClose={() => setOpen(false)} isOpen={isOpen}>
+        <p>A child</p>
+      </AccountSelectorModal>
+    </>
+  );
+};
 
 export { AccountSelector };
