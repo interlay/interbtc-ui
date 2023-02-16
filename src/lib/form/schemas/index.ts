db0a9106b553fd6cf945d317895293235e57b6a6
Rui Simão <rui.daniel.simao@gmail.com>
2023-02-16 16:49:13 +0000

    feat(Swap): apply formik to swap form

--- src/lib/form/schemas/index.ts
+++ src/lib/form/schemas/index.ts
@@ -7,5 +7,13 @@ export type {
 export { depositLiquidityPoolSchema, WITHDRAW_LIQUIDITY_POOL_FIELD, withdrawLiquidityPoolSchema } from './amm';
 export type { LoanFormData, LoanValidationParams } from './loans';
 export { loanSchema } from './loans';
+export type { SwapFormData, SwapValidationParams } from './swap';
+export {
+  SWAP_INPUT_AMOUNT_FIELD,
+  SWAP_INPUT_TOKEN_FIELD,
+  SWAP_OUTPUT_TOKEN_FIELD,
+  SwapErrorMessage,
+  swapSchema
+} from './swap';
 export type { CreateVaultFormData } from './vaults';
 export { CREATE_VAULT_DEPOSIT_FIELD, createVaultSchema } from './vaults';
