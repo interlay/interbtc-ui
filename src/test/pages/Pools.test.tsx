570892e34e2908fd02455379b2f4f9a5eb02e6fb
Rui Simão <rui.daniel.simao@gmail.com>
2023-02-21 10:42:22 +0000

    feat: continue

--- src/test/pages/Pools.test.tsx
+++ src/test/pages/Pools.test.tsx
@@ -6,10 +6,11 @@ import {
   LP_TOKEN_1,
   LP_TOKEN_1_NAME,
   LP_TOKEN_2,
+  LP_TOKEN_2_NAME,
   mockGetLiquidityProvidedByAccount
 } from '../mocks/@interlay/interbtc-api/parachain/amm';
 import { render, screen } from '../test-utils';
-import { getTable, withinTable } from './utils/table';
+import { withinTable } from './utils/table';
 
 const path = '/pools';
 
@@ -21,14 +22,16 @@ jest.mock('../../parts/Layout', () => {
 
 describe('Pools Page', () => {
   it('should render pools', async () => {
+    // should only display available pools
     let app = await render(<App />, { path });
 
     let otherPoolsTable = withinTable(/other pools/i);
 
     expect(otherPoolsTable.getAllByRole('row')).toHaveLength(2);
+    expect(otherPoolsTable.getByRole('row', { name: LP_TOKEN_1_NAME })).toBeInTheDocument();
+    expect(otherPoolsTable.getByRole('row', { name: LP_TOKEN_2_NAME })).toBeInTheDocument();
 
     expect(screen.queryByRole('grid', { name: /my pools/i })).not.toBeInTheDocument();
-    // expect(table.getByRole('row', { name: LP_TOKEN_1_NAME })).toBeInTheDocument();
 
     mockGetLiquidityProvidedByAccount.mockResolvedValue([
       newMonetaryAmount(0, LP_TOKEN_1),
@@ -37,15 +40,18 @@ describe('Pools Page', () => {
 
     app.unmount();
 
+    // should display both available and account pools
     app = await render(<App />, { path });
 
     otherPoolsTable = withinTable(/other pools/i);
 
     expect(otherPoolsTable.getAllByRole('row')).toHaveLength(1);
+    expect(otherPoolsTable.getByRole('row', { name: LP_TOKEN_1_NAME })).toBeInTheDocument();
 
     let myPoolsTable = withinTable(/my pools/i);
 
     expect(myPoolsTable.getAllByRole('row')).toHaveLength(1);
+    expect(myPoolsTable.getByRole('row', { name: LP_TOKEN_2_NAME })).toBeInTheDocument();
 
     app.unmount();
 
@@ -54,14 +60,17 @@ describe('Pools Page', () => {
       newMonetaryAmount(10, LP_TOKEN_2)
     ]);
 
+    // should only display account pools
     app = await render(<App />, { path });
 
-    otherPoolsTable = withinTable(/other pools/i);
-
-    expect(otherPoolsTable.getAllByRole('row')).toHaveLength(0);
+    expect(screen.queryByRole('grid', { name: /other pools/i })).not.toBeInTheDocument();
 
     myPoolsTable = withinTable(/my pools/i);
 
     expect(myPoolsTable.getAllByRole('row')).toHaveLength(2);
   });
+
+  // it('should render pools', async () => {
+
+  // })
 });
--- src/test/pages/utils/table.ts
+++ src/test/pages/utils/table.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { screen, within } from '../../test-utils';
 
-const getTable = (name: string | RegExp) => screen.getByRole('grid', { name: new RegExp(name, 'i') });
+const getTable = (name: string | RegExp) =>
+  screen.getByRole('grid', { name: typeof name === 'string' ? new RegExp(name, 'i') : name });
 
 const withinTable = (name: string | RegExp) => {
   const table = within(getTable(name));
