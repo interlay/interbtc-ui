bf91fe911d3bb1a3935c3db77507e63a56c8ba07
Daniel Simão <rui.daniel.simao@gmail.com>
2023-02-22 13:55:02 +0000

    feat(Swap): apply formik to swap form (#956)

--- src/lib/form/index.tsx
+++ src/lib/form/index.tsx
@@ -1,4 +1,4 @@
-export type { CreateVaultFormData, DepositLiquidityPoolFormData, LoanFormData } from './schemas';
 export * from './schemas';
+export type { FormErrors } from './use-form';
 export { useForm } from './use-form';
 export { isFormDisabled } from './utils';
--- src/pages/AMM/shared/utils.ts
+++ src/pages/AMM/shared/utils.ts
@@ -1,4 +1,4 @@
-import { CurrencyExt, LiquidityPool, PooledCurrencies } from '@interlay/interbtc-api';
+import { CurrencyExt, PooledCurrencies } from '@interlay/interbtc-api';
 import { MonetaryAmount } from '@interlay/monetary-js';
 
 import { convertMonetaryAmountToValueInUSD } from '@/common/utils/utils';
@@ -20,11 +20,4 @@ const calculateAccountLiquidityUSD = (
   totalSupply: MonetaryAmount<CurrencyExt>
 ): number => lpTokenAmount.mul(totalLiquidityUSD).div(totalSupply.toBig()).toBig().toNumber();
 
-const getPooledTickers = (liquidityPools: LiquidityPool[]): Set<string> =>
-  liquidityPools.reduce((acc, pool) => {
-    pool.pooledCurrencies.forEach((curr) => acc.add(curr.currency.ticker));
-
-    return acc;
-  }, new Set<string>());
-
-export { calculateAccountLiquidityUSD, calculateTotalLiquidityUSD, getPooledTickers };
+export { calculateAccountLiquidityUSD, calculateTotalLiquidityUSD };
