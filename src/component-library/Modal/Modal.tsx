c6d12172e773769853f855c7fdcd5bd22831c1e8
Daniel Simão <rui.daniel.simao@gmail.com>
2023-03-01 10:39:26 +0000

    fix(Modal): restructure, improve accessibility and animations (#986)

--- src/component-library/Modal/Modal.tsx
+++ src/component-library/Modal/Modal.tsx
@@ -1,70 +1,44 @@
-import { AriaDialogProps, useDialog } from '@react-aria/dialog';
-import { FocusScope } from '@react-aria/focus';
-import { AriaOverlayProps, OverlayContainer, useModalOverlay } from '@react-aria/overlays';
-import { OverlayTriggerState } from '@react-stately/overlays';
-import { forwardRef, ReactNode } from 'react';
+import { forwardRef } from 'react';
 
-import { XMark } from '@/assets/icons';
-import { useMountTransition } from '@/utils/hooks/use-mount-transition';
-
-import { theme } from '../theme';
+import { Overlay } from '../Overlay';
 import { useDOMRef } from '../utils/dom';
-import { StyledCloseCTA, StyledDialog, StyledDialogWrapper, StyledUnderlay } from './Modal.style';
-import { ModalContext } from './ModalContext';
+import { Dialog, DialogProps } from './Dialog';
+import { ModalWrapper, ModalWrapperProps } from './ModalWrapper';
 
 type Props = {
-  children: ReactNode;
-  align?: 'top' | 'center';
+  container?: Element;
   hasMaxHeight?: boolean;
+  align?: 'top' | 'center';
 };
 
-type InheritAttrs = Omit<AriaDialogProps & AriaOverlayProps, keyof Props>;
+type InheritAttrs = Omit<ModalWrapperProps & DialogProps, keyof Props>;
 
 type ModalProps = Props & InheritAttrs;
 
 const Modal = forwardRef<HTMLDivElement, ModalProps>(
-  ({ children, isDismissable = true, align = 'center', hasMaxHeight, ...props }, ref): JSX.Element | null => {
-    const dialogRef = useDOMRef(ref);
+  (
+    { children, isDismissable = true, align = 'center', hasMaxHeight, isKeyboardDismissDisabled, container, ...props },
+    ref
+  ): JSX.Element | null => {
+    const domRef = useDOMRef(ref);
     const { isOpen, onClose } = props;
-    const { shouldRender, transitionTrigger } = useMountTransition(!!isOpen, theme.transition.duration.duration100);
 
-    // Handle interacting outside the dialog and pressing
-    // the Escape key to close the modal.
-    const { modalProps, underlayProps } = useModalOverlay(
-      { isDismissable, ...props },
-      // These are the only props needed
-      { isOpen: !!isOpen, close: onClose } as OverlayTriggerState,
-      dialogRef
+    return (
+      <Overlay isOpen={isOpen} container={container}>
+        <ModalWrapper
+          ref={domRef}
+          align={align}
+          isDismissable={isDismissable}
+          isOpen={isOpen}
+          isKeyboardDismissDisabled={isKeyboardDismissDisabled}
+          onClose={onClose}
+        >
+          <Dialog hasMaxHeight={hasMaxHeight} align={align} {...props}>
+            {children}
+          </Dialog>
+        </ModalWrapper>
+      </Overlay>
     );
-
-    // Get props for the dialog and its title
-    const { dialogProps, titleProps } = useDialog(props, dialogRef);
-
-    const isCentered = align === 'center';
-
-    return isOpen || shouldRender ? (
-      <OverlayContainer>
-        <StyledUnderlay {...underlayProps} $isCentered={isCentered}>
-          <FocusScope contain restoreFocus autoFocus>
-            <StyledDialogWrapper
-              ref={dialogRef}
-              $transitionTrigger={transitionTrigger}
-              $isCentered={isCentered}
-              {...modalProps}
-            >
-              <ModalContext.Provider value={{ titleProps, bodyProps: { overflow: isCentered ? 'auto' : undefined } }}>
-                <StyledDialog $isCentered={isCentered} $hasMaxHeight={hasMaxHeight} {...dialogProps}>
-                  <StyledCloseCTA size='small' variant='text' aria-label='Dismiss' onPress={onClose}>
-                    <XMark />
-                  </StyledCloseCTA>
-                  {children}
-                </StyledDialog>
-              </ModalContext.Provider>
-            </StyledDialogWrapper>
-          </FocusScope>
-        </StyledUnderlay>
-      </OverlayContainer>
-    ) : null;
   }
 );
 
