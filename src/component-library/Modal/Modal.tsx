19838c7c4d5f2a469085a8f8f042e1948dbf1718
Rui Simão <rui.daniel.simao@gmail.com>
2023-03-01 09:22:54 +0000

    feat: final

--- src/component-library/Modal/Modal.tsx
+++ src/component-library/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { forwardRef } from 'react';
 
 import { Overlay } from '../Overlay';
+import { useDOMRef } from '../utils/dom';
 import { Dialog, DialogProps } from './Dialog';
 import { ModalWrapper, ModalWrapperProps } from './ModalWrapper';
 
@@ -19,12 +20,13 @@ const Modal = forwardRef<HTMLDivElement, ModalProps>(
     { children, isDismissable = true, align = 'center', hasMaxHeight, isKeyboardDismissDisabled, container, ...props },
     ref
   ): JSX.Element | null => {
+    const domRef = useDOMRef(ref);
     const { isOpen, onClose } = props;
 
     return (
       <Overlay isOpen={isOpen} container={container}>
         <ModalWrapper
-          ref={ref}
+          ref={domRef}
           align={align}
           isDismissable={isDismissable}
           isOpen={isOpen}
--- src/component-library/Modal/ModalWrapper.tsx
+++ src/component-library/Modal/ModalWrapper.tsx
@@ -1,9 +1,8 @@
 import { AriaModalOverlayProps, AriaOverlayProps, useModalOverlay } from '@react-aria/overlays';
 import { mergeProps } from '@react-aria/utils';
 import { OverlayTriggerState } from '@react-stately/overlays';
-import { forwardRef, ReactNode } from 'react';
+import { forwardRef, ReactNode, RefObject } from 'react';
 
-import { useDOMRef } from '../utils/dom';
 import { StyledModal, StyledUnderlay } from './Modal.style';
 
 type Props = {
@@ -22,22 +21,20 @@ const ModalWrapper = forwardRef<HTMLDivElement, ModalWrapperProps>(
     { children, isDismissable = true, align = 'center', onClose, isKeyboardDismissDisabled, isOpen, ...props },
     ref
   ): JSX.Element | null => {
-    const modalRef = useDOMRef(ref);
-
     // Handle interacting outside the dialog and pressing
     // the Escape key to close the modal.
     const { modalProps, underlayProps } = useModalOverlay(
       { isDismissable, isKeyboardDismissDisabled, ...props },
       // These are the only props needed
       { isOpen: !!isOpen, close: onClose } as OverlayTriggerState,
-      modalRef
+      ref as RefObject<HTMLElement>
     );
 
     const isCentered = align === 'center';
 
     return (
       <StyledUnderlay {...underlayProps} $isOpen={!!isOpen} $isCentered={isCentered}>
-        <StyledModal ref={modalRef} $isOpen={isOpen} $isCentered={isCentered} {...mergeProps(modalProps, props)}>
+        <StyledModal ref={ref} $isOpen={isOpen} $isCentered={isCentered} {...mergeProps(modalProps, props)}>
           {children}
         </StyledModal>
       </StyledUnderlay>
