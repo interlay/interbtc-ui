6f64becde7bf85e9891f48b19d2a30f583c865b4
Rui Simão <rui.daniel.simao@gmail.com>
2023-03-21 09:13:55 +0000

    fix: test performance

--- src/test/mocks/@interlay/interbtc-api/parachain/btcRelay.ts
+++ src/test/mocks/@interlay/interbtc-api/parachain/btcRelay.ts
@@ -2,4 +2,19 @@ const MOCK_BTC_RELAY_HEIGHT = 1000;
 
 const mockBtcRelayGetLatestBlockHeight = jest.fn(() => MOCK_BTC_RELAY_HEIGHT);
 
-export { MOCK_BTC_RELAY_HEIGHT, mockBtcRelayGetLatestBlockHeight };
+const DEFAULT_STABLE_PARACHAIN_CONFIRMATION = 1;
+
+const DEFAULT_STABLE_BITCOIN_CONFIRMATION = 1;
+
+const mockGetStableParachainConfirmations = jest.fn().mockReturnValue(DEFAULT_STABLE_PARACHAIN_CONFIRMATION);
+
+const mockGetStableBitcoinConfirmations = jest.fn().mockReturnValue(DEFAULT_STABLE_BITCOIN_CONFIRMATION);
+
+export {
+  DEFAULT_STABLE_BITCOIN_CONFIRMATION,
+  DEFAULT_STABLE_PARACHAIN_CONFIRMATION,
+  MOCK_BTC_RELAY_HEIGHT,
+  mockBtcRelayGetLatestBlockHeight,
+  mockGetStableBitcoinConfirmations,
+  mockGetStableParachainConfirmations
+};
