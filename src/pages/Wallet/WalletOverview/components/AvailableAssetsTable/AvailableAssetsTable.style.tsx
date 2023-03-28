482220aba5369a34563503d43f235b77b987ba28
Rui Simão <rui.daniel.simao@gmail.com>
2023-03-28 11:06:52 +0100

    fix: continue

--- src/pages/Wallet/WalletOverview/components/AvailableAssetsTable/AvailableAssetsTable.style.tsx
+++ src/pages/Wallet/WalletOverview/components/AvailableAssetsTable/AvailableAssetsTable.style.tsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 
-import { Divider, Flex, theme } from '@/component-library';
+import { Flex, theme } from '@/component-library';
 
 const ListItemWrapper = styled(Flex)`
   padding: ${theme.spacing.spacing4} ${theme.spacing.spacing2};
 `;
-
-const CardDivider = styled(Divider)``;
 
 export { ListItemWrapper };
