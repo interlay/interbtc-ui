c32d9b7a0ce5c4b37e262ed7cf6422f95a77e8f4
ns212 <nikolai@interlay.io>
2023-03-28 13:26:08 +0000

    api: add market data api

--- api/market_data.py
+++ api/market_data.py
@@ -6,35 +6,12 @@ app = Flask(__name__)
 
 api_key = os.environ.get("CG_API_KEY")
 
+
 @app.after_request
 def add_header(response):
     response.cache_control.max_age = 0
     response.cache_control.s_maxage = 300
     return response
-
-
-def get_price_coinpaprika():
-    args = request.args
-
-    url = "https://api.coinpaprika.com/v1/tickers/"
-    tickers = {
-        "btc-bitcoin": "bitcoin",
-        "kint-kintsugi": "kintsugi",
-        "usdt-tether": "tether",
-        "ksm-kusama": "kusama",
-    }
-
-    output = {}
-    for ticker in tickers:
-        headers_dict = {"content-type": "application/json"}
-        resp = requests.get(url + ticker, headers=headers_dict)
-        resp.raise_for_status()
-        data = resp.json()
-        price = data["quotes"]["USD"]["price"]
-        output[tickers[ticker]] = price
-
-    print(output)
-    return str(output)
 
 
 @app.route("/marketdata/price", methods=["GET"])
@@ -44,7 +21,7 @@ def get_price():
     headers_dict = {
         "content-type": "application/json",
         "accept": "application/json",
-        "x-cg-pro-api-key": api_key
+        "x-cg-pro-api-key": api_key,
     }
     url = "https://pro-api.coingecko.com/api/v3/simple/price"
     resp = requests.get(url, params=args, headers=headers_dict)
