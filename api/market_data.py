a27f3198c53367b0ed91c97003d5ed11e8f04981
ns212 <nikolai@interlay.io>
2023-03-28 12:18:33 +0000

    api: add market data api

--- api/market_data.py
+++ api/market_data.py
@@ -12,14 +12,45 @@ def add_header(response):
     return response
 
 
+def get_price_coinpaprika():
+    args = request.args
+
+    url = "https://api.coinpaprika.com/v1/tickers/"
+    tickers = {
+        "btc-bitcoin": "bitcoin",
+        "kint-kintsugi": "kintsugi",
+        "usdt-tether": "tether",
+        "ksm-kusama": "kusama",
+    }
+
+    output = {}
+    for ticker in tickers:
+        headers_dict = {"content-type": "application/json"}
+        resp = requests.get(url + ticker, headers=headers_dict)
+        resp.raise_for_status()
+        data = resp.json()
+        price = data["quotes"]["USD"]["price"]
+        output[tickers[ticker]] = price
+
+    print(output)
+    return str(output)
+
+
 @app.route("/marketdata/price", methods=["GET"])
 def get_price():
     args = request.args
 
-    headers_dict = {"content-type": "application/json"}
-    url = "https://api.coingecko.com/api/v3/simple/price"
-    resp = requests.get(url, params=args, headers=headers_dict).json()
-    return str(resp)
+    try:
+        headers_dict = {"content-type": "application/json"}
+        url = "https://api.coingecko.com/api/v3/simple/price"
+        resp = requests.get(url, params=args, headers=headers_dict)
+        print(args)
+        print(resp)
+        resp.raise_for_status()
+        data = resp.json()
+        return str(data)
+    except Exception as e:
+        return get_price_coinpaprika()
 
 
 if __name__ == "__main__":
