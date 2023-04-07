e1339ac694f3b05327cf66ff668d912dfe4e1bf9
ns212 <nikolai@interlay.io>
2023-04-07 13:32:52 +0000

    api: handle terms and conditions

---
+++ api/terms.ts
@@ -0,0 +1,29 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import postgres from 'pg'
+const { Pool } = postgres;
+
+const pool = new Pool()
+const pattern = new URLPattern({ pathname: '/api/terms/:wallet' });
+
+export default async function (request: VercelRequest, response: VercelResponse) {
+    if (!pattern.test(request.url)) {
+        return response.status(400).send('Bad Request');
+    }
+
+    const result = pattern.exec(request.url);
+    const wallet = result.pathname.groups.wallet;
+
+    if (request.method === 'GET') {
+        const result = await pool.query('select exists(select 1 from signed_terms where wallet_id=$1)', [wallet])
+        return response.send(result.rows[0]);
+    } else if (request.method === 'POST') {
+        try {
+            const result = await pool.query('insert into signed_terms (wallet_id) values ($1)', [wallet])
+            return response.status(201);
+        } catch (error) {
+            console.log(error);
+            return response.status(400).send('Bad Request');
+        }
+    }
+    return response.status(400).send('Bad Request');
+}
