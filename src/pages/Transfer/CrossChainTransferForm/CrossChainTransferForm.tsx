8f31ee8667adcd49f5aaebb7db2f205afb5e9725
Thomas Jeatt <hello@tomjeatt.com>
2023-05-10 11:34:01 +0100

    refactor: remove duplicated code for fetching tokens

--- src/pages/Transfer/CrossChainTransferForm/CrossChainTransferForm.tsx
+++ src/pages/Transfer/CrossChainTransferForm/CrossChainTransferForm.tsx
@@ -140,15 +140,21 @@ const CrossChainTransferForm = (): JSX.Element => {
 
     form.setFieldValue(CROSS_CHAIN_TRANSFER_TO_FIELD, e.target.value, true);
 
-    const tokens = await getAvailableTokens(
-      form.values[CROSS_CHAIN_TRANSFER_FROM_FIELD] as ChainName,
-      e.target.value as ChainName,
-      accountId.toString(),
-      form.values[CROSS_CHAIN_TRANSFER_TO_ACCOUNT_FIELD] as string
-    );
+    const tokens = await handleGetTokens();
 
     setTransferableTokens(tokens);
     setCurrentToken(tokens[0]);
+  };
+
+  const handleGetTokens = async () => {
+    if (!accountId) return;
+
+    return await getAvailableTokens(
+      form.values[CROSS_CHAIN_TRANSFER_FROM_FIELD] as ChainName,
+      destinationChains[0].id as ChainName,
+      accountId?.toString(),
+      form.values[CROSS_CHAIN_TRANSFER_TO_ACCOUNT_FIELD] as string
+    );
   };
 
   useEffect(() => {
@@ -206,12 +212,7 @@ const CrossChainTransferForm = (): JSX.Element => {
     if (!accountId) return;
 
     const getTokensForNewChain = async () => {
-      const tokens = await getAvailableTokens(
-        form.values[CROSS_CHAIN_TRANSFER_FROM_FIELD] as ChainName,
-        destinationChains[0].id as ChainName,
-        accountId.toString(),
-        form.values[CROSS_CHAIN_TRANSFER_TO_ACCOUNT_FIELD] as string
-      );
+      const tokens = await handleGetTokens();
 
       setTransferableTokens(tokens);
       setCurrentToken(tokens[0]);
