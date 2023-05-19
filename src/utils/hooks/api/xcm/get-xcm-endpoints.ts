324e82f6cf1fcd7b3c3868724e12f1573675d853
Thomas Jeatt <hello@tomjeatt.com>
2023-05-19 10:03:29 +0100

    refactor: specify endpoints and remove unnecessary logic

--- src/utils/hooks/api/xcm/get-xcm-endpoints.ts
+++ src/utils/hooks/api/xcm/get-xcm-endpoints.ts
@@ -1,30 +1,22 @@
 import { ChainName } from '@interlay/bridge';
 
-type XCMEndpoints =
-  | {
-      [chain: string]: string[];
-    }
-  | undefined;
+type XCMEndpointsRecord = Record<ChainName, string[]>;
 
-const getXCMEndpoints = (chains: ChainName[]): XCMEndpoints => {
-  switch (true) {
-    case chains.includes('kusama'):
-      return {
-        kusama: ['wss://kusama-rpc.polkadot.io', 'wss://kusama.api.onfinality.io/public-ws'],
-        kintsugi: ['wss://api-kusama.interlay.io/parachain', 'wss://kintsugi.api.onfinality.io/public-ws'],
-        statemine: ['wss://statemine-rpc.polkadot.io', 'wss://statemine.api.onfinality.io/public-ws']
-      };
-    case chains.includes('polkadot'):
-      return {
-        polkadot: ['wss://rpc.polkadot.io', 'wss://polkadot.api.onfinality.io/public-ws'],
-        interlay: ['wss://api.interlay.io/parachain', 'wss://interlay.api.onfinality.io/public-ws'],
-        statemint: ['wss://statemint-rpc.polkadot.io', 'wss://statemint.api.onfinality.io/public-ws']
-      };
-
-    default:
-      return undefined;
-  }
+const XCMEndpoints: XCMEndpointsRecord = {
+  acala: ['wss://acala-rpc-0.aca-api.network', 'wss://acala-rpc.dwellir.com'],
+  astar: [],
+  bifrost: [],
+  heiko: [],
+  hydra: [],
+  interlay: ['wss://api.interlay.io/parachain', 'wss://interlay.api.onfinality.io/public-ws'],
+  karura: [],
+  kintsugi: ['wss://api-kusama.interlay.io/parachain', 'wss://kintsugi.api.onfinality.io/public-ws'],
+  kusama: ['wss://kusama-rpc.polkadot.io', 'wss://kusama.api.onfinality.io/public-ws'],
+  parallel: [],
+  polkadot: ['wss://rpc.polkadot.io', 'wss://polkadot.api.onfinality.io/public-ws'],
+  statemine: ['wss://statemine-rpc.polkadot.io', 'wss://statemine.api.onfinality.io/public-ws'],
+  statemint: ['wss://statemint-rpc.polkadot.io', 'wss://statemint.api.onfinality.io/public-ws']
 };
 
-export { getXCMEndpoints };
-export type { XCMEndpoints };
+export { XCMEndpoints };
+export type { XCMEndpointsRecord };
