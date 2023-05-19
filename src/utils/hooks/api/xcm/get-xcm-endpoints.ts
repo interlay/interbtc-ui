b19c4515d3a989df4dc724e5c56fc417107bf957
Thomas Jeatt <hello@tomjeatt.com>
2023-05-19 11:15:00 +0100

    chore: update endpoints

--- src/utils/hooks/api/xcm/get-xcm-endpoints.ts
+++ src/utils/hooks/api/xcm/get-xcm-endpoints.ts
@@ -5,17 +5,17 @@ type XCMEndpointsRecord = Record<ChainName, string[]>;
 const XCMEndpoints: XCMEndpointsRecord = {
   acala: ['wss://acala-rpc-0.aca-api.network', 'wss://acala-rpc.dwellir.com'],
   astar: ['wss://rpc.astar.network', 'wss://astar-rpc.dwellir.com'],
-  bifrost: ['wss://bifrost-polkadot.api.onfinality.io/public-ws', 'wss://hk.p.bifrost-rpc.liebi.com/ws'],
-  heiko: ['wss://heiko-rpc.parallel.fi', 'wss://parallel-heiko.api.onfinality.io/public-ws'],
-  hydra: ['wss://hydradx-rpc.dwellir.com', 'wss://hydradx.api.onfinality.io/public-ws'],
-  interlay: ['wss://api.interlay.io/parachain', 'wss://interlay.api.onfinality.io/public-ws'],
+  bifrost: ['wss://bifrost-rpc.dwellir.com'],
+  heiko: ['wss://heiko-rpc.parallel.fi'],
+  hydra: ['wss://rpc.hydradx.cloud', 'wss://hydradx-rpc.dwellir.com'],
+  interlay: ['wss://api.interlay.io/parachain'],
   karura: ['wss://karura-rpc-0.aca-api.network', 'wss://karura-rpc.dwellir.com'],
-  kintsugi: ['wss://api-kusama.interlay.io/parachain', 'wss://kintsugi.api.onfinality.io/public-ws'],
-  kusama: ['wss://kusama-rpc.polkadot.io', 'wss://kusama.api.onfinality.io/public-ws'],
+  kintsugi: ['wss://api-kusama.interlay.io/parachain'],
+  kusama: ['wss://kusama-rpc.polkadot.io', 'wss://kusama-rpc.dwellir.com'],
   parallel: ['wss://rpc.parallel.fi'],
-  polkadot: ['wss://rpc.polkadot.io', 'wss://polkadot.api.onfinality.io/public-ws'],
-  statemine: ['wss://statemine-rpc.polkadot.io', 'wss://statemine.api.onfinality.io/public-ws'],
-  statemint: ['wss://statemint-rpc.polkadot.io', 'wss://statemint.api.onfinality.io/public-ws']
+  polkadot: ['wss://rpc.polkadot.io', 'wss://polkadot-rpc.dwellir.com'],
+  statemine: ['wss://statemine-rpc.polkadot.io', 'wss://statemine-rpc.dwellir.com'],
+  statemint: ['wss://statemint-rpc.polkadot.io', 'wss://statemint-rpc.dwellir.com']
 };
 
 export { XCMEndpoints };
