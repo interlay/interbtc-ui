f145413516821c041a38be36f7cb19d09977a053
Rui Simão <rui.daniel.simao@gmail.com>
2023-06-02 11:33:16 +0100

    wip

---
+++ src/pages/Bridge/BridgeOverview/BridgeOverview.tsx
@@ -0,0 +1,7 @@
+import MainContainer from '@/parts/MainContainer';
+
+const BridgeOverview = (): JSX.Element => {
+  return <MainContainer>Hello</MainContainer>;
+};
+
+export default BridgeOverview;
