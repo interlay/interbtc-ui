94a77579b565485c8358e063aab3d40b8267ec85
Peter <peter@interlay.io>
2023-06-08 15:48:20 +0200

    fix(loans): display correct subsidy rewards accrued amount and APY

--- src/components/LoanPositionsTable/ApyCell.tsx
+++ src/components/LoanPositionsTable/ApyCell.tsx
@@ -15,7 +15,6 @@ type ApyCellProps = {
   earnedInterest?: MonetaryAmount<CurrencyExt>;
   accumulatedDebt?: MonetaryAmount<CurrencyExt>;
   rewardsPerYear: MonetaryAmount<CurrencyExt> | null;
-  accruedRewards: MonetaryAmount<CurrencyExt> | null;
   prices?: Prices;
   isBorrow?: boolean;
   onClick?: () => void;
@@ -25,7 +24,6 @@ const ApyCell = ({
   apy,
   currency,
   rewardsPerYear,
-  accruedRewards,
   accumulatedDebt,
   earnedInterest,
   prices,
@@ -54,7 +52,6 @@ const ApyCell = ({
         apy={apy}
         currency={currency}
         prices={prices}
-        rewards={accruedRewards}
         rewardsApy={rewardsApy}
         isBorrow={isBorrow}
         accumulatedDebt={accumulatedDebt}
