b0d99a931bc6990bc25cc6f41fbac64bcc30ca35
Daniel Simão <rui.daniel.simao@gmail.com>
2023-06-15 15:23:21 +0100

    feat: integrate use-transaction with TransactionFeeDetails (#1294)
    * feat: integrate use-transaction with TransactionFeeDetails
    
    * fix: code review

--- src/utils/hooks/transaction/use-transaction.ts
+++ src/utils/hooks/transaction/use-transaction.ts
@@ -3,13 +3,14 @@ import { MonetaryAmount } from '@interlay/monetary-js';
 import { ExtrinsicStatus } from '@polkadot/types/interfaces';
 import { ISubmittableResult } from '@polkadot/types/types';
 import { mergeProps } from '@react-aria/utils';
-import { useCallback, useState } from 'react';
+import { Key, useCallback, useState } from 'react';
 import { MutationFunction, useMutation, UseMutationOptions, UseMutationResult } from 'react-query';
 
 import { GOVERNANCE_TOKEN } from '@/config/relay-chains';
 import { useSubstrate } from '@/lib/substrate';
 
 import { useGetLiquidityPools } from '../api/amm/use-get-liquidity-pools';
+import { useGetCurrencies } from '../api/use-get-currencies';
 import { getExtrinsic, getStatus } from './extrinsics';
 import { Transaction, TransactionActions, TransactionArgs } from './types';
 import { useTransactionNotifications } from './use-transaction-notifications';
@@ -84,6 +85,8 @@ function useTransaction<T extends Transaction>(
   options?: UseTransactionOptions
 ): UseTransactionResult<T> {
   const { state } = useSubstrate();
+
+  const { getCurrencyFromTicker } = useGetCurrencies(true);
 
   const [isSigned, setSigned] = useState(false);
   const [feeCurrency, setFeeCurrency] = useState(GOVERNANCE_TOKEN);
@@ -188,9 +191,14 @@ function useTransaction<T extends Transaction>(
     [feeCurrency, pools, getParams]
   );
 
-  const handleFeeCurrencyChange = useCallback((currency: CurrencyExt) => {
+  const handleFeeTokenSelection = (ticker: Key) => {
+    // TODO: update TokenData to deal with Currency type
+    const currency = getCurrencyFromTicker(ticker as string);
+
+    if (!currency) return;
+
     setFeeCurrency(currency);
-  }, []);
+  };
 
   return {
     ...transactionMutation,
