19b122ff5aff56615f35660489060327ea342b89
Peter <peter@interlay.io>
2023-06-15 10:51:17 +0200

    wip

--- src/utils/hooks/transaction/use-transaction.ts
+++ src/utils/hooks/transaction/use-transaction.ts
@@ -5,18 +5,21 @@ import {
   isCurrencyEqual,
   LiquidityPool,
   MultiPath,
-  newCurrencyId
+  newCurrencyId,
+  Trade
 } from '@interlay/interbtc-api';
 import { MonetaryAmount } from '@interlay/monetary-js';
+import { SubmittableExtrinsic } from '@polkadot/api/types';
 import { ExtrinsicStatus } from '@polkadot/types/interfaces';
 import { ISubmittableResult } from '@polkadot/types/types';
 import { mergeProps } from '@react-aria/utils';
 import { useCallback, useState } from 'react';
 import { MutationFunction, useMutation, UseMutationOptions, UseMutationResult } from 'react-query';
 
-import { GOVERNANCE_TOKEN, RELAY_CHAIN_NATIVE_TOKEN } from '@/config/relay-chains';
+import { GOVERNANCE_TOKEN } from '@/config/relay-chains';
 import { useSubstrate } from '@/lib/substrate';
 
+import { useGetLiquidityPools } from '../api/amm/use-get-liquidity-pools';
 import { getExtrinsic, getStatus } from './extrinsics';
 import { Transaction, TransactionActions, TransactionArgs } from './types';
 import { useTransactionNotifications } from './use-transaction-notifications';
@@ -24,18 +27,18 @@ import { submitTransaction } from './utils/submit';
 
 type TransactionResult = { status: 'success' | 'error'; data: ISubmittableResult; error?: Error };
 
-// TODO: add feeEstimate and feeEstimateAsync
 type ExecuteArgs<T extends Transaction> = {
   // Executes the transaction
   execute<D extends Transaction = T>(...args: TransactionArgs<D>): void;
   // Similar to execute but returns a promise which can be awaited.
   executeAsync<D extends Transaction = T>(...args: TransactionArgs<D>): Promise<TransactionResult>;
+  estimateFee<D extends Transaction = T>(...args: TransactionArgs<D>): Promise<MonetaryAmount<CurrencyExt>>;
 };
 
-// TODO: add feeEstimate and feeEstimateAsync
 type ExecuteTypeArgs<T extends Transaction> = {
   execute<D extends Transaction = T>(type: D, ...args: TransactionArgs<D>): void;
   executeAsync<D extends Transaction = T>(type: D, ...args: TransactionArgs<D>): Promise<TransactionResult>;
+  estimateFee<D extends Transaction = T>(type: D, ...args: TransactionArgs<D>): Promise<MonetaryAmount<CurrencyExt>>;
 };
 
 type ExecuteFunctions<T extends Transaction> = ExecuteArgs<T> | ExecuteTypeArgs<T>;
@@ -51,67 +54,115 @@ type UseTransactionResult<T extends Transaction> = {
 } & ReactQueryUseMutationResult &
   ExecuteFunctions<T>;
 
-const reverseSwapPath = (path: MultiPath): Array<CurrencyId> => {
-  const reversedPath = path.reverse();
-  const inputCurrency = newCurrencyId(window.bridge.api, reversedPath[0].output);
-  const result: Array<CurrencyId> = [inputCurrency];
-  for (const { input } of reversedPath) {
-    result.push(newCurrencyId(window.bridge.api, input));
-  }
-  return result;
+const constructSwapPathPrimitive = (path: MultiPath): Array<CurrencyId> => {
+  const inputCurrency = newCurrencyId(window.bridge.api, path[0].input);
+  return [inputCurrency, ...path.map(({ output }) => newCurrencyId(window.bridge.api, output))];
 };
 
-const computeOptimalInputAmount = (
+// Recursively double input amount until the trade with higher than minimum output
+// amount is found.
+const getOptimalTradeForTxFeeSwap = (
   minOutputAmount: MonetaryAmount<CurrencyExt>,
   inputAmount: MonetaryAmount<CurrencyExt>,
   pools: Array<LiquidityPool>
-): MonetaryAmount<CurrencyExt> => {
-  const controlTrade = window.bridge.amm.getOptimalTrade(inputAmount, minOutputAmount.currency, pools);
-  if (controlTrade?.outputAmount.lt(minOutputAmount)) {
-    // If the output amount is lower than txFee double the input currency amount and check again.
-    return computeOptimalInputAmount(minOutputAmount, inputAmount.mul(2), pools);
+): Trade => {
+  const trade = window.bridge.amm.getOptimalTrade(inputAmount, minOutputAmount.currency, pools);
+  if (trade === null) {
+    // TODO: handle - trade path not found, not possible to do the swap
+    throw new Error();
   }
-  return inputAmount;
+  if (trade?.outputAmount.lt(minOutputAmount)) {
+    // If the output amount is lower than txFee double the input currency amount and check again.
+    return getOptimalTradeForTxFeeSwap(minOutputAmount, inputAmount.mul(2), pools);
+  }
+  return trade;
 };
 
-const wrapWithTxFeeSwap = async (
+const getTxFeeSwapData = async (
+  nativeTxFee: MonetaryAmount<CurrencyExt>,
   feeCurrency: CurrencyExt,
-  baseExtrinsicData: ExtrinsicData
-): Promise<ExtrinsicData> => {
-  if (isCurrencyEqual(feeCurrency, GOVERNANCE_TOKEN)) {
-    return baseExtrinsicData;
-  }
-  // TODO: Refactor (maybe parametrize) this after txFee estimation work is done.
-  const [nativeTxFee, pools] = await Promise.all([
-    window.bridge.transaction.getFeeEstimate(baseExtrinsicData.extrinsic),
-    window.bridge.amm.getLiquidityPools()
-  ]);
-
+  baseExtrinsic: SubmittableExtrinsic<'promise'>,
+  pools: Array<LiquidityPool>
+): Promise<{ swapPathPrimitive: Array<CurrencyId>; inputAmount: MonetaryAmount<CurrencyExt> }> => {
+  // First we construct reverse direction trade to get estimated swap path and amount
   const reverseDirectionTrade = window.bridge.amm.getOptimalTrade(nativeTxFee, feeCurrency, pools);
   if (reverseDirectionTrade === null) {
     // TODO: handle - trade path not found, not possible to do the swap
     throw new Error();
   }
+  // Final native token transaction fee is estimated for base extrinsic wrapped in multiTransactionPayment call.
+  // NOTE: We assume here the reverse direction trade has similar weight.
+  const reverseDirectionExtrinsic = window.bridge.api.tx.multiTransactionPayment.withFeeSwapPath(
+    constructSwapPathPrimitive(reverseDirectionTrade.path),
+    reverseDirectionTrade.outputAmount.toString(true),
+    baseExtrinsic
+  );
+  const withSwapTxFee = await window.bridge.transaction.getFeeEstimate(reverseDirectionExtrinsic);
+  const { inputAmount, path } = getOptimalTradeForTxFeeSwap(
+    withSwapTxFee.mul(1.5),
+    reverseDirectionTrade.outputAmount,
+    pools
+  );
+  const swapPathPrimitive = constructSwapPathPrimitive(path);
 
-  const inputAmount = computeOptimalInputAmount(nativeTxFee, reverseDirectionTrade.outputAmount, pools);
-  const path = reverseSwapPath(reverseDirectionTrade.path);
+  return { inputAmount, swapPathPrimitive };
+};
+
+const getTransactionFee: (
+  feeCurrency: CurrencyExt,
+  pools: Array<LiquidityPool>
+) => (params: TransactionActions) => Promise<MonetaryAmount<CurrencyExt>> = (feeCurrency, pools) => async (params) => {
+  const baseExtrinsicData = await getExtrinsic(params);
+  const nativeTxFee = await window.bridge.transaction.getFeeEstimate(baseExtrinsicData.extrinsic);
+
+  if (isCurrencyEqual(feeCurrency, GOVERNANCE_TOKEN)) {
+    return nativeTxFee;
+  }
+
+  const { inputAmount: swapTxFee } = await getTxFeeSwapData(
+    nativeTxFee,
+    feeCurrency,
+    baseExtrinsicData.extrinsic,
+    pools
+  );
+
+  return swapTxFee;
+};
+
+const wrapWithTxFeeSwap = async (
+  feeCurrency: CurrencyExt,
+  baseExtrinsicData: ExtrinsicData,
+  pools: Array<LiquidityPool>
+): Promise<ExtrinsicData> => {
+  if (isCurrencyEqual(feeCurrency, GOVERNANCE_TOKEN)) {
+    return baseExtrinsicData;
+  }
+  const nativeTxFee = await window.bridge.transaction.getFeeEstimate(baseExtrinsicData.extrinsic);
+
+  const { swapPathPrimitive, inputAmount } = await getTxFeeSwapData(
+    nativeTxFee,
+    feeCurrency,
+    baseExtrinsicData.extrinsic,
+    pools
+  );
   const wrappedCall = window.bridge.api.tx.multiTransactionPayment.withFeeSwapPath(
-    path,
+    swapPathPrimitive,
     inputAmount.toString(true),
     baseExtrinsicData.extrinsic
   );
-
+  console.log(wrappedCall.toHex());
   return { extrinsic: wrappedCall };
 };
 
-const mutateTransaction: (feeCurrency?: CurrencyExt) => MutationFunction<TransactionResult, TransactionActions> = (
-  feeCurrency
-) => async (params) => {
+const mutateTransaction: (
+  feeCurrency: CurrencyExt,
+  pools: Array<LiquidityPool>
+) => MutationFunction<TransactionResult, TransactionActions> = (feeCurrency, pools) => async (params) => {
   const expectedStatus = params.customStatus || getStatus(params.type);
   const baseExtrinsic = await getExtrinsic(params);
   // If fee currency is specified wrap the extrinsic in `withFeeSwapPath` call.
   const finalExtrinsic =
-    feeCurrency === undefined ? baseExtrinsic : await wrapWithTxFeeSwap(feeCurrency, baseExtrinsic);
+    feeCurrency === undefined ? baseExtrinsic : await wrapWithTxFeeSwap(feeCurrency, baseExtrinsic, pools);
 
   return submitTransaction(window.bridge.api, params.accountAddress, finalExtrinsic, expectedStatus, params.events);
 };
@@ -163,8 +214,10 @@ function useTransaction<T extends Transaction>(
     notifications.mutationProps
   );
 
+  const { data: pools } = useGetLiquidityPools();
+
   const { mutate, mutateAsync, ...transactionMutation } = useMutation(
-    mutateTransaction(RELAY_CHAIN_NATIVE_TOKEN),
+    mutateTransaction(options?.feeCurrency || GOVERNANCE_TOKEN, pools || []),
     optionsProp
   );
 
@@ -229,12 +282,22 @@ function useTransaction<T extends Transaction>(
     }
   };
 
+  const handleEstimateFee = useCallback(
+    (...args: Parameters<UseTransactionResult<T>['estimateFee']>) => {
+      const params = getParams(args);
+
+      return getTransactionFee(options?.feeCurrency || GOVERNANCE_TOKEN, pools || [])(params);
+    },
+    [options?.feeCurrency, pools, getParams]
+  );
+
   return {
     ...transactionMutation,
     isSigned,
     reject: handleReject,
     execute: handleExecute,
-    executeAsync: handleExecuteAsync
+    executeAsync: handleExecuteAsync,
+    estimateFee: handleEstimateFee
   };
 }
 
