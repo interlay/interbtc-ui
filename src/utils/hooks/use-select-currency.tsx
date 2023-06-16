e7cd0e45f126235349f0fe4c1fe7651fcbb88052
Peter <peter@interlay.io>
2023-06-16 14:06:31 +0200

    feat(bringyourownfee): add check for existing trade path

--- src/utils/hooks/use-select-currency.tsx
+++ src/utils/hooks/use-select-currency.tsx
@@ -1,18 +1,27 @@
+import { CurrencyExt, LiquidityPool } from '@interlay/interbtc-api';
+import { MonetaryAmount } from '@interlay/monetary-js';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { StoreType } from '@/common/types/util.types';
 import { convertMonetaryAmountToValueInUSD, formatUSD } from '@/common/utils/utils';
 import { TokenData } from '@/component-library';
+import { GOVERNANCE_TOKEN } from '@/config/relay-chains';
 
 import { getCoinIconProps } from '../helpers/coin-icon';
 import { getTokenPrice } from '../helpers/prices';
+import { useGetLiquidityPools } from './api/amm/use-get-liquidity-pools';
 import { useGetBalances } from './api/tokens/use-get-balances';
 import { useGetCurrencies } from './api/use-get-currencies';
 import { useGetPrices } from './api/use-get-prices';
 
 type SelectCurrencyResult = {
   items: TokenData[];
+};
+
+const canBeSwappedForNativeCurrency = (currency: CurrencyExt, pools: Array<LiquidityPool>): boolean => {
+  const trade = window.bridge.amm.getOptimalTrade(new MonetaryAmount(currency, 1), GOVERNANCE_TOKEN, pools);
+  return trade !== null;
 };
 
 const useSelectCurrency = (): SelectCurrencyResult => {
@@ -23,9 +32,18 @@ const useSelectCurrency = (): SelectCurrencyResult => {
   const { getAvailableBalance } = useGetBalances();
   const prices = useGetPrices();
 
+  const { data: pools } = useGetLiquidityPools();
+
+  const currenciesWithSwapPath = useMemo(() => {
+    if (currencies === undefined || pools === undefined) {
+      return [];
+    }
+    return currencies.filter((currency) => canBeSwappedForNativeCurrency(currency, pools));
+  }, [currencies, pools]);
+
   const items = useMemo(
     () =>
-      currencies?.map((currency) => {
+      currenciesWithSwapPath.map((currency) => {
         const balance = getAvailableBalance(currency.ticker);
         const balanceUSD = balance
           ? convertMonetaryAmountToValueInUSD(balance, getTokenPrice(prices, currency.ticker)?.usd)
@@ -37,8 +55,8 @@ const useSelectCurrency = (): SelectCurrencyResult => {
           value: currency.ticker,
           ...getCoinIconProps(currency)
         };
-      }) || [],
-    [currencies, getAvailableBalance, prices]
+      }),
+    [currenciesWithSwapPath, getAvailableBalance, prices]
   );
 
   return {
