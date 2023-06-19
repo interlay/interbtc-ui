e2d4a5c9333af59730ce253c07c46756af158829
Rui Simão <rui.daniel.simao@gmail.com>
2023-06-19 16:05:25 +0100

    fix: final

--- src/lib/form/utils.ts
+++ src/lib/form/utils.ts
@@ -3,6 +3,6 @@ import { useForm } from './use-form';
 const isFormDisabled = (form: ReturnType<typeof useForm>): boolean => !form.isValid || !form.dirty;
 
 const isFormComplete = (form: ReturnType<typeof useForm>, ingoreKeys: string[]): boolean =>
-  !Object.keys(form.errors).filter((errorKey) => !ingoreKeys.includes(errorKey)).length;
+  !Object.keys(form.errors).filter((errorKey) => !ingoreKeys.includes(errorKey)).length && form.dirty;
 
 export { isFormComplete, isFormDisabled };
