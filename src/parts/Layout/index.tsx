c05cd8e1152643aafb143fed45ce801d9384eff0
Thomas Jeatt <hello@tomjeatt.com>
2023-06-20 15:47:11 +0100

    refactor: add Wrapper component

--- src/parts/Layout/index.tsx
+++ src/parts/Layout/index.tsx
@@ -4,8 +4,6 @@ import MaintenanceBanner from '@/parts/MaintenanceBanner';
 import Sidebar from '@/parts/Sidebar';
 import Topbar from '@/parts/Topbar';
 
-import { StyledLayout } from './Layout.style';
-
 interface Props {
   className?: string;
   children: React.ReactNode;
@@ -13,15 +11,13 @@ interface Props {
 
 const Layout = ({ className, children }: Props): JSX.Element => {
   return (
-    <StyledLayout>
-      <Sidebar className={className}>
-        <div className={clsx('relative', 'min-h-screen')}>
-          <MaintenanceBanner />
-          <Topbar />
-          {children}
-        </div>
-      </Sidebar>
-    </StyledLayout>
+    <Sidebar className={className}>
+      <div className={clsx('relative', 'min-h-screen')}>
+        <MaintenanceBanner />
+        <Topbar />
+        {children}
+      </div>
+    </Sidebar>
   );
 };
 
--- src/parts/Wrapper/Wrapper.style.tsx
+++ src/parts/Wrapper/Wrapper.style.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 import dysonsphere from '@/assets/img/dysonsphere.svg';
 
-const StyledLayout = styled('div')`
+const StyledWrapper = styled('div')`
   background: url(${dysonsphere}) no-repeat;
-  background-size: cover;
-  background-position: top 0 right 100%;
+  /* background-size: cover; */
+  background-position: right bottom;
   background-attachment: fixed;
 `;
 
-export { StyledLayout };
+export { StyledWrapper };
