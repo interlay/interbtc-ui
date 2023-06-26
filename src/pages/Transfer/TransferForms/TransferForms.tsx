90e896b2b0a8125170bb88ff4311031992f4dce2
Thomas Jeatt <hello@tomjeatt.com>
2023-06-26 14:01:41 +0100

    refactor: add tab props

--- src/pages/Transfer/TransferForms/TransferForms.tsx
+++ src/pages/Transfer/TransferForms/TransferForms.tsx
@@ -1,30 +1,35 @@
 import { Flex, Tabs, TabsItem } from '@/component-library';
 import MainContainer from '@/parts/MainContainer';
+import { useTabPageLocation } from '@/utils/hooks/use-tab-page-location';
 
 import { CrossChainTransferForm, TransferForm } from './components';
 import { StyledCard, StyledFormWrapper, StyledWrapper } from './TransferForms.styles';
 
-const TransferForms = (): JSX.Element => (
-  <MainContainer>
-    <StyledWrapper>
-      <StyledCard gap='spacing2'>
-        <Flex direction='column' gap='spacing8'>
-          <Tabs size='large' fullWidth>
-            <TabsItem title='Transfer' key='transfer'>
-              <StyledFormWrapper>
-                <TransferForm />
-              </StyledFormWrapper>
-            </TabsItem>
-            <TabsItem title='Cross Chain Transfer' key='crossChainTransfer'>
-              <StyledFormWrapper>
-                <CrossChainTransferForm />
-              </StyledFormWrapper>
-            </TabsItem>
-          </Tabs>
-        </Flex>
-      </StyledCard>
-    </StyledWrapper>
-  </MainContainer>
-);
+const TransferForms = (): JSX.Element => {
+  const { tabsProps } = useTabPageLocation();
+
+  return (
+    <MainContainer>
+      <StyledWrapper>
+        <StyledCard gap='spacing2'>
+          <Flex direction='column' gap='spacing8'>
+            <Tabs {...tabsProps} size='large' fullWidth>
+              <TabsItem title='Transfer' key='transfer'>
+                <StyledFormWrapper>
+                  <TransferForm />
+                </StyledFormWrapper>
+              </TabsItem>
+              <TabsItem title='Cross Chain Transfer' key='crossChainTransfer'>
+                <StyledFormWrapper>
+                  <CrossChainTransferForm />
+                </StyledFormWrapper>
+              </TabsItem>
+            </Tabs>
+          </Flex>
+        </StyledCard>
+      </StyledWrapper>
+    </MainContainer>
+  );
+};
 
 export default TransferForms;
