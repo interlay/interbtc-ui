0cf213c91788ef54d208ba8fd4bb439711405e71
Rui Simão <rui.daniel.simao@gmail.com>
2023-07-04 16:12:06 +0100

    fix: continue

--- src/test/mocks/@interlay/interbtc-api/parachain/extrinsic.ts
+++ src/test/mocks/@interlay/interbtc-api/parachain/extrinsic.ts
@@ -8,7 +8,7 @@ const DEFAULT_EXTRINSIC: ExtrinsicData = {
 
         setTimeout(() => {
           cb({ status: { isReady: true, isInBlock: true, isFinalized: true, type: 'Finalized' } });
-        }, 3);
+        }, 1);
       });
     })
   } as unknown) as ExtrinsicData['extrinsic']
