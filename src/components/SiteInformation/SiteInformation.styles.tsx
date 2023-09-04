ddd5c22b7eb31caaf77b24b22eb67e0555ca936f
Tom Jeatt <hello@tomjeatt.com>
2023-09-04 13:52:58 +0100

    refactor: extend main container

---
+++ src/components/SiteInformation/SiteInformation.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import { MainContainer } from '../MainContainer';
+
+const StyledContainer = styled(MainContainer)`
+  padding-bottom: 0;
+  padding-top: 0;
+`;
+
+export { StyledContainer };
--- src/components/SiteInformation/SiteInformation.tsx
+++ src/components/SiteInformation/SiteInformation.tsx
@@ -1,20 +1,24 @@
 import { Alert, TextLink } from '@/component-library';
 
+import { StyledContainer } from './SiteInformation.styles';
+
 const SiteInformation = (): JSX.Element => {
   const hasLink = !!process.env.REACT_APP_SITE_INFORMATION_LINK;
 
   return (
-    <Alert status='warning'>
-      {process.env.REACT_APP_SITE_INFORMATION_MESSAGE}
-      {hasLink && (
-        <>
-          {' '}
-          <TextLink external icon to={process.env.REACT_APP_SITE_INFORMATION_LINK || ''}>
-            More information
-          </TextLink>
-        </>
-      )}
-    </Alert>
+    <StyledContainer>
+      <Alert status='warning'>
+        {process.env.REACT_APP_SITE_INFORMATION_MESSAGE}
+        {hasLink && (
+          <>
+            {' '}
+            <TextLink external icon to={process.env.REACT_APP_SITE_INFORMATION_LINK || ''}>
+              More information
+            </TextLink>
+          </>
+        )}
+      </Alert>
+    </StyledContainer>
   );
 };
 
