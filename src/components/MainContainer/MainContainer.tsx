726239d3698b4049670618a09a7792a2219b8986
Tom Jeatt <hello@tomjeatt.com>
2023-09-04 14:18:02 +0100

    rename const

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -6,11 +6,11 @@ import { StyledContainer } from './MainContainer.styles';
 type MainContainerProps = FlexProps;
 
 const MainContainer = ({ direction = 'column', gap = 'spacing8', ...props }: MainContainerProps): JSX.Element => {
-  const showGlobalWarningMessage = !!process.env.REACT_APP_SITE_INFORMATION_MESSAGE;
+  const showSiteInformationMessage = !!process.env.REACT_APP_SITE_INFORMATION_MESSAGE;
 
   return (
     <StyledContainer direction={direction} gap={gap} {...props}>
-      {showGlobalWarningMessage && <SiteInformation />}
+      {showSiteInformationMessage && <SiteInformation />}
       {props.children}
     </StyledContainer>
   );
