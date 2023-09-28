c16a83b4493f82701d165ffea6f3143d4615ca60
Tom Jeatt <hello@tomjeatt.com>
2023-09-28 12:00:39 +0100

    fix: correct ids for vDOT

--- src/component-library/CoinIcon/icons/VDOT.tsx
+++ src/component-library/CoinIcon/icons/VDOT.tsx
@@ -5,7 +5,7 @@ import { Icon, IconProps } from '@/component-library/Icon';
 const VDOT = forwardRef<SVGSVGElement, IconProps>((props, ref) => (
   <Icon {...props} ref={ref} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
     <title>VDOT</title>
-    <g clipPath='url(#a)'>
+    <g clipPath='url(#vdot-a)'>
       <path
         fill='#fff'
         d='M24 12.244C24 5.778 18.627.536 12 .536S0 5.778 0 12.244C0 18.709 5.373 23.95 12 23.95s12-5.242 12-11.707Z'
@@ -17,17 +17,17 @@ const VDOT = forwardRef<SVGSVGElement, IconProps>((props, ref) => (
         clipRule='evenodd'
       />
       <path
-        stroke='url(#b)'
+        stroke='url(#vdot-b)'
         d='M22.75 12.244c0 5.774-4.801 10.475-10.75 10.475S1.25 18.018 1.25 12.244 6.051 1.768 12 1.768 22.75 6.47 22.75 12.244Z'
       />
-      <path fill='url(#c)' stroke='#fff' d='M18.5 23.5a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z' />
+      <path fill='url(#vdot-c)' stroke='#fff' d='M18.5 23.5a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z' />
       <path
         fill='#fff'
-        d='m18.454 20.945-2.404-3.52a.29.29 0 0 1 .246-.45h1.069c.096 0 .185.046.24.122l.854 1.171a.3.3 0 0 0 .482 0l.853-1.171a.298.298 0 0 1 .241-.121h1.069c.236 0 .377.257.246.448l-2.404 3.52a.3.3 0 0 1-.492 0Z'
+        d='m18.454 20.945-2.404-3.52c-.13-.192.01-.45.246-.45h1.069c.096 0 .185.046.24.122l.854 1.171a.3.3 0 0 0 .482 0l.853-1.171a.298.298 0 0 1 .241-.121h1.069c.236 0 .377.257.246.448l-2.404 3.52a.3.3 0 0 1-.492 0Z'
       />
     </g>
     <defs>
-      <linearGradient id='b' x1='12' x2='12' y1='.536' y2='23.951' gradientUnits='userSpaceOnUse'>
+      <linearGradient id='vdot-b' x1='12' x2='12' y1='.536' y2='23.951' gradientUnits='userSpaceOnUse'>
         <stop stopColor='#7AEDCF' />
         <stop offset='.201' stopColor='#68CEFA' />
         <stop offset='.403' stopColor='#689CF8' />
@@ -35,14 +35,14 @@ const VDOT = forwardRef<SVGSVGElement, IconProps>((props, ref) => (
         <stop offset='.802' stopColor='#E65659' />
         <stop offset='1' stopColor='#F2C241' />
       </linearGradient>
-      <linearGradient id='c' x1='17.938' x2='17.938' y1='23' y2='14' gradientUnits='userSpaceOnUse'>
+      <linearGradient id='vdot-c' x1='17.938' x2='17.938' y1='23' y2='14' gradientUnits='userSpaceOnUse'>
         <stop stopColor='#fff' />
         <stop offset='0' stopColor='#F1B744' />
         <stop offset='.302' stopColor='#D65777' />
         <stop offset='.563' stopColor='#69B4FA' />
         <stop offset='1' stopColor='#79EBD3' />
       </linearGradient>
-      <clipPath id='a'>
+      <clipPath id='vdot-a'>
         <path fill='#fff' d='M0 0h24v24H0z' />
       </clipPath>
     </defs>
