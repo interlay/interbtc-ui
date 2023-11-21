a24b33c30f19e0ed649c09ceb9a87d940af124d1
Brendon Votteler <bvotteler@users.noreply.github.com>
2023-11-21 11:22:42 +0200

    fix: process env var names

--- api/tvl_dex.js
+++ api/tvl_dex.js
@@ -2,11 +2,11 @@ import { createInterBtcApi } from "@interlay/interbtc-api";
 
 const tvlDex = async (request, response) => {
     if (request.method === 'GET') {
-        const interbtcApi = await createInterBtcApi(REACT_APP_RELAY_CHAIN_URL, REACT_APP_BITCOIN_NETWORK);
+        const interbtcApi = await createInterBtcApi(process.env.REACT_APP_RELAY_CHAIN_URL, process.env.REACT_APP_BITCOIN_NETWORK);
 
         const pools = await interbtcApi.amm.getLiquidityPools();
         const currencies = pools.flatMap((pool) => pool.pooledCurrencies);
-        
+
         return response.status(200)
             .setHeader("content-type", "application/json")
             .setHeader("cache-control", "public, maxage=0, s-maxage=300")
