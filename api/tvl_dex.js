df9104753c31884afd2789ee17189d57996c17d8
Brendon Votteler <bvotteler@users.noreply.github.com>
2023-11-21 09:10:08 +0200

    feat: return pooled MonetaryAmounts in json format

--- api/tvl_dex.js
+++ api/tvl_dex.js
@@ -4,8 +4,13 @@ const tvlDex = async (request, response) => {
     if (request.method === 'GET') {
         const interbtcApi = await createInterBtcApi(REACT_APP_RELAY_CHAIN_URL, REACT_APP_BITCOIN_NETWORK);
 
-        interbtcApi.disconnect();
-        return response.status(200).send(`OK. (chain url ${REACT_APP_RELAY_CHAIN_URL} / newtork ${REACT_APP_BITCOIN_NETWORK}`);
+        const pools = await interbtcApi.amm.getLiquidityPools();
+        const currencies = pools.flatMap((pool) => pool.pooledCurrencies);
+        
+        return response.status(200)
+            .setHeader("content-type", "application/json")
+            .setHeader("cache-control", "public, maxage=0, s-maxage=300")
+            .json(currencies);
     } else {
         return response.status(400).send('Bad Request');
     }
