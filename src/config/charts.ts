774e02f8d8fdb3386b688293e7458913e8f0e41e
Tom Jeatt <hello@tomjeatt.com>
2024-02-08 12:03:42 +0000

    remove redundant comment

--- src/config/charts.ts
+++ src/config/charts.ts
@@ -1,6 +1,3 @@
-// Get 6 values to be able to calculate difference between 5 days ago and 6 days ago.
-// Thus volume per day 5 days ago can be displayed.
-// Cumulative volume is also only displayed to 5 days.
 const COUNT_OF_DATES_FOR_CHART = 180;
 
 export { COUNT_OF_DATES_FOR_CHART };
