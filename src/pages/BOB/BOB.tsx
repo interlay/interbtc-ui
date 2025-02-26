17d421a98a9280eae35fc66c44298c504d59383e
Tom Jeatt <hello@tomjeatt.com>
2025-02-26 11:44:20 +0000

    chore: copy change

--- src/pages/BOB/BOB.tsx
+++ src/pages/BOB/BOB.tsx
@@ -157,7 +157,10 @@ const BOB = (): JSX.Element => {
                     to receive the NFT and sign the transaction with your Interlay account to prove your community
                     status.
                   </P>
-                  <P>We&apos;ll let you know in Interlay Discord when the NFT will be available to claim.</P>
+                  <P>
+                    Submit your Ethereum address below to register. We&apos;ll let you know in Interlay Discord when the NFT
+                    is available to claim.
+                  </P>
                 </Flex>
                 <form onSubmit={form.handleSubmit}>
                   <Flex direction='column' gap='spacing8'>
@@ -171,7 +174,7 @@ const BOB = (): JSX.Element => {
                     </Flex>
                     <Flex direction='column' gap='spacing4'>
                       <AuthCTA type='submit' size='large'>
-                        Claim your NFT
+                        Register your Ethereum address
                       </AuthCTA>
                     </Flex>
                   </Flex>
