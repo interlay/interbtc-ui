f9730bd3e63b9bb59f3fc1f95bc89c84fd409fb4
tomjeatt <40243778+tomjeatt@users.noreply.github.com>
2025-10-13 13:33:58 +0100

    [release] Kintsugi 2.41.19 (#1692)

--- src/pages/SendAndReceive/SendAndReceiveForms/SendAndReceiveForms.tsx
+++ src/pages/SendAndReceive/SendAndReceiveForms/SendAndReceiveForms.tsx
@@ -1,10 +1,9 @@
-import { Alert, Flex, P, Tabs, TabsItem, TextLink } from '@/component-library';
+import { Flex, Tabs, TabsItem } from '@/component-library';
 import { MainContainer } from '@/components';
 import { usePageQueryParams } from '@/hooks/use-page-query-params';
 import { QUERY_PARAMETERS, QUERY_PARAMETERS_VALUES } from '@/utils/constants/links';
 
-// import { BridgeForm, TransferForm } from './components';
-import { TransferForm } from './components';
+import { BridgeForm, TransferForm } from './components';
 import { StyledCard, StyledFormWrapper, StyledWrapper } from './SendAndReceiveForms.styles';
 
 const SendAndReceiveForms = (): JSX.Element => {
@@ -25,16 +24,7 @@ const SendAndReceiveForms = (): JSX.Element => {
               </TabsItem>
               <TabsItem title='Bridge' key={QUERY_PARAMETERS_VALUES.TRANSFER.TAB.BRIDGE}>
                 <StyledFormWrapper>
-                  <Alert status='info'>
-                    <P size='s'>
-                    The Kintsugi Bridge is in maintenance mode while{' '}
-                    <TextLink external to='https://kusama.subsquare.io/referenda/601' color='secondary'>
-                      Referenda 601
-                    </TextLink>{' '}
-                    (Runtime Upgrade 1.9.1) is in progress. Bridging will be available when all upgrades have been completed.
-                    </P>
-                  </Alert>
-                  {/* <BridgeForm /> */}
+                  <BridgeForm />
                 </StyledFormWrapper>
               </TabsItem>
             </Tabs>
